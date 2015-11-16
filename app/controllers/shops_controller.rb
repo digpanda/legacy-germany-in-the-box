1077f600c5f97f2e4e9f8180a8d0823543255e4b
idlesummerbreeze <idlesummerbreeze@gmail.com>
2015-11-16 22:38:59 +0100

    add basic view for shop

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,8 +4,20 @@ class ShopsController <  ApplicationController
 
   skip_before_action :verify_authenticity_token
 
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   before_action(:only =>  [:create, :update]) { 
     base64_to_uploadedfile :shop, :logo
   }
+
+  private
+
+  def set_post
+    @shop = Shop.find(params[:id])
+  end
+
+  def post_params
+    params.require(:shop).permit(:name, :desc, :logo, :banner)
+  end
 
 end
