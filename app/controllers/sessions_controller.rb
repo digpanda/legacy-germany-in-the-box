81f6ec5eba2d5299ae9e61d88ffee456d288f9a3
idlesummerbreeze <idlesummerbreeze@gmail.com>
2015-12-24 21:39:28 +0100

    show errors in the header of sign in dialog

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,11 @@ class SessionsController < Devise::SessionsController
   skip_before_filter :require_no_authentication, :only => [:new]
 
   def create
-    logger.info("###########################################" + session[:login_failure_counter].to_s)
     if session[:login_failure_counter].present? and session[:login_failure_counter] > 2
       if valid_captcha?(params[:captcha])
         super
       else
-        flash[:error] = "Captcha has wrong, try a again."
+        flash[:error] = "Captcha has wrong, try again."
         redirect_to home_path
       end
     else
