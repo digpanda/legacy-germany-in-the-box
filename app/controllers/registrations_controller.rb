ca4d1a6584fe1da3367a01118fd42acb6b8349a8
idlesummerbreeze <idlesummerbreeze@gmail.com>
2015-12-31 11:18:20 +0100

    fix up sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,22 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     if valid_captcha?(params[:captcha])
-      build_resource
-      if resource.save
-        if resource.active_for_authentication?
-          set_flash_message :notice, :signed_up if is_navigational_format?
-          sign_up(resource_name, resource)
-          respond_with resource, :location => after_sign_up_path_for(resource)
-        else
-          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-          expire_session_data_after_sign_in!
-          respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-        end
-      else
-        clean_up_passwords resource
-        flash[:error] ||= resource.errors.full_messages.join(', ')
-        redirect_to home_path
-      end
+      super
     else
       flash[:error] = "Captcha has wrong, try a again."
       redirect_to home_path
