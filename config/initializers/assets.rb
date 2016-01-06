3d340283a419a6759d849c4ce8c34e61b79d921a
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-01-06 23:51:23 +0100

    left menu

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( achat.css couponia/* jquery.js jquery_ujs.js jquery-ui/widget.js jquery-ui/autocomplete.js )
+Rails.application.config.assets.precompile += %w( achat.css cssmenu/* couponia/* jquery.js jquery_ujs.js jquery-ui/widget.js jquery-ui/autocomplete.js )
