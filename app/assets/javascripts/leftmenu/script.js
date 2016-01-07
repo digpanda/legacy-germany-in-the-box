e2676d1b3dbd1e215f8e7f600484f2cc931de3d4
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-01-07 12:07:12 +0100

    css for left menu

--- app/assets/javascripts/leftmenu/script.js
+++ app/assets/javascripts/leftmenu/script.js
@@ -1,7 +1,7 @@
 ( function( $ ) {
     $( document ).ready(function() {
-        $('#cssmenu > ul > li > a').click(function() {
-            $('#cssmenu li').removeClass('active');
+        $('#left_menu > ul > li > a').click(function() {
+            $('#left_menu li').removeClass('active');
             $(this).closest('li').addClass('active');
             var checkElement = $(this).next();
             if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
@@ -9,7 +9,7 @@
                 checkElement.slideUp('normal');
             }
             if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-                $('#cssmenu ul ul:visible').slideUp('normal');
+                $('#left_menu ul ul:visible').slideUp('normal');
                 checkElement.slideDown('normal');
             }
             if($(this).closest('li').find('ul').children().length == 0) {
