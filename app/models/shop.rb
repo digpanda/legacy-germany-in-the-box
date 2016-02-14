17906f333795ac5a018ead3a6b7d6147541ff6ed
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-02-14 14:18:17 +0100

    shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,3 @@
-
 class Shop
   include Mongoid::Document
   include Mongoid::Timestamps::Created::Short
@@ -6,25 +5,32 @@ class Shop
 
   strip_attributes
 
-  field :name,        type: String
-  field :desc,        type: String
-  field :logo,        type: String
-  field :banner,      type: String
-  field :philosophy,  type: String
-  field :story,       type: String
-  field :ustid,       type: String
-  field :eroi,        type: String
-  field :sms,         type: Boolean,  default: false
+  field :name,            type: String
+  field :desc,            type: String
+  field :logo,            type: String
+  field :banner,          type: String
+  field :philosophy,      type: String
+  field :story,           type: String
+  field :ustid,           type: String
+  field :eroi,            type: String
+  field :sms,             type: Boolean,    default: false
+  field :sms_mobile,      type: String
+  field :min_value,       type: BigDecimal
 
-  has_one :bank_account
+  field :status,          type: Symbol,     default: :new
 
-  validates :name,    presence: true
-  validates :sms,     presence: true
-  validates :ustid,   presence: false,  length: { minimum: 25, maximum: 25 }
-  validates :story,   presence: false,  length: { minimum: 25, maximum: 25 }
-
-  has_many :products, inverse_of: :shop
-
-  mount_uploader :logo, AttachmentUploader
+  mount_uploader :logo,   AttachmentUploader
   mount_uploader :banner, AttachmentUploader
+
+  has_one   :bank_account
+  has_one   :address
+  has_many  :products,    inverse_of: :shop
+
+  validates :name,        presence: true
+  validates :sms,         presence: true
+  validates :ustid,       presence: false,  length: { minimum: 25, maximum: 25 }
+  validates :story,       presence: false,  length: { minimum: 25, maximum: 25 }
+  validates :sms_mobile,  presence: false,  :unless => lambda { self.sms }
+  validates :address,     presence: true
+  validates :status,      presence: true,   inclusion: {in: [:new, :opened, :closed]}
 end
