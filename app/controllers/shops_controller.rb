4b89a5966f2b8a60974f376a2a80c1bdf0dbc66b
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-03-30 16:16:10 +0200

    agb process

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -15,14 +15,18 @@ class ShopsController <  ApplicationController
        current_user.shop.target_groups.clear unless sp[:target_groups]
        current_user.shop.sales_channels.clear unless sp[:sales_channels]
 
-       if current_user.shop.update(sp)
+       if current_user.shop.agb && current_user.shop.update(sp)
          if params[:user_info_edit_part] == :edit_producer.to_s
            flash[:success] = I18n.t(:update_producer_ok, scope: :edit_shop)
          else
            flash[:success] = I18n.t(:update_ok, scope: :edit_shop)
          end
-        format.html { redirect_to edit_user_path(current_user, :user_info_edit_part => params[:user_info_edit_part]) }
-      else
+
+         format.html { redirect_to edit_user_path(current_user, :user_info_edit_part => params[:user_info_edit_part]) }
+       elsif (not current_user.shop.agb) && current_user.shop.update(sp)
+         flash[:success] = I18n.t(:update_agb_ok, scope: :edit_shop)
+         format.html { redirect_to edit_user_path(current_user, :user_info_edit_part => :edit_shop) }
+       else
         flash[:error] = current_user.shop.errors.full_messages.first
         format.html { redirect_to edit_user_path(current_user, :user_info_edit_part => params[:user_info_edit_part]) }
       end
