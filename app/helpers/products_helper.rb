7219ac1a9e9ca77d1d2ce8f7f616c5a757f2fbc0
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-04-03 10:58:40 +0200

    bugfix

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -194,4 +194,21 @@ module ProductsHelper
   def get_grouped_variants_options(product)
     get_grouped_variants_options_from_cache(product)
   end
+
+  def gen_check_product_details
+    %Q{
+      v = $('select.sku-variants-options');
+
+      if ( v.val() == 0 ) {
+        v.next().addClass('invalidBorderClass');
+        return false;
+      }
+
+      if ( $('img.img-responsive[src=""]').length > 3 ) {
+        return false;
+      }
+
+      return true;
+    }
+  end
 end
