695a5ee7f621e6295c019e8e241987546b2d01e3
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-04-19 00:10:32 +0200

    bugfix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@
   config.storage              = :qiniu
   config.qiniu_access_key     = 'sjmi6rq8r6Z7oO84m9WQ3grXZJNaDmBlHC5eDWsu'
   config.qiniu_secret_key     = 'Vp_6J0c4GZML4PESk5vMv6yVgTCYY7x4XtKt3JCj'
-  config.qiniu_bucket         = 'carrierwave-laiyinn-upload'
+  config.qiniu_bucket         = 'carrierwave-laiyinn-prod'
   config.qiniu_bucket_domain  = '7xskyn.com2.z0.glb.qiniucdn.com'
   config.qiniu_bucket_private = true
   config.qiniu_block_size     = 4*1024*1024
