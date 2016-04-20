1694abe0a8b2cc3e6f4e26ce6c96833daf371fe1
Hao Jiang <hao.jiang@gmail.com>
2016-04-20 09:51:12 +0200

    update qiuniu domain

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@
   config.qiniu_access_key     = 'sjmi6rq8r6Z7oO84m9WQ3grXZJNaDmBlHC5eDWsu'
   config.qiniu_secret_key     = 'Vp_6J0c4GZML4PESk5vMv6yVgTCYY7x4XtKt3JCj'
   config.qiniu_bucket         = 'carrierwave-laiyinn-prod'
-  config.qiniu_bucket_domain_test  = '7xskyn.com2.z0.glb.qiniucdn.com'
   config.qiniu_bucket_domain  = '7xt4mc.com1.z0.glb.clouddn.com'
   config.qiniu_bucket_private = true
   config.qiniu_block_size     = 4*1024*1024
