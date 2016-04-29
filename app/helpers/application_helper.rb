e66ff681035af04ae635bfa7ae88b915882bbf44
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-04-29 21:53:27 +0200

    move js to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,13 +34,43 @@ module ApplicationHelper
     }
   end
 
+  def generate_validate_img_file_js
+    %Q{
+      function validateImgFile(inputFile) {
+        var maxExceededMessage = "#{I18n.t(:max_exceeded_message, scope: :image_upload)}";
+        var extErrorMessage = "#{I18n.t(:ext_error_message, scope: :image_upload)}";;
+        var allowedExtension = ["jpg", "jpeg", "png"];
+
+        var extName;
+        var maxFileSize = 1048576;
+        var sizeExceeded = false;
+        var extError = false;
+
+        $.each(inputFile.files, function() {
+          if (this.size && maxFileSize && this.size > maxFileSize) {sizeExceeded=true;};
+          extName = this.name.split('.').pop();
+          if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
+        });
+        if (sizeExceeded) {
+          window.alert(maxExceededMessage);
+          $(inputFile).val('');
+        };
+
+        if (extError) {
+          window.alert(extErrorMessage);
+          $(inputFile).val('');
+        };
+      }
+    }
+  end
+
   def get_uploaded_thumbnail_url(img)
     img ? (img.url ? img.url(:thumb) : nil) : nil
   end
 
-  # renders a given haml block inside a layout
   def inside_layout(layout = 'application', &block)
     render :inline => capture_haml(&block), :layout => "layouts/#{layout}"
   end
+
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,2 @@
 module UsersHelper
-
-  def generate_set_datepicker_lang_js
-    %Q{
-      $( document ).ready( function () {
-        $('.date-picker').datepicker({ language: '#{I18n.locale}', autoclose: true, todayHighlight: true })
-      })
-    }
-  end
-
 end
