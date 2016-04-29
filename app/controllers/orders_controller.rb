bce28f228011f18bf2b638a41240a552f5405a14
Loschcode <bonjour@laurentschaffner.com>
2016-04-29 15:50:03 +0200

    payment gateway progress

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -126,15 +126,18 @@ class OrdersController < ApplicationController
     # This will be changed later on
     @wirecard = Wirecard.new({
 
+      :username => "engine.digpanda",
+      :password => "x3Zyr8MaY7TDxj6F",
+
       :merchant_id => "dfc3a296-3faf-4a1d-a075-f72f1b67dd2a",
       :secret_key => "6cbfa34e-91a7-421a-8dde-069fc0f5e0b8",
-      :username => "engine.digpanda",
-      :password => "x3Zyr8MaY7TDxj6F"
+
+      :amount => 1.01,
+      :currency => 'CNY',
+      :order_number => '123456',
+      :order_detail => '1 widget',
 
     })
-    
-    #@wirecard.pay(50)
-    @wirecard.digital_signature
 
   end
 
--- app/lib/wirecard.rb
+++ app/lib/wirecard.rb
@@ -3,32 +3,7 @@ class Wirecard
   require 'net/http'
   require 'digest'
 
-  attr_reader :merchant_id, :secret_key, :username, :password, :url
-
-=begin
-
-Request URL=https://sandbox-engine.thesolution.com/engine/hpp/
-Request Method=POST
-Status Code=302 Found Accept=text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8 Accept-Encoding=gzip, deflate
-Accept-Language=en-US,en;q=0.8 Content-Type=application/x-www-form-urlencoded Host=sandbox-engine.thesolution.com
-
-requested_amount:1.01
-requested_amount_currency:CNY
-locale:en
-order_number:123456
-order_detail:1 widget form_url:https://sandbox-engine.thesolution.com/engine/hpp/ request_id:ca077aff-b2e0-0e0b-3be2-08aa3b888b52 request_time_stamp:20160425212012 merchant_account_id:dfc3a296-3faf-4a1d-a075-f72f1b67dd2a payment_method:upop
-transaction_type:debit redirect_url:https://sandbox-engine.thesolution.com/shop/complete.jsp?state=success& request_signature:723e75bfe0b730282a754c054389c09043e67e2bd75f96ff64f4939ceeaa09f6
-psp_name:demo success_redirect_url:https://sandbox-engine.thesolution.com/shop/complete.jsp?state=success& fail_redirect_url:https://sandbox-engine.thesolution.com/shop/complete.jsp?state=failed& cancel_redirect_url:https://sandbox-engine.thesolution.com/shop/complete.jsp?state=cancel& processing_redirect_url:https://sandbox-engine.thesolution.com/shop/complete.jsp?state=processing& first_name:John
-last_name:Doe
-email:john.doe@wirecard.com
-phone:1 555 555 5555
-street1:123 test
-street2:
-city:Toronto
-state:ON
-postal_code:M4P1E8
-country:CA
-=end
+  attr_reader :merchant_id, :secret_key, :username, :password, :url, :amount, :currency, :order_number, :order_detail, :request_time_stamp, :request_id, :redirect_url
 
   # https://hostname/engine/rest/merchants/{merchant-account-id}/payments/{transaction-id}
   # https://sandbox-engine.thesolution.com/engine/rest/merchants/dfc3a296-3faf-4a1d-a075-f72f1b67dd2a/payments/af3864e1-0b2b-11e6-9e82-00163e64ea9f
@@ -39,30 +14,38 @@ country:CA
     @username ||= args[:username]
     @password ||= args[:password]
     @url = "https://sandbox-engine.thesolution.com/engine/hpp/"
+    @amount ||= args[:amount].to_s
+    @currency ||= args[:currency]
+    @order_number ||= args[:order_number]
+    @order_detail ||= args[:order_detail]
+    @request_time_stamp ||= unix_timestamp
+    @request_id ||= 'ca077aff-b2e0-0e0b-3be2-08aa3b888b52'
+    @redirect_url ||= "https://sandbox-engine.thesolution.com/shop/complete.jsp"
   end
 
   # TODO : Refacto and abstract later on.
   def hosted_payment_request_datas
-
+    
     {
 
-      :requested_amount => '1.01',
-      :request_amount_currency => 'CNY',
+      :requested_amount => amount,
+      :requested_amount_currency => currency,
       :locale => 'en',
-      :order_number => 123456,
-      :order_detail => '1 widget',
+      :order_number => order_number,
+      :order_detail => order_detail,
       :form_url => "https://sandbox-engine.thesolution.com/engine/hpp/",
-      :request_id => "ca077aff-b2e0-0e0b-3be2-08aa3b888b52",
-      :request_time_stamp => "20160425212012",
-      :merchant_account_id => "dfc3a296-3faf-4a1d-a075-f72f1b67dd2a",
+      :request_id => request_id,
+      :request_time_stamp => request_time_stamp,
+      :merchant_account_id => merchant_id,
       :payment_method => "upop",
       :transaction_type => "debit",
-      :redirect_url => "https://sandbox-engine.thesolution.com/shop/complete.jsp?state=success& request_signature:723e75bfe0b730282a754c054389c09043e67e2bd75f96ff64f4939ceeaa09f6",
+      :redirect_url => "#{redirect_url}?state=success&",
+      :request_signature => digital_signature,
       :psp_name => "demo",
-      :success_redirect_url => "https://sandbox-engine.thesolution.com/shop/complete.jsp?state=success&",
-      :fail_redirect_url => "https://sandbox-engine.thesolution.com/shop/complete.jsp?state=failed&",
-      :cancel_redirect_url => "https://sandbox-engine.thesolution.com/shop/complete.jsp?state=cancel&",
-      :processing_redirect_url => "https://sandbox-engine.thesolution.com/shop/complete.jsp?state=processing&",
+      :success_redirect_url => "#{redirect_url}?state=success&",
+      :fail_redirect_url => "#{redirect_url}?state=failed&",
+      :cancel_redirect_url => "#{redirect_url}?state=cancel&",
+      :processing_redirect_url => "#{redirect_url}?state=processing&",
       :first_name => "John",
       :last_name => "Doe",
       :email => "john.doe@wirecard.com",
@@ -78,21 +61,21 @@ country:CA
 
   end
 
-  def digital_signature(action='purchase', amount='1.01', currency='USD', redirect_url='')
+  def digital_signature
 
     signature = {
       # WARNING : the order of the hash matters
-      :request_time_stamp => unix_timestamp.to_s,
-      :request_id =>'order-random78979',
+      :request_time_stamp => request_time_stamp,
+      :request_id => request_id,
       :merchant_account_id => merchant_id,
-      :transaction_type => 'purchase',
+      :transaction_type => "debit",
       :requested_amount => amount,
       :request_amount_currency => currency,
-      :redirect_url => redirect_url,
+      :redirect_url => "#{redirect_url}?state=success&",
       :ip_address => '127.0.0.1',
       :secret_key => secret_key
     }
-
+    
     Digest::SHA256.hexdigest(signature.values.join)
 
 =begin
@@ -107,10 +90,12 @@ ip_address                  = '127.0.0.1'
 secret_key                  = 'efabf47b-e43b-4785-873f-1c5bc65b7cd2'
 =end
 
+# 20120430123012order-12345b19fb056-d8da-449b-ac85-cfbfd0558914purchase1.01USD127.0.0.1efabf47b-e43b-4785-873f-1c5bc65b7cd2
+
   end
 
   def unix_timestamp
-    Time.now.to_i
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
   end
 
 
