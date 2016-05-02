5df9c618077edc2fb0d7ae3cf334f070a961ad3d
Loschcode <bonjour@laurentschaffner.com>
2016-05-02 15:11:42 +0200

    routes deprecation changes + webhook doc

--- app/controllers/wirecard/webhook_controller.rb
+++ app/controllers/wirecard/webhook_controller.rb
@@ -2,6 +2,14 @@ class Wirecard::WebhookController < ApplicationController
 
   def merchant_status_change
 
+=begin
+  {
+  "merchant_id": "<id>",
+  "merchant_status": "<status>", //PROCESSING
+  "reseller_id": "<hash>"
+  }
+=end
+
     binding.pry
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -15,37 +15,37 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resource :page do
-    match :agb,         via: [:get],    to: :agb,         as: :agb
-    match :privacy,     via: [:get],    to: :privacy,     as: :privacy
-    match :imprint,     via: [:get],    to: :imprint,     as: :imprint
-    match :saleguide,   via: [:get],    to: :saleguide,   as: :saleguide
-    match :fees,        via: [:get],    to: :fees,        as: :fees
-    match :home,        via: [:get],    to: :home,        as: :home
+    match :agb,         via: [:get],    action: :agb,         as: :agb
+    match :privacy,     via: [:get],    action: :privacy,     as: :privacy
+    match :imprint,     via: [:get],    action: :imprint,     as: :imprint
+    match :saleguide,   via: [:get],    action: :saleguide,   as: :saleguide
+    match :fees,        via: [:get],    action: :fees,        as: :fees
+    match :home,        via: [:get],    action: :home,        as: :home
   end
 
   get '/set_session_locale/:locale', to: 'application#set_session_locale', as: 'set_session_locale'
 
   resources :collections, except: [:new] do
-    match 'remove_product/:product_id', via: [:patch],  to: :remove_product,        as: :remove_product_from,       :on => :member
-    match 'add_product/:product_id',    via: [:patch],  to: :add_product,           as: :add_product_to,            :on => :member
-    match :remove_all_products,         via: [:patch],  to: :remove_all_products,   as: :remove_all_products_from,  :on => :member
-    match :add_products,                via: [:patch],  to: :add_products,          as: :add_products_to,           :on => :member
-    match :remove_products,             via: [:patch],  to: :remove_products,       as: :remove_products_from,      :on => :member
-    match 'toggle_product/:product_id', via: [:get],    to: :toggle_product,        as: :toggle_product_in,         :on => :member
-    match :like_collection,             via: [:patch],  to: :like_collection,       as: :like,                      :on => :member
-    match :dislike_collection,          via: [:patch],  to: :dislike_collection,    as: :dislike,                   :on => :member
+    match 'remove_product/:product_id', via: [:patch],  action: :remove_product,        as: :remove_product_from,       :on => :member
+    match 'add_product/:product_id',    via: [:patch],  action: :add_product,           as: :add_product_to,            :on => :member
+    match :remove_all_products,         via: [:patch],  action: :remove_all_products,   as: :remove_all_products_from,  :on => :member
+    match :add_products,                via: [:patch],  action: :add_products,          as: :add_products_to,           :on => :member
+    match :remove_products,             via: [:patch],  action: :remove_products,       as: :remove_products_from,      :on => :member
+    match 'toggle_product/:product_id', via: [:get],    action: :toggle_product,        as: :toggle_product_in,         :on => :member
+    match :like_collection,             via: [:patch],  action: :like_collection,       as: :like,                      :on => :member
+    match :dislike_collection,          via: [:patch],  action: :dislike_collection,    as: :dislike,                   :on => :member
 
-    match 'is_collected/:product_id',         via: [:get],    to: :is_collected,            as: :is_collected,          :on => :collection
-    match :create_and_add,                    via: [:post],   to: :create_and_add,          as: :create_and_add,        :on => :collection
-    match 'search/:keyword',                  via: [:get],    to: :search,                  as: :search,                :on => :collection
-    match :show_my_collections,               via: [:get],    to: :show_my_collections,     as: :show_my,               :on => :collection
-    match :show_liked_by_me,                  via: [:get],    to: :show_liked_by_me,        as: :show_liked_by_me,      :on => :collection
-    match 'show_user_collections/:user_id',   via: [:get],    to: :show_user_collections,   as: :show_user,             :on => :collection
+    match 'is_collected/:product_id',         via: [:get],    action: :is_collected,            as: :is_collected,          :on => :collection
+    match :create_and_add,                    via: [:post],   action: :create_and_add,          as: :create_and_add,        :on => :collection
+    match 'search/:keyword',                  via: [:get],    action: :search,                  as: :search,                :on => :collection
+    match :show_my_collections,               via: [:get],    action: :show_my_collections,     as: :show_my,               :on => :collection
+    match :show_liked_by_me,                  via: [:get],    action: :show_liked_by_me,        as: :show_liked_by_me,      :on => :collection
+    match 'show_user_collections/:user_id',   via: [:get],    action: :show_user_collections,   as: :show_user,             :on => :collection
   end
 
   namespace :wirecard do
     namespace :webhook do 
-      post 'merchant_status_change', to: :merchant_status_change, as: :merchant_status_change
+      post 'merchant_status_change', action: :merchant_status_change, as: :merchant_status_change
     end
   end
 
@@ -53,67 +53,67 @@ Rails.application.routes.draw do
   end
 
   resources :products, except: [:index, :new] do
-    match 'remove_sku/:sku_id',                     via: [:delete], to: :remove_sku,                  as: :remove_sku,                  :on => :member
-    match 'remove_variant/:variant_id',             via: [:delete], to: :remove_variant,              as: :remove_variant,              :on => :member
-    match 'remove_option/:variant_id/:option_id',   via: [:delete], to: :remove_option,               as: :remove_option,               :on => :member
-    match :get_sku_for_options,                     via: [:get],    to: :get_sku_for_options,         as: :get_sku_for_options,         :on => :member
-    match :like,                                    via: [:patch],  to: :like,                        as: :like,                        :on => :member
-    match :dislike,                                 via: [:patch],  to: :dislike,                     as: :dislike,                     :on => :member
-    match :show_skus,                               via: [:get],    to: :show_skus,                   as: :show_skus,                   :on => :member
-    match :skus,                                    via: [:get],    to: :skus,                        as: :skus,                        :on => :member
-    match :new_sku,                                 via: [:get],    to: :new_sku,                     as: :new_sku,                     :on => :member
-    match :edit_sku,                                via: [:get],    to: :edit_sku,                    as: :edit_sku,                    :on => :member
-    match :clone_sku,                               via: [:get],    to: :clone_sku,                   as: :clone_sku,                   :on => :member
+    match 'remove_sku/:sku_id',                     via: [:delete], action: :remove_sku,                  as: :remove_sku,                  :on => :member
+    match 'remove_variant/:variant_id',             via: [:delete], action: :remove_variant,              as: :remove_variant,              :on => :member
+    match 'remove_option/:variant_id/:option_id',   via: [:delete], action: :remove_option,               as: :remove_option,               :on => :member
+    match :get_sku_for_options,                     via: [:get],    action: :get_sku_for_options,         as: :get_sku_for_options,         :on => :member
+    match :like,                                    via: [:patch],  action: :like,                        as: :like,                        :on => :member
+    match :dislike,                                 via: [:patch],  action: :dislike,                     as: :dislike,                     :on => :member
+    match :show_skus,                               via: [:get],    action: :show_skus,                   as: :show_skus,                   :on => :member
+    match :skus,                                    via: [:get],    action: :skus,                        as: :skus,                        :on => :member
+    match :new_sku,                                 via: [:get],    action: :new_sku,                     as: :new_sku,                     :on => :member
+    match :edit_sku,                                via: [:get],    action: :edit_sku,                    as: :edit_sku,                    :on => :member
+    match :clone_sku,                               via: [:get],    action: :clone_sku,                   as: :clone_sku,                   :on => :member
 
-    match :autocomplete_product_name,               via: [:get],    to: :autocomplete_product_name,   as: :autocomplete_product_name,   :on => :collection
-    match 'search',                                 via: [:get],    to: :search,                      as: :search,                      :on => :collection
-    match :popular,                                 via: [:get],    to: :popular,                     as: :popular,                                  :on => :collection
+    match :autocomplete_product_name,               via: [:get],    action: :autocomplete_product_name,   as: :autocomplete_product_name,   :on => :collection
+    match 'search',                                 via: [:get],    action: :search,                      as: :search,                      :on => :collection
+    match :popular,                                 via: [:get],    action: :popular,                     as: :popular,                                  :on => :collection
   end
 
   resources :users do
-    match 'search/:keyword',  via: [:get],    to: :search,            as: :search,              :on => :collection
+    match 'search/:keyword',  via: [:get],    action: :search,            as: :search,              :on => :collection
 
-    match :edit_account,      via: [:get],    to: :edit_account,      as: :edit_account,        :on => :member
-    match :edit_personal,     via: [:get],    to: :edit_personal,     as: :edit_personal,       :on => :member
-    match :edit_bank,         via: [:get],    to: :edit_bank,         as: :edit_bank,           :on => :member
-    match :follow,            via: [:patch],  to: :follow,            as: :follow,              :on => :member
-    match :unfollow,          via: [:patch],  to: :unfollow,          as: :unfollow,            :on => :member
-    match :get_followers,     via: [:get],    to: :get_followers,     as: :get_followers,       :on => :member
-    match :get_following,     via: [:get],    to: :get_following,     as: :get_following,       :on => :member
+    match :edit_account,      via: [:get],    action: :edit_account,      as: :edit_account,        :on => :member
+    match :edit_personal,     via: [:get],    action: :edit_personal,     as: :edit_personal,       :on => :member
+    match :edit_bank,         via: [:get],    action: :edit_bank,         as: :edit_bank,           :on => :member
+    match :follow,            via: [:patch],  action: :follow,            as: :follow,              :on => :member
+    match :unfollow,          via: [:patch],  action: :unfollow,          as: :unfollow,            :on => :member
+    match :get_followers,     via: [:get],    action: :get_followers,     as: :get_followers,       :on => :member
+    match :get_following,     via: [:get],    action: :get_following,     as: :get_following,       :on => :member
 
-    match :show_orders,       via: [:get],    to: :show_orders,       :controller => :orders,             as: :show_orders,         :on => :member
-    match :show_addresses,    via: [:get],    to: :show_addresses,    :controller => :addresses,          as: :show_addresses,      :on => :member
-    match :show_collections,  via: [:get],    to: :show_collections,  :controller => :collections,        as: :show_collections,    :on => :member
-    match :new_collection,    via: [:get],    to: :new,               :controller => :collections,        as: :new_collection,      :on => :member
-    match :edit_collection,   via: [:get],    to: :edit,              :controller => :collections,        as: :edit_collection,     :on => :member
+    match :show_orders,       via: [:get],    action: :show_orders,       :controller => :orders,             as: :show_orders,         :on => :member
+    match :show_addresses,    via: [:get],    action: :show_addresses,    :controller => :addresses,          as: :show_addresses,      :on => :member
+    match :show_collections,  via: [:get],    action: :show_collections,  :controller => :collections,        as: :show_collections,    :on => :member
+    match :new_collection,    via: [:get],    action: :new,               :controller => :collections,        as: :new_collection,      :on => :member
+    match :edit_collection,   via: [:get],    action: :edit,              :controller => :collections,        as: :edit_collection,     :on => :member
   end
 
   resources :shops, except: [:new, :edit, :create] do
-    match :edit_setting,    via: [:get],    to: :edit_setting,    as: :edit_setting,    :on => :member
-    match :edit_producer,   via: [:get],    to: :edit_producer,   as: :edit_producer,   :on => :member
-    match :show_products,          via: [:get],    to: :show_products,          as: :show_products,          :on => :member
-    match :apply_wirecard,   via: [:get],    to: :apply_wirecard,   as: :apply_wirecard,   :on => :member
+    match :edit_setting,    via: [:get],    action: :edit_setting,    as: :edit_setting,    :on => :member
+    match :edit_producer,   via: [:get],    action: :edit_producer,   as: :edit_producer,   :on => :member
+    match :show_products,          via: [:get],    action: :show_products,          as: :show_products,          :on => :member
+    match :apply_wirecard,   via: [:get],    action: :apply_wirecard,   as: :apply_wirecard,   :on => :member
 
     resources :products,    only: [:new]
   end
 
   resources :shop_applications, except: [:edit, :update] do
-    match :is_registered,     via: [:get],  to: :registered?,         as: :is_registered,       :on => :collection
+    match :is_registered,     via: [:get],  action: :registered?,         as: :is_registered,       :on => :collection
   end
 
   resources :orders, only: [:destroy, :show] do
-    match :add_product,               via: [:patch],        to: :add_product,             as: :add_product_to,              :on => :collection
-    match :adjust_products_amount,    via: [:patch],        to: :adjust_products_amount,  as: :adjust_products_amount_in,   :on => :collection
-    match :checkout,                  via: [:post],         to: :checkout,                as: :checkout,                    :on => :collection
-    match :manage_cart,               via: [:get],          to: :manage_cart,             as: :manage_cart,                 :on => :collection
-    match 'set_address/:shop_id/',    via: [:patch, :get],  to: :set_address,             as: :set_address,                 :on => :collection
+    match :add_product,               via: [:patch],        action: :add_product,             as: :add_product_to,              :on => :collection
+    match :adjust_products_amount,    via: [:patch],        action: :adjust_products_amount,  as: :adjust_products_amount_in,   :on => :collection
+    match :checkout,                  via: [:post],         action: :checkout,                as: :checkout,                    :on => :collection
+    match :manage_cart,               via: [:get],          action: :manage_cart,             as: :manage_cart,                 :on => :collection
+    match 'set_address/:shop_id/',    via: [:patch, :get],  action: :set_address,             as: :set_address,                 :on => :collection
 
-    match :continue,                  via: [:get],          to: :continue,                as: :continue,                    :on => :member
+    match :continue,                  via: [:get],          action: :continue,                as: :continue,                    :on => :member
   end
 
   resources :categories, only: [:show, :index] do
-    match :list_products,   via: [:get],    to: :list_products,               as: :list_products,     :on => :member
-    match :show_products,   via: [:get],    to: :show_products,               as: :show_products_in,  :on => :member
+    match :list_products,   via: [:get],    action: :list_products,               as: :list_products,     :on => :member
+    match :show_products,   via: [:get],    action: :show_products,               as: :show_products_in,  :on => :member
   end
 
 end
