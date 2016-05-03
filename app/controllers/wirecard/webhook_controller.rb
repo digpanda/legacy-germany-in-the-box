a334589be96de09baa37640eb58eab8ff7362910
Loschcode <bonjour@laurentschaffner.com>
2016-05-03 10:52:23 +0200

    gem file compression for readability + webhook comment help

--- app/controllers/wirecard/webhook_controller.rb
+++ app/controllers/wirecard/webhook_controller.rb
@@ -16,6 +16,25 @@ class Wirecard::WebhookController < ApplicationController
 
     devlog.info("Service received `#{merchant_id}`, `#{merchant_status}`, `#{reseller_id}`")
 
+=begin
+
+ 
+                    Array
+(
+    [merchant_id] => digpanda merchant id
+    [merchant_status] => ACTIVE
+    [reseller_id] => nba81H29Gba
+    [wirecard_credentials] => Array
+        (
+            [ee_user_cc] => engine.digpanda
+            [ee_password_cc] => x3Zyr8MaY7TDxj6F
+            [ee_secret_cc] => 6cbfa34e-91a7-421a-8dde-069fc0f5e0b8
+            [ee_maid_cc] => dfc3a296-3faf-4a1d-a075-f72f1b67dd2a
+        )
+)
+
+=end
+
   end
 
 end
