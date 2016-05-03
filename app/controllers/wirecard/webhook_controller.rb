d52c9ebdc9a985a48954e8cd9c08a52de45a30c8
Loschcode <bonjour@laurentschaffner.com>
2016-05-03 17:34:40 +0200

    wirecard_status change system

--- app/controllers/wirecard/webhook_controller.rb
+++ app/controllers/wirecard/webhook_controller.rb
@@ -16,6 +16,10 @@ class Wirecard::WebhookController < ApplicationController
 
     devlog.info("Service received `#{merchant_id}`, `#{merchant_status}`, `#{reseller_id}`")
 
+    shop = Shop.find(merchant_id)
+    shop.merchant_status = merchant_status
+    shop.save
+
 =begin
 
  
