aa2c29200df9feea642c3ce78b74cd8738fc2b80
Loschcode <bonjour@laurentschaffner.com>
2016-05-04 13:48:47 +0200

    json post added to webhook wirecard

--- app/controllers/wirecard/webhook_controller.rb
+++ app/controllers/wirecard/webhook_controller.rb
@@ -6,15 +6,19 @@ class Wirecard::WebhookController < ApplicationController
 
   before_action :validate_merchant_datas
 
-  # note : i made it fast, we should refacto and put some of this in libraries. - Laurent
+  attr_reader :datas
+  respond_to :json
+
+  # note : i made it fast, we should refacto and put some of this in libraries and organize it within an /api/ folder
+  # - Laurent
   def merchant_status_change
 
     devlog.info "Wirecard started to communicate with our system" 
 
-    merchant_id = params["merchant_id"]
-    merchant_status = params["merchant_status"]
-    reseller_id = params["reseller_id"]
-    wirecard_credentials = params["wirecard_credentials"]
+    merchant_id = datas["merchant_id"]
+    merchant_status = datas["merchant_status"]
+    reseller_id = datas["reseller_id"]
+    wirecard_credentials = datas["wirecard_credentials"]
     
     devlog.info "Service received `#{merchant_id}`, `#{merchant_status}`, `#{reseller_id}`" 
 
@@ -25,9 +29,10 @@ class Wirecard::WebhookController < ApplicationController
       devlog.info "It passed the authentication"
 
       shop = Shop.find(merchant_id)
-
       shop.wirecard_status = merchant_status.downcase
       shop.save
+
+      devlog.info "System is done."
 
       render text: "Ok" and return
 
@@ -40,10 +45,17 @@ class Wirecard::WebhookController < ApplicationController
   private
 
   def required_merchant_datas
-    !!(params["merchant_id"].present? && 
-       params["merchant_status"].present? && 
-       params["reseller_id"].present? &&
-       params["wirecard_credentials"].present? && Hash === params["wirecard_credentials"])
+
+    @datas = ActiveSupport::JSON.decode(request.body.read)
+
+    devlog.info "Checking parameters ..."
+    devlog.info "VARIABLES : #{params.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}}" 
+    devlog.info "JSON : #{datas.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}}"
+
+    !!(datas["merchant_id"].present? && 
+       datas["merchant_status"].present? && 
+       datas["reseller_id"].present? &&
+       datas["wirecard_credentials"].present? && Hash === datas["wirecard_credentials"])
   end
 
   def validate_merchant_datas
