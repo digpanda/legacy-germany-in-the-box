bea732db5ce27e0d6cec8cd25b141742af1b5ebf
Loschcode <bonjour@laurentschaffner.com>
2016-05-05 12:18:53 +0200

    minor refactoring sublayout menu

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,6 +1,14 @@
 module LayoutsHelper
 
-  def is_menu_active?(item)
+  def is_menu_active?(item,default=false)
+
+    if params[:user_info_edit_part].nil?
+      if default
+        return 'active'
+      else
+        return ''
+      end
+    end
 
     if Array === item
       item.map(&:to_s).include?(params[:user_info_edit_part]) ? 'active' : ''
