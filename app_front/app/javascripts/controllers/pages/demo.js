78daff1b94023e3fb49a5f5f75ba664f584f64aa
Loschcode <bonjour@laurentschaffner.com>
2016-05-09 15:05:22 +0200

    my account minor change

--- app_front/app/javascripts/controllers/pages/demo.js
+++ app_front/app/javascripts/controllers/pages/demo.js
@@ -23,18 +23,6 @@ var Demo = {
       "enableTouch": true
     });
 
-   $(".flexnav").flexNav({
-
-    'animationSpeed':     50,            // default for drop down animation speed
-    'transitionOpacity':  false,           // default for opacity animation
-    'buttonSelector':     '.menu-button', // default menu button class name
-    'hoverIntent':        false,          // Change to true for use with hoverIntent plugin
-    'hoverIntentTimeout': 150,            // hoverIntent default timeout
-    'calcItemWidths':     false,          // dynamically calcs top level nav item widths
-    'hover':              true            // would you like hover support?      
-    
-  });
-
   },
 
 }
