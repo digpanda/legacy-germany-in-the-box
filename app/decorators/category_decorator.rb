6a3634176cbab3b325ce0a6289530a56c20e6ec9
Loschcode <bonjour@laurentschaffner.com>
2016-05-12 10:12:01 +0200

    minor refacto home page

--- app/decorators/category_decorator.rb
+++ app/decorators/category_decorator.rb
@@ -1,23 +1,17 @@
 class CategoryDecorator < Draper::Decorator
 
-  include Imageable
-
   delegate_all
   decorates :category
 
   def draw_tree
 
-
-=begin
-    children.each_with_object({}) do |child, memo|
-      memo[:name] = child.children.any? ? child.decorate.draw_tree : {}
-    end
-=end
-
     children.inject("") do |memo, child| # We should refacto this code and put the HTML in a view
 
+      #binding.pry
+      #yield(child)
+
       memo << "<li class=\"dir\"><a href=\"#\">#{child.name}</a>"
-      memo << "<ul>#{child.decorate.draw_tree}</ul>" if not child.children.empty?
+      memo << "<ul>#{child.decorate.draw_tree}</ul>" if child.children.any?
       memo << "</li>"
 
     end.html_safe
