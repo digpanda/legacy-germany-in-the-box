b849a17ae0bf0e49e59894ea6058501c257b670b
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-05-13 00:05:24 +0200

    borderguru

--- spec/lib/borderguru/payloads/shipping_api_test.rb
+++ spec/lib/borderguru/payloads/shipping_api_test.rb
@@ -36,9 +36,70 @@ describe BorderGuru::Payloads::ShippingApi do
   end
 
   it 'adds order value to the payload' do
-    # two different order line items with an ordered
-    # quantity of 2, each
-    assert_equal BigDecimal.new(1 * 1 * 11), @payload.to_h[:subtotal]
+    assert_equal BigDecimal.new(2 * 1 * 11), @payload.to_h[:subtotal]
   end
+
+  it 'adds order weight to the payload' do
+    assert_equal 2 * 1 * 0.1, @payload.to_h[:totalWeight]
+    assert_equal 'kg', @payload.to_h[:totalWeightScale]
+  end
+
+  it "adds shop's country of dispatcher to payload" do
+    assert_equal 'DE', @payload.to_h[:countryOfOrigin]
+  end
+
+  it "adds shop's currency to payload" do
+    assert_equal 'EUR', @payload.to_h[:currency]
+  end
+
+  it 'adds country of destination to payload' do
+    assert_equal 'CN', @payload.to_h[:countryOfDestination]
+  end
+
+  it "adds the shop's name to payload" do
+    assert_equal 'Seppls Lederhosenladen', @payload.to_h[:storeName]
+  end
+
+  it 'adds a previously obtained quote ID to payload' do
+    assert_equal 'BG-DE-CN-01234567898', @payload.to_h[:quoteIdentifier]
+  end
+
+
+  it 'adds the proper shipping address' do
+    shipping_address = @payload.to_h[:shippingAddress].first
+    {
+        firstName:          '薇',
+        lastName:           '李',
+        streetName:         '和平区华江里',
+        additionalInfo:     '309室',
+        postcode:           '300222',
+        city:               '天津',
+        country:            'China',
+        telephone:          '+86123456',
+        email:              'customer01@hotmail.com',
+        countryCode:        'CN'
+    }.each do |key, expected|
+      assert_equal expected, shipping_address[key]
+    end
+  end
+
+  it 'adds the proper billing address' do
+    billing_address = @payload.to_h[:billingAddress].first
+    {
+        firstName:          '薇',
+        lastName:           '李',
+        streetName:         '和平区华江里',
+        additionalInfo:     '309室',
+        postcode:           '300222',
+        city:               '天津',
+        country:            'China',
+        telephone:          '+86123456',
+        email:              'customer01@hotmail.com',
+        countryCode:        'CN'
+    }.each do |key, expected|
+      assert_equal expected, billing_address[key]
+    end
+  end
+
 end
 
