677c81657824e0d935a7a4d17927e66cb579f07c
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-05-13 00:12:10 +0200

    borderguru

---
+++ spec/lib/borderguru/payloads/tracking_api_test.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'border_guru/payloads/tracking_api'
+
+describe BorderGuru::Payloads::TrackingApi do
+
+  # before do
+  #   @payload = BorderGuru::Payloads::TrackingApi.new(
+  #       order: create(:order),
+  #       dispatcher: create(:shop).dispatcher
+  #   )
+  # end
+  #
+  # it 'adds the merchant order id to payload' do
+  #   assert 'Order0123456', @payload.to_h[:merchantOrderId]
+  # end
+  #
+  # it 'adds the dispatchers location to payload' do
+  #   assert_equal 'Hamburg, Germany', @payload.to_h[:trackingLocation]
+  # end
+  #
+  # it "adds the order's weight" do
+  #   assert_equal 6.0, @payload.to_h[:trackingWeight]
+  # end
+  #
+  # it "adds the order's weight scale" do
+  #   assert_equal 'kg', @payload.to_h[:trackingWeightScale]
+  # end
+
+end
+
