590c2ac63a2c7d2a3791bf6741f97ad83f92d82e
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-05-13 15:29:17 +0200

    borderguru

--- spec/lib/borderguru/payloads/cancel_order_api_test.rb
+++ spec/lib/borderguru/payloads/cancel_order_api_test.rb
@@ -1,18 +1,16 @@
-require 'test_helper'
+require 'rails_helper'
 require 'border_guru/payloads/cancel_order_api'
 
-class BorderGuru::Payloads::CancelOrderApiTest < ActiveSupport::TestCase
+describe BorderGuru::Payloads::CancelOrderApi do
 
-  test 'adds the shipment id to payload' do
-    assert_equal '061039676', payload.to_h[:shipmentIdentifier]
+  before do
+    @payload = BorderGuru::Payloads::CancelOrderApi.new(
+        border_guru_shipment_id: '061039676'
+    )
   end
 
-  private
-
-  def payload
-    BorderGuru::Payloads::CancelOrderApi.new(
-      border_guru_shipment_id: '061039676'
-    )
+  it 'adds the shipment id to payload' do
+    assert_equal '061039676', @payload.to_h[:shipmentIdentifier]
   end
 
 end
