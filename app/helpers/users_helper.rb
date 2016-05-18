a382bbfdd6b3789e3b370e39c9d7c62f5bac76ee
Loschcode <bonjour@laurentschaffner.com>
2016-05-18 11:26:51 +0200

    seems shopkeeper / seems customer changes done

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,11 @@
 module UsersHelper
 
   def seems_like_a_customer?
-    current_user.nil? || current_user.is_customer?
+    (current_user.nil? && is_chinese?) || current_user&.is_customer?
+  end
+
+  def seems_like_a_shopkeeper?
+    (current_user.nil? && is_german) || current_user&.is_shopkeeper?
   end
 
   def is_german?
