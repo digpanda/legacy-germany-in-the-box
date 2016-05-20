84be18620ba26c7863c7101fabe2107a2a0a4934
Loschcode <bonjour@laurentschaffner.com>
2016-05-20 15:20:57 +0200

    emailing system

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,29 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.middleware.use ExceptionNotification::Rack,
+  :email => {
+    :email_prefix => "Report - ",
+    :sender_address => %{"Bug DigPanda Production" <notifier@digpanda.com>},
+    :exception_recipients => %w{laurent.schaffner@digpanda.com, jiang@digpanda.com}
+  }
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = {host: 'germanyinthebox.com', port: 80}
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: 'gmail.com',
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: 'germanyinthebox@gmail.com',
+      password: 'pandarocks'
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
