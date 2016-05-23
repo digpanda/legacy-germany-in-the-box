1937291fe3512a855f719814e2e075622427195b
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-05-24 00:06:14 +0200

    setting exchange rate

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -7,4 +7,17 @@ class Admin::SettingsController < ApplicationController
     render 'users/admin/edit_setting'
   end
 
+  def update
+    Settings.instance.update(settings_params)
+    Settings.instance.save
+    redirect_to request.referer
+  end
+
+  private
+
+  def settings_params
+    delocalize_config = { settings: { :exchange_rate_to_yuan => :number} }
+    params.require('/admin/settings').permit(:exchange_rate_to_yuan).delocalize(delocalize_config)
+  end
+
 end
--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,16 +1,21 @@
-require 'singleton'
-
 class Settings
-  include Singleton
   include MongoidBase
 
   field :exchange_rate_to_yuan, type: BigDecimal, default: 8
 
   validates :exchange_rate_to_yuan,   presence: true,   :numericality => { :greater_than => 0 }
 
-  def initialize(*args)
-    @settings = super(*args)
-    self.save!
-    @settings
+  field :singleton_guard, type: Integer
+  validates_inclusion_of :singleton_guard, :in => [0]
+
+  def self.instance
+    unless single = where(:id => 1).first
+      single = Settings.new()
+      single.id = 1
+      single.singleton_guard = 0
+      single.save!
+    end
+
+    single
   end
 end
