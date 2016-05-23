c2ac2e4fb06ae3c9deab64649e6be14451ccb16f
Loschcode <bonjour@laurentschaffner.com>
2016-05-23 20:37:51 +0200

    test execute for staging brunch compile

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,6 +49,10 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
+
+      # brunch
+      execute "cd /var/www/germany_in_the_box/current/app_front | brunch build --production"
+
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
