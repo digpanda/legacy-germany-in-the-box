c2dd13f368774042218061b0bcc5fcf15ee870dc
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-05-23 21:19:06 +0200

    remove garbage

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,8 +1,6 @@
 class Setting
   include MongoidBase
 
-  acts_as_singleton
-
   field :exchange_rate_to_yuan, type: BigDecimal, default: 8
 
   validates :exchange_rate_to_yuan,   presence: true,   :numericality => { :greater_than => 0 }
