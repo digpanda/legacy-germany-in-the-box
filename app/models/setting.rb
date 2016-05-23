5ccc41472e1715853537917ccdaeb7ed9f03d6f2
Loschcode <bonjour@laurentschaffner.com>
2016-05-23 21:04:21 +0200

    bugfix staging

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,7 +1,7 @@
 class Setting
   include MongoidBase
 
-  acts_as_singleton
+  # acts_as_singleton <-- this was blowing up the staging environment so i commented it, my bad i shouldn't have merge the code without your approval - Laurent
 
   field :exchange_rate_to_yuan, type: BigDecimal, default: 8
 
