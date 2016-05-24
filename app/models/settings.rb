c4ba8049ee6891616a9abbcd881b7f0deadd6344
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-05-24 10:16:51 +0200

    bugfix

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -4,6 +4,8 @@ class Settings
   include Singleton
   include MongoidBase
 
+  field :exchange_rate_to_yuan, type: BigDecimal, default: 8
+
   @@instance ||= Settings.first_or_create()
 
   private_class_method :create
