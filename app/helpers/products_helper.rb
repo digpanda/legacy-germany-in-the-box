c9d3f68f06b926c1b07e4d7caa1f87293da4d89b
Loschcode <bonjour@laurentschaffner.com>
2016-06-01 23:44:21 +0200

    minor bug fixes on old collection system

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,55 +1,6 @@
 module ProductsHelper
   include AppCache
 
-  def generate_toggle_product_in_collection_js(collection_id, product_id)
-    %Q{
-      $.ajax({
-          dataType: 'json',
-          url: '#{toggle_product_in_collection_path(collection_id, product_id)}',
-          async: true,
-          success: function(json) {
-            checkboxes = $("[id^=collection_checkbox]")
-            flag = false
-            for (i=0; i<checkboxes.length; ++i) {
-              if (checkboxes[i].checked ) {
-                flag = true
-                break
-              }
-            }
-
-            if (flag) {
-              $('#dropdown_toggle_#{product_id}').addClass('btn-success')
-              $('#dropdown_toggle_#{product_id}').removeClass('btn-primary')
-            } else {
-              $('#dropdown_toggle_#{product_id}').addClass('btn-primary')
-              $('#dropdown_toggle_#{product_id}').removeClass('btn-success')
-            }
-          }
-     });
-    }
-  end
-
-  def generate_create_and_add_to_collection_js(product_id)
-    %Q{
-      $('#create_and_add_to_collection_form_input_name_#{product_id}').each(function() {
-        if($(this).val()){
-          $.ajax({
-            type: 'POST',
-            url: '#{create_and_add_collections_path}',
-            data: $('#create_and_add_to_collection_form_#{product_id}').serialize(),
-            success: function(json) {
-              $('#create_and_add_to_collection_dropdown_button_#{product_id}').click()
-            }
-          });
-        } else {
-          $(this).css("border","1px solid red");
-        }
-      });
-
-      return false;
-    }
-  end
-
   def gen_remove_variant_panel
     %Q{
       function () {
