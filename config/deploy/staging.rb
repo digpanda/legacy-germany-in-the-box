0a6a1278163e3f5c3a8dc2bad0ddf649faf225d0
Hao Jiang <hao.jiang@gmail.com>
2016-06-03 02:57:17 +0200

    merge dev into staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -12,7 +12,7 @@ set :nvm_map_bins, %w{node npm}
 #set :nvm_custom_path, '/home/ubuntu/.nvm/bin'
 
 set :ssh_options, {
-  keys: %w(../private/staging/digpanda.pem),
+  keys: %w(/Users/haojiang/Downloads/keys/digpanda.pem),
   forward_agent: true,
   auth_methods: %w(publickey, password)
 }
