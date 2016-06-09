78ef8495cca486d547980606d38fefb6520ba626
Loschcode <bonjour@laurentschaffner.com>
2016-06-09 11:36:20 +0200

    migration added

---
+++ db/migrate/20160609093341_new_categories.rb
@@ -0,0 +1,35 @@
+class NewCategories < Mongoid::Migration
+  def self.up
+
+    Category.all.delete
+
+    #
+    # root category - level 0
+    #
+    category_food_drinks = Category.create!(
+        :name_translations => {:en => 'Food', :'zh-CN' => '食品和饮料', :de => 'Lebensmittel  & Getränke'},
+        :slug => 'food'
+    )
+
+    category_medicine_care = Category.create!(
+        :name_translations => {:en => 'Medicine', :'zh-CN' => '药品保健', :de => 'Gesundheit & Medizin'},
+        :slug => 'medicine'
+    )
+
+    category_cosmetics_care = Category.create!(
+        :name_translations => {:en => 'Cosmetics', :'zh-CN' => '美妆', :de => 'Kosmetik & Pflege'},
+        :slug => 'cosmetics'
+    )
+
+    category_fashion = Category.create!(
+        :name_translations => {:en => 'Fashion', :'zh-CN' => '时尚', :de => 'Mode'},
+        :slug => 'fashion'
+    )
+
+    Rails.cache.clear
+
+  end
+
+  def self.down
+  end
+end
