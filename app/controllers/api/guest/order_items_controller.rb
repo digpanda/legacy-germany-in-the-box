e733dcb79f71a1c219ed4fe2395884a9c5720d32
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-06-16 13:46:08 +0200

    bugfix for + and - quantity

--- app/controllers/api/guest/order_items_controller.rb
+++ app/controllers/api/guest/order_items_controller.rb
@@ -6,21 +6,21 @@ class Api::Guest::OrderItemsController < Guest::OrderItemsController
   def set_quantity
 
     product = @order_item.product
-    skus = product.skus.find(@order_item.sku_id)
+    sku = product.skus.find(@order_item.sku_id)
     quantity = params[:quantity].to_i
     order = @order_item.order
 
-    new_total = skus.price * quantity * Settings.first.exchange_rate_to_yuan
+    new_total = sku.price * quantity * Settings.first.exchange_rate_to_yuan
 
-    if reach_todays_limit?(order, new_total)
+    if @order_item.quantity < quantity && reach_todays_limit?(order, new_total)
       render :json => { :success => false, :error => I18n.t(:override_maximal_total, scope: :edit_order, total: Settings.instance.max_total_per_day, currency: Settings.instance.platform_currency.symbol) }
       return
     end
 
-    if skus.unlimited or skus.quantity >= quantity
+    if sku.unlimited or sku.quantity >= quantity
       @order_item.quantity = quantity
     else
-      render :json => { :success => false, :error => I18n.t(:not_all_available, scope: :checkout, :product_name => product.name, :option_names => skus.decorate.get_options_txt) }
+      render :json => { :success => false, :error => I18n.t(:not_all_available, scope: :checkout, :product_name => product.name, :option_names => sku.decorate.get_options_txt) }
     end
 
     if @order_item.save
