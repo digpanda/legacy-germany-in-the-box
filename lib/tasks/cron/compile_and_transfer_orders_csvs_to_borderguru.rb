b5f8d4cfa0300c654dbfd253d8695c34485f4031
Loschcode <bonjour@laurentschaffner.com>
2016-06-16 21:47:06 +0200

    production protection and such

--- lib/tasks/cron/compile_and_transfer_orders_csvs_to_borderguru.rb
+++ lib/tasks/cron/compile_and_transfer_orders_csvs_to_borderguru.rb
@@ -58,7 +58,11 @@ class CompileAndTransferOrdersCsvsToBorderguru
     # We transfer the information to BorderGuru
     # We could avoid opening the file twice but it's a double process.
     #
-    files_pushed = PushCsvsToBorderguruFtp.new.perform
+    if ::Rails.env.production?
+      files_pushed = PushCsvsToBorderguruFtp.new.perform
+    else
+      devlog "We can't push files to BorderGuru FTP. We are not in production environment."
+    end
 
     unless files_pushed[:success]
       devlog "A problem occured while transfering the files to BorderGuru (#{files_pushed[:error]})."
