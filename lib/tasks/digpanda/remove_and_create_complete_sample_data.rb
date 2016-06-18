d782cc3f3f90ca90652a1bd96cf53da6f5f9a2ca
Loschcode <bonjour@laurentschaffner.com>
2016-06-18 21:27:55 +0200

    binding pry removed

--- lib/tasks/digpanda/remove_and_create_complete_sample_data.rb
+++ lib/tasks/digpanda/remove_and_create_complete_sample_data.rb
@@ -49,7 +49,6 @@ class RemoveAndCreateCompleteSampleData
   private
 
   def convert_product_without_first_sku_left(product)
-    binding.pry
     puts "We convert #{product.name} to a `without_first_sku_left`"
     product.name += " NO FIRST SKU"
     product.desc = "The price of the first Sku is #{product.skus.first.price} and shouldn't appear."
