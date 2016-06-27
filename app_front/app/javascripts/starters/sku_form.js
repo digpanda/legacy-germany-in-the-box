61ef7996e0766226b32956897726f69a0bdcf584
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-06-27 17:06:22 +0200

    bugfix

--- app_front/app/javascripts/starters/sku_form.js
+++ app_front/app/javascripts/starters/sku_form.js
@@ -18,9 +18,9 @@ var SkuForm = { // CURRENTLY NOT IN USED IN THE SYSTEM
             $('input[id^=product_skus_attributes_][id$=_unlimited]').change(
                 function () {
                     if ( $(this).is(":checked") ) {
-                        $('input[id^=product_skus_attributes_][id$=quantity]').val(0).prop('disabled', 'true')
+                        $('input[id^=product_skus_attributes_][id$=quantity]').val(0).prop('disabled', 'true').parent().hide();
                     } else {
-                        $('input[id^=product_skus_attributes_][id$=quantity]').removeAttr('disabled')
+                        $('input[id^=product_skus_attributes_][id$=quantity]').removeAttr('disabled').parent().show()
                     }
                 }
             )
