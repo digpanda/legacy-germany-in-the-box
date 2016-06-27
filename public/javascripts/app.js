eecd590ed28b9f44e6ba2d11402c728fb29d3ca3
Loschcode <bonjour@laurentschaffner.com>
2016-06-27 17:22:53 +0200

    small refacto avatar mixin

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1686,9 +1686,9 @@ var SkuForm = { // CURRENTLY NOT IN USED IN THE SYSTEM
         if ($("#js-sku-form").length > 0) {
             $('input[id^=product_skus_attributes_][id$=_unlimited]').change(function () {
                 if ($(this).is(":checked")) {
-                    $('input[id^=product_skus_attributes_][id$=quantity]').val(0).prop('disabled', 'true');
+                    $('input[id^=product_skus_attributes_][id$=quantity]').val(0).prop('disabled', 'true').parent().hide();
                 } else {
-                    $('input[id^=product_skus_attributes_][id$=quantity]').removeAttr('disabled');
+                    $('input[id^=product_skus_attributes_][id$=quantity]').removeAttr('disabled').parent().show();
                 }
             });
         }
