b2aa7285bfaa7b5545684b97e08390f605d742bd
Loschcode <bonjour@laurentschaffner.com>
2016-06-30 16:23:47 +0200

    workday minor change

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -1,3 +1,3 @@
 # BorderGuru business time
-BusinessTime::Config.beginning_of_workday = "8:00 am"
+BusinessTime::Config.beginning_of_workday = "10:00 am"
 BusinessTime::Config.end_of_workday = "12:00 pm"
