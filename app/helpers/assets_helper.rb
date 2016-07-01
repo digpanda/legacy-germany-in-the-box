828f10e075f0f4d30e43b42d1b0a6d0d5f856197
Loschcode <bonjour@laurentschaffner.com>
2016-07-01 14:52:33 +0200

    minor bugfix asset versioning

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -2,12 +2,14 @@ require 'digest/md5'
 
 module AssetsHelper
 
+  CSS_APP_PATH = "public/stylesheets/app.css"
+
   def assets_version_hash
-    Digest::MD5.hexdigest(assets_version)
+    Digest::MD5.hexdigest(assets_version.to_s)
   end
 
   def assets_version
-    File.mtime(Rails.root.join("public/assets/stylesheets/app.css")) # we don't need to check anything else because we use brunch and everything is recompiled systematically
+    File.mtime(Rails.root.join(CSS_APP_PATH)) # we don't need to check anything else because we use brunch and everything is recompiled systematically
   end
 
 end
