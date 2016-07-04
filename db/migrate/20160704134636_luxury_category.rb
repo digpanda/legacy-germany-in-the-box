12aa2bd863f17304d3b0c8de6932ddb73360b35a
Loschcode <bonjour@laurentschaffner.com>
2016-07-04 15:59:14 +0200

    luxury category added

---
+++ db/migrate/20160704134636_luxury_category.rb
@@ -0,0 +1,13 @@
+class LuxuryCategory < Mongoid::Migration
+  def self.up
+
+    Category.create!(
+      :name_translations => {:en => 'Luxury', :'zh-CN' => '奢侈品', :de => 'Luxus'},
+      :slug => 'luxury'
+    )
+
+  end
+
+  def self.down
+  end
+end
