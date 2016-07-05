1062e695ae986068e76a59e5050cc4f0801b3168
idlesummerbreeze <idlesummerbreeze@gmail.com>
2016-07-06 01:17:06 +0200

    speed up creating duty categories

--- lib/tasks/digpanda/remove_and_create_duty_categories.rb
+++ lib/tasks/digpanda/remove_and_create_duty_categories.rb
@@ -2,6 +2,7 @@ require 'csv'
 
 class Tasks::Digpanda::RemoveAndCreateDutyCategories
 
+  #Todo: can also to create categories in a recursive way, but for the man who do it later.
   def initialize
 
     puts "We clear the file cache"
@@ -13,23 +14,53 @@ class Tasks::Digpanda::RemoveAndCreateDutyCategories
     csv_file = File.join(Rails.root, 'vendor', 'bg-duty-categories.csv')
     puts "We fetch the appropriate CSV file"
 
+    level1 = {}
+    level2 = {}
+    level3 = {}
+
     CSV.foreach(csv_file, quote_char: '"', col_sep: ';', row_sep: :auto, headers: false) do |row|
 
       if row[2].blank?
-        DutyCategory.create!(:code => row[0].strip, :name_translations => {:en => row[1].strip} )
+
+        l1_name = row[1].strip
+        l1 = DutyCategory.create!(:code => row[0].strip, :name_translations => {:en => l1_name} )
+
+        level1[l1_name]=l1
+
       else
 
         if row[3].blank?
-          parent = DutyCategory.where(:'name.en' => row[1].strip).first
-          DutyCategory.create!(:code => row[0].strip, :name_translations => {:en => row[2].strip}, :parent => parent )
+
+          l1_name = row[1].strip
+          l1 = level1[l1_name]
+
+          l2_name = row[2].strip
+          l2 = DutyCategory.create!(:code => row[0].strip, :name_translations => {:en => l2_name }, :parent => l1 )
+
+          level2[[l1_name, l2_name]] = l2
+
         else
           
           if row[4].blank?
-            parent = DutyCategory.where(:'name.en' => row[2].strip).to_a.detect { |c| c.parent.name_translations[:en] == row[1].strip }
-            DutyCategory.create!(:code => row[0].strip, :name_translations => {:en => row[3].strip}, :parent => parent )
+
+            l1_name = row[1].strip
+            l2_name = row[2].strip
+            l2 = level2[[l1_name, l2_name]]
+
+            l3_name = row[3].strip
+            l3 = DutyCategory.create!(:code => row[0].strip, :name_translations => {:en => l3_name }, :parent => l2 )
+
+            level3[[l1_name, l2_name, l3_name]] = l3
           else
-            parent = DutyCategory.where(:'name.en' => row[3].strip).to_a.select { |c| c.parent.name_translations[:en] == row[2].strip }.detect { |c| c.parent.name_translations[:en] == row[1].strip }
-            DutyCategory.create!(:code => row[0].strip, :name_translations => {:en => row[4].strip}, :parent => parent )
+
+            l1_name = row[1].strip
+            l2_name = row[2].strip
+            l3_name = row[3].strip
+            l3 = level3[[l1_name, l2_name, l3_name]]
+
+            l4_name = row[4].strip
+            DutyCategory.create!(:code => row[0].strip, :name_translations => {:en => l4_name}, :parent => l3 )
+
           end
 
         end
