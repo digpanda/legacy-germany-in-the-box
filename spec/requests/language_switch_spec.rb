65a511ca47a0e096444d107958b69c20fa3f43b1
Loschcode <bonjour@laurentschaffner.com>
2016-07-14 10:12:31 +0200

    testing sign-in redirection on language switch

--- spec/requests/language_switch_spec.rb
+++ spec/requests/language_switch_spec.rb
@@ -1,14 +1,11 @@
-describe "Language switch", :type => :request  do
+describe "language#update process", :type => :request  do
 
-  context "admin on the website" do
+  context "logged-in as admin" do
 
     let(:current_user) { FactoryGirl.create(:admin) }
-    before(:each) { 
-      post user_session_path, :user => { :email => current_user.email, :password => '12345678' } # because it's a request, inter-controller system
-      #login_customer current_user
-    }
+    before(:each) { post user_session_path, :user => { :email => current_user.email, :password => '12345678' } } # because it's a request, inter-controller system
 
-    it "set the language to german from a specific page" do
+    it "sets the language to german from the admin shop page" do
 
       get shops_path
       expect(response).to have_http_status(:ok)
@@ -19,4 +16,17 @@ describe "Language switch", :type => :request  do
     end
 
   end
+
+  context "as guest" do
+
+    it "sets the language to german and relocate automatically to the sign-in" do
+
+      patch language_path("de", location: new_user_session_path)
+      expect(session[:locale]).to eq("de")
+      expect(response).to redirect_to new_user_session_path
+      
+    end
+
+  end
+
 end
