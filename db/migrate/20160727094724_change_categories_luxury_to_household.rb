cb96d22e5e57afbae18890cede9d375cca0a5a1d
Loschcode <bonjour@laurentschaffner.com>
2016-07-27 11:53:21 +0200

    luxury to household

---
+++ db/migrate/20160727094724_change_categories_luxury_to_household.rb
@@ -0,0 +1,14 @@
+class ChangeCategoriesLuxuryToHousehold < Mongoid::Migration
+  def self.up
+
+    category = Category.where(slug: 'luxury').first
+    if category
+      category.slug = 'household'
+      category.name_translations = {:en => 'Household', :'zh-CN' => '家居', :de => 'Haushalt'}
+      category.save!
+    end
+  end
+
+  def self.down
+  end
+end
