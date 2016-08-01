75dc47efbadf9b23a64232f6a536e060de6b49e6
Loschcode <bonjour@laurentschaffner.com>
2016-08-01 15:26:50 +0200

    model changes again

--- app/models/duty_category.rb
+++ app/models/duty_category.rb
@@ -9,9 +9,4 @@ class DutyCategory
 
   validates :code,  presence: true, :unless => lambda { self.parent.blank? }, length: {maximum: Rails.configuration.max_tiny_text_length}
 
-=begin
-  def second_last_branche?
-    AppCache.get_second_last_duty_category_branches_from_cache.include?(self.id)
-  end
-=end
 end
