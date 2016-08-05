1e3fe9ba1149d474f8b2475ced041d7e0ded2570
Loschcode <bonjour@laurentschaffner.com>
2016-08-05 15:14:38 +0200

    fix again

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -2,7 +2,7 @@ module ShopsHelper
 
   def get_allowed_locales
     if current_user&.decorate&.admin?
-      %W(zh-CN, de).map(&:to_sym)
+      %w(zh-CN de).map(&:to_sym)
     else
       [I18n.locale]
     end
