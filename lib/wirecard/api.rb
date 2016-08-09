7db5ae5aa9de2966f1afb9388414fbe0ce5214dd
Loschcode <bonjour@laurentschaffner.com>
2016-08-09 19:15:30 +0200

    light optimization

--- lib/wirecard/api.rb
+++ lib/wirecard/api.rb
@@ -1,12 +1,8 @@
-require 'net/http'
-
-# this library is currently not tested
-# be careful
 module Wirecard
   class Api < Base
 
     class << self
-      
+
       def transaction(merchant_id, transaction_id)
         Transaction.new(merchant_id, transaction_id)
       end
--- lib/wirecard/api/transaction.rb
+++ lib/wirecard/api/transaction.rb
@@ -1,5 +1,3 @@
-require 'net/http'
-
 module Wirecard
   class Api
     class Transaction
