072b5a398316df8f63ac62b268d7161b9372e9d8
Loschcode <laurent.schaffner.code@gmail.com>
2016-08-25 12:47:56 +0200

    sign out issue solving + some translations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -37,7 +37,10 @@ class SessionsController < Devise::SessionsController
   end
 
   def destroy
-    super
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
   end
 
   def cancel_login
