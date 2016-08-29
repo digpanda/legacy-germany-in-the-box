622682f0851ebcc3249c9b3f3b8259d972351a95
Loschcode <laurent.schaffner.code@gmail.com>
2016-08-29 11:32:46 +0200

    binding.pry removal

--- lib/border_guru_email/transmit_orders/update.rb
+++ lib/border_guru_email/transmit_orders/update.rb
@@ -5,7 +5,6 @@ module BorderGuruEmail
       # update the order model accordingly
       def confirm_sent!
         orders.each do |order|
-          binding.pry
           order.hermes_pickup_email_sent_at = Time.now
           order.save
         end
