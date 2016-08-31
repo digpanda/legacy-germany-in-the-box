eddd31c9e4fe1d05353dd60996b4672fe8a144af
Loschcode <laurent.schaffner.code@gmail.com>
2016-08-31 10:38:36 +0200

    preparation to transaction id manual reset

--- app/controllers/admin/order_payments_controller.rb
+++ app/controllers/admin/order_payments_controller.rb
@@ -18,6 +18,7 @@ class Admin::OrderPaymentsController < ApplicationController
   # when the order payment is stuck on `scheduled` you can manually update the transaction_id
   def transaction_id
 
+    # TODO: PUT IT BACK
     #if order_payment.transaction_id
     #  flash[:error] = "Transaction ID is already present for this payment."
     #  redirect_to navigation.back(1)
@@ -25,7 +26,7 @@ class Admin::OrderPaymentsController < ApplicationController
     #end
 
     order_payment.transaction_id = params["transaction_id"]
-    order_payment.status = :unverified
+    order_payment.status = :scheduled # :unverified TODO : PUT IT BACK
     order_payment.save
     flash[:success] = "Transaction ID was set manually."
 
