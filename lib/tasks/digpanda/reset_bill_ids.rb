5af5e9cc22f4cb7641adaaf3afd19dfaf9a44220
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-01 15:58:02 +0200

    change billing system reset bill ids rake task

--- lib/tasks/digpanda/reset_bill_ids.rb
+++ lib/tasks/digpanda/reset_bill_ids.rb
@@ -1,7 +1,24 @@
 class Tasks::Digpanda::ResetBillIds
 
   def initialize
+    reset_all_bill_ids!
+    assign_bill_ids!
+  end
 
+  private
+
+  def reset_all_bill_ids!
+    # we first reset all the bill ids to 0 to start with
+    # we skip the callbacks to remove it without any auto make bill id trigger
+    Order.skip_callback(:save, :after, :make_bill_id)
+    Order.all.each do |order|
+      order.bill_id = nil
+      order.save
+    end
+    Order.set_callback(:save, :after, :make_bill_id)
+  end
+
+  def assign_bill_ids!
     Order.all.each do |order|
       puts "Working on `#{order.id}` from `#{order.c_at}` with status `#{order.status}`"
       order.bill_id = nil
@@ -10,7 +27,6 @@ class Tasks::Digpanda::ResetBillIds
       order.save
       puts "Final bill id : #{order.bill_id}"
     end
-
   end
 
 end
