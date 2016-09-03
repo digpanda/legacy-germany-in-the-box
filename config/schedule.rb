e57d837dc1dcb29e842be63ceed8f78342143efb
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-03 12:54:23 +0200

    change UTC hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,19 +20,19 @@
 # Learn more: http://github.com/javan/whenever
 set :output, "/var/www/germany_in_the_box/current/log/borderguru_cron.log"
 
-# We are currently in German Time
-every :day, :at => '9:15pm' do
+# We are currently in UTC time
+every :day, :at => '7:15am' do
   command 'The system has launched the rake task `cron:compile_and_transfer_orders_csvs_to_borderguru`'
   rake "cron:compile_and_transfer_orders_csvs_to_borderguru"
 end
 
-every :day, :at => '10:15pm' do
+every :day, :at => '8:15am' do
   command 'The system has launched the rake task `cron:transmit_pickup_orders_to_hermes`'
   rake "cron:transmit_pickup_orders_to_hermes"
 end
 
 every :hour do
-  command 'THe system has launched the rake task `cron:get_missing_shippings_from_border_guru`'
+  command 'The system has launched the rake task `cron:get_missing_shippings_from_border_guru`'
   rake "cron:get_missing_shippings_from_border_guru"
 end
 
