5e94b3c4c4a196d3bae7f110e7d97e8159f4087d
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-07 17:10:13 +0300

    highlight menu completely refactored and cleaned

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,21 +1,11 @@
 module LayoutsHelper
 
-  def is_menu_active?(item,default=false)
-
-    if params[:user_info_edit_part].nil?
-      if default
-        return 'active'
-      else
-        return ''
-      end
-    end
-
-    if Array === item
-      item.map(&:to_s).include?(params[:user_info_edit_part]) ? 'active' : ''
+  def menu_active?(path)
+    if url_for.index(path) == 0
+      "active"
     else
-      params[:user_info_edit_part] ? (params[:user_info_edit_part] == item.to_s ? 'active' : '') : 'active'
+      ""
     end
-
   end
 
 end
