b8c0185e8a2fd1ec69f5c0d2badabc28ff90eaba
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-08 16:35:38 +0300

    credentials bitbucket change

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'germany_in_the_box'
-set :repo_url, 'https://digpanda:achat2cool@bitbucket.org/digpanda/achat-backend-main.git'
+set :repo_url, 'https://digpanda:gitb2cool@bitbucket.org/digpanda/achat-backend-main.git'
 
 set :passenger_environment_variables, { :path => '/usr/bin:$PATH' }
 set :passenger_restart_command, '/usr/bin/passenger-config restart-app'
