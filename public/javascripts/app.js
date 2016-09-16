61b83c6729a00a137aeac36c45da71ee1b648460
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-16 12:50:41 +0200

    mascottes added on different sides + changed / improved on photoshop

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1128,7 +1128,7 @@ var PostForm = {
     document.body.appendChild(form); // <- JS way
     // $('body').append(form); // <- jQuery way
     //console.log(form);
-    //form.submit();
+    form.submit();
   }
 
 };
