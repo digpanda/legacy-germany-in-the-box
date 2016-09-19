a4aa1a97ce91909a013f0a3d265cecd2049fdfd8
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-19 10:26:35 +0200

    bugfix responsive system on homepage

--- app/models/order.rb
+++ app/models/order.rb
@@ -31,7 +31,7 @@ class Order
   scope :nonempty,    ->  {  where( :order_items_count.gt => 0 ) }
   scope :bought,      ->  { self.in( :status => [:paid, :custom_checkable, :custom_checking, :shipped] ) }
   scope :bought_or_cancelled, -> { self.in( :status => [:paid, :custom_checkable, :custom_checking, :shipped, :cancelled] ) }
-  scope :bought_or_unverified,      ->  { self.in( :status => [:payment_unverified, :paid, :custom_checkable, :custom_checking, :shipped] ) }
+  scope :bought_or_unverified,      ->  { self.in( :status => [:payment_unverified, :paid, :custom_checkable, :custom_checking, :shipped] ) } # cancelled isn't included in this
 
   # :new -> didn't try to pay
   # :paying -> is inside the process of payment
