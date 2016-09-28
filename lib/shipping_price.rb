6adf0a6d9569d00ebbeb00de1cc8f72eb927e878
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-28 17:24:45 +0200

    comments on new shipping price system

--- lib/shipping_price.rb
+++ lib/shipping_price.rb
@@ -1,4 +1,6 @@
  #  small library to guess the shipping prices via calculations
+ #  it takes the first prices into consideration and couple them with the < weight to match
+ #  if it goes more than the last `special kilo price` segment, it simply use the casual price per kilo
 class ShippingPrice
 
   VOLUMETRIC_DIVIDOR = 5000.00
