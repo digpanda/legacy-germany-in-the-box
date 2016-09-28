bebce3a819dbe9b5894c15a8e0b86846c8135c6f
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-28 14:12:41 +0200

    minor bug fixes + refacto partial shops

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -35,14 +35,14 @@ class Admin::ShopsController < ApplicationController
   end
 
   def destroy
-    if shop.addresses.delete_all && shop.destroy
+    if shop.addresses.delete_all && shop.payment_gateways.delete_all && shop.destroy
       flash[:success] = I18n.t(:delete_ok, scope: :edit_shops)
     else
       flash[:error] = @shop_application.errors.full_messages.first
     end
     redirect_to navigation.back(1)
   end
-  
+
   private
 
   def set_shop
