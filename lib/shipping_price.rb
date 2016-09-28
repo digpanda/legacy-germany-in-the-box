dd3edd34a0845fe9c15563423b65d93cf7296eb2
Loschcode <laurent.schaffner.code@gmail.com>
2016-09-28 17:21:26 +0200

    system shipping price changed to the new one

--- lib/shipping_price.rb
+++ lib/shipping_price.rb
@@ -3,14 +3,17 @@ class ShippingPrice
 
   VOLUMETRIC_DIVIDOR = 5000.00
 
-  FIRST_KILO_PRICE = 8.8
-  PRICE_PER_KILO = 2.3
+  FIRST_PRICES = [5.19, 9.19, 10.49, 11.79]
+  FIRST_PRICES_KILOS_SEGMENTS = [0.5, 1.0, 1.5, 2.0]
+
+  PRICE_PER_KILO = 2.5
 
   VAT_PERCENT = 19.00 # %
   APPROXIMATION_PERCENT = 20.00 # %
 
-  # add `to_b` functionality to floats / fixnum
+  # add `to_b`, `ceil_tp` functionality to floats / fixnum
   Float.include CoreExtensions::Float::BigDecimalConverter
+  Float.include CoreExtensions::Float::CeilTo
   Fixnum.include CoreExtensions::Fixnum::BigDecimalConverter
 
   attr_reader :order
@@ -23,17 +26,27 @@ class ShippingPrice
     (price_without_vat * vat).to_f.round(2)
   end
 
+  def below_two_kilos_price
+    FIRST_PRICES_KILOS_SEGMENTS.each_with_index do |segment, index|
+      if rounded_volumetric_weight < segment
+        return FIRST_PRICES[index]
+      end
+    end
+    return FIRST_PRICES.last
+  end
+
   def price_without_vat
-    (FIRST_KILO_PRICE.to_b + other_kilos * PRICE_PER_KILO.to_b)
+    binding.pry
+    (below_two_kilos_price.to_b + other_kilos * PRICE_PER_KILO.to_b)
   end
 
   private
 
   def other_kilos
-    if rounded_volumetric_weight >= 1
-      rounded_volumetric_weight - 1
+    if rounded_volumetric_weight > FIRST_PRICES_KILOS_SEGMENTS.last
+      rounded_volumetric_weight - FIRST_PRICES_KILOS_SEGMENTS.last
     else
-      0
+      0.0
     end
   end
 
@@ -42,7 +55,8 @@ class ShippingPrice
   end
 
   def rounded_volumetric_weight
-    @rounded_volumetric_weight ||= (products_volumetric_weight * approximation).ceil
+    # i'm not sure about the to_f before which was added
+    @rounded_volumetric_weight ||= (products_volumetric_weight * approximation).to_f.ceil_to(1)
   end
 
   def vat
