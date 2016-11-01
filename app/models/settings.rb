07dae374bb8316caea23b9bd4af3547fdb71c110
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-01 14:19:01 +0100

    dynamic announcement

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -7,7 +7,8 @@ class Settings
   field :exchange_rate_to_yuan, type: BigDecimal, default: 7.6
   field :platform_currency,     type: ISO4217::Currency, default: ISO4217::Currency.from_code('CNY')
   field :supplier_currency,     type: ISO4217::Currency, default: ISO4217::Currency.from_code('EUR')
-  field :max_total_per_day,    type: BigDecimal, default: 1000
+  field :max_total_per_day,     type: BigDecimal, default: 1000
+  field :highlight_title,       type: String
 
   validates :platform_currency,       presence: true
   validates :supplier_currency,       presence: true
@@ -17,7 +18,7 @@ class Settings
   def self.instance
     @rate ||= Settings.first_or_create()
   end
-  
+
   private_class_method :create
 
 end
