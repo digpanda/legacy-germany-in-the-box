4b430224a75b4c58fea12745d138795a538aaa5e
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-01 14:33:59 +0100

    change redirection shops system path

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -15,6 +15,10 @@ class ShopsController <  ApplicationController
   # def index
   #   @shops = Shop.in(shopkeeper: User.where(role: 'shopkeeper').map { |u| u.id} ).all;
   # end
+  #
+  def show
+    redirect_to guest_shop_path(shop)
+  end
 
   def edit_setting
   end
@@ -24,9 +28,6 @@ class ShopsController <  ApplicationController
   end
 
   def show_products
-  end
-
-  def show
   end
 
   def update
