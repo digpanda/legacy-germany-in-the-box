4cd2fbdb1afe3eb62e6dd06adc7679a9a1f4706e
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-03 19:41:38 +0100

    address binded to the yuan limit, in progress

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -23,6 +23,32 @@ class UserDecorator < Draper::Decorator
     @today_paid_orders_total_price ||= today_paid_orders.inject(0) { |sum, order| sum += order.decorate.total_price_in_yuan }
   end
 
+  # def reach_todays_limit?(order, new_price_increase)
+  #   if today_paid_orders(order).size == 0
+  #     false
+  #   else
+  #    (today_paid_orders_total_price(order) + order.decorate.total_price_in_yuan + new_price_increase) > Settings.instance.max_total_per_day
+  #   end
+  # end
+  #
+  # def today_paid_orders(order)
+  #   @today_paid_orders ||= begin
+  #     orders.bought.where(:paid_at.gte => Date.today).each.inject([]) do |memo, current_order|
+  #       if order.shipping_address == current_order.shipping_address
+  #         memo << order
+  #       end
+  #     end || []
+  #   end
+  # end
+  #
+  # def today_paid_orders_total_price(order)
+  #   @today_paid_orders_total_price ||= begin
+  #     today_paid_orders(order).inject(0) do |sum, current_order|
+  #       sum += current_order.decorate.total_price_in_yuan
+  #     end
+  #   end
+  # end
+
   def full_name
     "#{fname} #{lname}"
   end
