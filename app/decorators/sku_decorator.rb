482909b3ef5c450d8e137c05dc46edeac5338c77
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-03 16:44:38 +0100

    memoization of fees and smart api call

--- app/decorators/sku_decorator.rb
+++ app/decorators/sku_decorator.rb
@@ -51,23 +51,23 @@ class SkuDecorator < Draper::Decorator
   end
 
   def price_with_currency_yuan
-    price.in_euro.to_yuan.display
+    price_with_fees.in_euro.to_yuan.display
   end
 
   def price_with_currency_yuan_html
-    price.in_euro.to_yuan.display_html
+    price_with_fees.in_euro.to_yuan.display_html
   end
 
   def price_in_yuan
-    price.in_euro.to_yuan.amount
+    price_with_fees.in_euro.to_yuan.amount
   end
 
   def price_with_currency_euro
-    price.in_euro.display
+    price_with_fees.in_euro.display
   end
 
   def price_with_currency_euro_html
-    price.in_euro.display_html
+    price_with_fees.in_euro.display_html
   end
 
   def price_before_discount_in_yuan
@@ -87,7 +87,7 @@ class SkuDecorator < Draper::Decorator
   end
 
   def before_discount_price
-    price * 100 / (100 - discount)
+    price_with_fees * 100 / (100 - discount)
   end
 
   def discount_with_percent
