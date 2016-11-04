a59eb52b51e4ed9322ec2f1add5e2954a8475e5d
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-04 17:41:08 +0100

    big adjustment on border guru api, to test

--- lib/border_guru/payloads/concerns/having_vendibles.rb
+++ lib/border_guru/payloads/concerns/having_vendibles.rb
@@ -9,7 +9,7 @@ module BorderGuru
       # concern which provides #total_value and #total_weight.
       def product_summaries(order)
         {
-          subtotal: order.total_price_with_discount.round(2), #order.total_value,
+          subtotal: order.total_price_with_discount,
           totalWeight: order.total_weight,
           dimensionalWeight: order.total_dimensional_weight,
           dimensionalWeightScale: WEIGHT_UNIT,
@@ -17,19 +17,30 @@ module BorderGuru
         }
       end
 
-      # we will transmit those informations for each single order item to borderguru
-      # this line is currently used in the system - Laurent 2016/07/15
+      def adjusted_price(order)
+        order.order_items.inject(0) do |acc, current_order_item|
+          acc = acc + current_order_item.price_with_coupon_applied.round(2)
+        end
+      end
+
+      # the fact we use discount can make a few cents difference
+      # which have to be compensated to the last order item if needed
+      # to stay exact through the API
+      def subtotal_difference(order)
+        order.total_price_with_discount.round(2) - adjusted_price(order)
+      end
+
       def line_items(order_items)
-        order_items.map do |order_item|
+        order_items.map.each_with_index do |order_item, index|
           {
             sku: order_item.sku.id,
             shortDescription: order_item.sku.product.name,
-            price: order_item.price_with_coupon_applied.to_f.round(2),
+            price: (index == 0 ? (order_item.price_with_coupon_applied.round(2) + subtotal_difference(order_item.order)) : order_item.price_with_coupon_applied.round(2)),
             category: Rails.env.production? ? order_item.sku.product.duty_category.code : 'test',
             weight: order_item.weight,
             weightScale: WEIGHT_UNIT,
             countryOfManufacture: order_item.sku.product.shop.sender_address.country.alpha2
-          }.merge yield order_item
+          }.merge yield(order_item)
         end
       end
     end
