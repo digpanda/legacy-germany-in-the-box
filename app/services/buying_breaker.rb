a01e9190d37a836f762010f99e2b614a92f58509
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-04 11:57:18 +0100

    BuyingBreaker kind of done

--- app/services/buying_breaker.rb
+++ app/services/buying_breaker.rb
@@ -4,13 +4,61 @@
 # - the user select an address matching with an ID which has already spent too much today
 class BuyingBreaker < BaseService
 
-  def initialize
+  BUYING_LIMIT = Settings.instance.max_total_per_day # 1000
+
+  attr_reader :order
+
+  def initialize(order)
+    @order = order
   end
 
-  def reach_order_limit?(order)
+  # we check if he reached the limit
+  # on adding up one item in this specific order
+  def per_order?(order_item)
+    if (order_item.price + order.decorate.total_price_in_yuan) > BUYING_LIMIT
+      true
+    else
+      false
+    end
   end
 
-  def reach_customer_limit?(order)
+  # we check if he reached the limit
+  # after he selected an address
+  # NOTE : the address must be a shipping_address
+  # the comparison is made on the recipient of the package
+  def per_address?(address)
+    # TODO : we need to abstract this into another class (obviously, Address)
+    if address_today_paid(address) > BUYING_LIMIT
+      true
+    else
+      false
+    end
   end
-  
+
+  private
+
+  def address_today_paid(address)
+    address_today_orders(address).inject(0) do |sum, current_order|
+      sum += current_order.decorate.total_price_in_yuan
+    end
+  end
+
+  def address_today_orders(address)
+    @address_today_orders ||= begin
+      today_paid_orders.each.inject([]) do |memo, current_order|
+        if address == current_order.shipping_address
+          memo << order
+        end
+      end || []
+    end
+  end
+
+  def orders
+    @orders ||= order.user.orders
+  end
+
+  def today_paid_orders
+    @today_paid_orders ||= orders.bought.where(:paid_at.gte => Date.today)
+  end
+
 end
