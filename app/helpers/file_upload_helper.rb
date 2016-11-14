a9149fa1d4037e1fcd383864c00ea43ae6061e49
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-14 17:29:04 +0100

    multiple debug on sku image destroyer system

--- app/helpers/file_upload_helper.rb
+++ app/helpers/file_upload_helper.rb
@@ -1,7 +1,7 @@
 module FileUploadHelper
 
-  def js_file_upload(form:, field:)
-    render partial: "shared/partials/file_upload", :locals => {form: form, field: field}
+  def js_file_upload(form:, field:, destroy_image_path:)
+    render partial: "shared/partials/file_upload", :locals => {form: form, field: field, destroy_image_path: destroy_image_path}
   end
 
 end
