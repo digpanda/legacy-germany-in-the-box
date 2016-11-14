5298c78c196a849d5b87a90e7f09424b2ef6cb89
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-14 17:49:16 +0100

    multiple changes and refactoring, sku pretty much done

--- app/helpers/file_upload_helper.rb
+++ app/helpers/file_upload_helper.rb
@@ -1,7 +1,19 @@
 module FileUploadHelper
 
-  def js_file_upload(form:, field:, destroy_image_path:)
-    render partial: "shared/partials/file_upload", :locals => {form: form, field: field, destroy_image_path: destroy_image_path}
+  def js_file_upload(form:, field:)
+    render partial: "shared/partials/file_upload", :locals => {form: form, field: field}
+  end
+
+  # this helper is solely used in the file_upload partial
+  # to guess what path to use to destroy any image
+  # depending on the form class
+  def guess_destroy_image_path(form, field)
+    case form.object
+    when Sku
+      shopkeeper_product_sku_destroy_image_path(form.object.product, form.object, image_field: field)
+    when Shop
+      destroy_image_shopkeeper_shop_path(f.object, image_field: field)
+    end
   end
 
 end
