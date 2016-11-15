489f3a19dc9724f0b0e6886c399451e3acb6fb2f
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-15 12:33:27 +0100

    some more minor changes

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -1,4 +1,8 @@
 module TipsHelper
+
+  # add small tips anywhere in the system
+  # we pass through helpers and then partial
+  # to make it short and flexible within the view
   def js_tips(locals={})
     render partial: "shared/partials/tips", :locals => locals
   end
