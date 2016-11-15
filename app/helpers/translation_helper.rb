3f8037d36f8b4a910855a77ed40345358769fe58
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-16 00:31:18 +0100

    minor tweak comment

--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -9,7 +9,7 @@ module TranslationHelper
   end
 
   # we use a homemade class to communicate the instructions and turn it
-  # into an improved text_field, text_area while keeping the HTML outside.
+  # into an improved text_field, text_area while keeping the HTML inside the view
   def translate_fields(form, field, &block)
     FieldsTranslator.translate_fields(form, field, &block)
   end
