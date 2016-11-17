8ce94bf91c17d0aa47c97427b2c26259298f230c
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-17 16:57:08 +0100

    minor refactoring and finishing

--- config/routes/app/connect.rb
+++ config/routes/app/connect.rb
@@ -1,4 +1,10 @@
 # Connection related
+namespace :connect do
+
+end
+
+# the devise routing system must be kept outisde the namespace
+# otherwise it does not understand what model to use (`connect_user`)
 devise_for :users, :path => 'connect/', :controllers => {
 
   # we have to repeat the namespace
@@ -9,33 +15,3 @@ devise_for :users, :path => 'connect/', :controllers => {
   omniauth_callbacks: "connect/omniauth_callbacks"
 
 }
-namespace :connect do
-
-
-  #devise_for :users #, :path => 'users/' #, :skip => :all
-
-  # devise_for :users, :controllers => {
-  #
-  #   # we have to repeat the namespace
-  #   # because devise does not understand it
-  #   registrations: "connect/registrations",
-  #   sessions: "connect/sessions",
-  #   passwords: "connect/passwords",
-  #   omniauth_callbacks: "connect/omniauth_callbacks"
-  #
-  # }
-
-  #
-  # devise_scope :user do
-  #  resources :sessions
-  #  resources :registrations
-  #  resources :passwords
-  #  resources :omniauth_callbacks
-  # end
-
-  # it doesn't seem to be in used at all within the system.
-  # devise_scope :user do
-  #   # match 'users/sign_out', via: [:delete],   to: 'sessions#destroy',             as: :signout
-  # end
-
-end
