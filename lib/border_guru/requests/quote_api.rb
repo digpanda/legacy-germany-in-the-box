c54958a65b42fccb56da2c83abb6bd626c9fad73
Loschcode <laurent.schaffner.code@gmail.com>
2016-11-25 16:59:58 +0100

    cache added to borderguru quote api

--- lib/border_guru/requests/quote_api.rb
+++ lib/border_guru/requests/quote_api.rb
@@ -1,16 +1,27 @@
 require_relative 'base'
+require 'api_cache'
 
 module BorderGuru
   module Requests
     class QuoteApi < Base
 
       def dispatch!
-        @raw_response = @@access_token.post(
-          "/api/quotes/calculate?quoteParams=#{quote_params}",
+        @raw_response = APICache.get(request_url, :fail => []) do
+          api_call!
+        end
+        nil
+      end
+
+      def request_url
+        @request_url ||= "/api/quotes/calculate?quoteParams=#{quote_params}"
+      end
+
+      def api_call!
+        @@access_token.post(
+          request_url,
           nil,
           "Content-Type" => 'application/x-www-form-urlencoded'
         )
-        nil
       end
 
     end
