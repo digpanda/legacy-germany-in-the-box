ecb4671876630690cb4a9b8c1df35acc2c18b68a
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-06 12:45:39 +0100

    change migration again

--- db/migrate/20161202080105_order_item_sku_id_to_embedded_sku.rb
+++ db/migrate/20161202080105_order_item_sku_id_to_embedded_sku.rb
@@ -22,7 +22,7 @@ class OrderItemSkuIdToEmbeddedSku < Mongoid::Migration
           order_item.delete
           puts "Removed successfully."
         else
-          puts "Won't be applied to `#{order_item.id}` from order `#{order_item.order.id}` you may need to delete this corrupted order."
+          puts "Won't be applied to `#{order_item.id}` from order `#{order_item.order.id}` you may need to delete this corrupted order. It will be corrected on further migrations."
         end
       end
     end
--- db/migrate/20161206104717_change_corrupted_sku.rb
+++ db/migrate/20161206104717_change_corrupted_sku.rb
@@ -5,39 +5,43 @@ class ChangeCorruptedSku < Mongoid::Migration
     OrderItem.all.each do |order_item|
       unless order_item.product.skus.where(id: order_item.sku_id).first
         puts "SkuId `#{order_item.sku_id}` seems unknown but we will try to proceed anyway ..."
+
         if order_item.sku_id.nil?
           puts "SkuId is nil, we forget about this entry."
           next
         end
 
-        original_sku = nil
-        # dirty, yes i know.
-        Product.all.each do |product|
-          if product.skus.where(id: order_item.sku_id).first
-            original_sku = product.skus.where(id: order_item.sku_id).first
+        # let's try to find this through
+        # all the products possible
+        original_sku = begin
+            Product.all.each do |product|
+            if product.skus.where(id: order_item.sku_id).first
+              product.skus.where(id: order_item.sku_id).first
+            end
           end
         end
 
+        # since we could not find the exact sku we replace it
+        # by the first one from the product, this shouldn't alter the system too much.
+        # and it's only a few orders.
         if original_sku.nil?
-          # since we could not find the exact sku we replace it
-          # by the first one from the product, this shouldn't alter the system too much.
-          # and it's only a few orders.
-          original_sku = order_item.order.product.skus.first
-          puts "Could not find original Sku. Aborting."
-          next
-        else
-          new_sku = original_sku.clone
-          order_item.sku = new_sku
-          order_item.sku_origin = original_sku
-          # changes which could have been important through the time
-          order_item.sku.weight = order_item.weight
-          order_item.sku.price = order_item.price
-          order_item.sku.option_ids = order_item.option_ids
-          order_item.sku.save
-          # end of change through the time
-          order_item.save
-          puts "Proceeded."
+          original_sku = order_item.product.skus.first
+          puts "Could not find original Sku. We select the first one from the Product."
         end
+
+        # REPLICA OF PREVIOUS MIGRATION
+        new_sku = original_sku.clone
+        order_item.sku = new_sku
+        order_item.sku_origin = original_sku
+        # changes which could have been important through the time
+        order_item.sku.weight = order_item.weight
+        order_item.sku.price = order_item.price
+        order_item.sku.option_ids = order_item.option_ids
+        order_item.sku.save
+        # end of change through the time
+        order_item.save
+        puts "Proceeded."
+
       end
     end
   end
