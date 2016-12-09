979ef25d3ae730659678c4d3137103ec0fc4aa5d
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-09 17:37:23 +0100

    started the conversion

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,29 +6,29 @@ RSpec.configure do |config|
   end
 end
 
-require "selenium/webdriver"
-
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-
-  args = []
-  args << "--window-size=1400,1000" # window size
-
-  Capybara::Selenium::Driver.new(app, :browser => :chrome, :args => args)
-
-end
-
-# Capybara.app_host =  "http://local.dev:3000"
 Capybara.app_host = "http://local.dev:3333"
 Capybara.always_include_port = true
 Capybara.default_host = "http://local.dev:3333"
-#Capybara.default_host
 Capybara.server_port = 3333
 Capybara.server_host = "local.dev"
-
-#Capybara.run_server = false
-Capybara.current_driver = :selenium
+# Capybara.run_server = false
 Capybara.default_max_wait_time = 30
+
+# require "selenium/webdriver"
+# Capybara.current_driver = :selenium
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+#
+#   args = []
+#   args << "--window-size=1400,1000" # window size
+#
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome, :args => args)
+# end
+
+require 'capybara/poltergeist'
+Capybara.default_driver = :poltergeist
+Capybara.current_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
 
 # we turn VCR off by default because we don't want to use it systematically
 VCR.turn_off!
