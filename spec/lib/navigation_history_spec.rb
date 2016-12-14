7346e044f947db4a1e1a95508ca00f8ecd1d3e6b
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-14 11:44:45 +0100

    fixing the navigation history tests

--- spec/lib/navigation_history_spec.rb
+++ spec/lib/navigation_history_spec.rb
@@ -4,22 +4,6 @@ describe NavigationHistory do
 
   context "#store" do
 
-=begin
-    it "should not store if it is not a GET request" do
-
-      request = double(:get? => false)
-      expect(NavigationHistory.new(request, {}).store).to eql(false)
-
-    end
-=end
-=begin DOESNT EXIST ANYMORE
-    it "should not store if it is an AJAX call" do
-
-      request = double(:get? => true, :xhr? => true)
-      expect(NavigationHistory.new(request, {}).store).to eql(false)
-
-    end
-=end
     it "should store the successive paths and return the correct one" do
 
       request = double(:get? => true, :xhr? => false, :path => 'ONE_PATH', :fullpath => 'ONE_FULL_PATH')
@@ -34,7 +18,7 @@ describe NavigationHistory do
     it "should go back to the previous URL" do
 
       request = double(:get? => true, :xhr? => false)
-      session = {:previous_urls => ['1', '2', '3', '4', '5']}
+      session = {:previous_urls => {:default => ['1', '2', '3', '4', '5']}}
 
       nav = NavigationHistory.new(request, session)
       expect(nav.back(1)).to eql('1')
@@ -44,7 +28,7 @@ describe NavigationHistory do
     it "should go back to several URLs before it" do
 
       request = double(:get? => true, :xhr? => false)
-      session = {:previous_urls => ['1', '2', '3', '4', '5']}
+      session = {:previous_urls => {:default => ['1', '2', '3', '4', '5']}}
 
       nav = NavigationHistory.new(request, session)
       expect(nav.back(3)).to eql('3')
@@ -54,7 +38,7 @@ describe NavigationHistory do
     it "should not find the URL back so redirect to root_url" do
 
       request = double(:get? => true, :xhr? => false)
-      session = {:previous_urls => ['1', '2', '3', '4', '5']}
+      session = {:previous_urls => {:default => ['1', '2', '3', '4', '5']}}
 
       nav = NavigationHistory.new(request, session)
       expect(nav.back(10)).to eql(root_url)
