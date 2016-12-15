c065f89fa7809b9e96e2fbd3adf98d8a24657781
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-15 12:37:00 +0100

    back to normal for wirecard checkout

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1305,7 +1305,7 @@ var PostForm = {
     document.body.appendChild(form); // <- JS way
     // $('body').append(form); // <- jQuery way
     //console.log(form);
-    //form.submit();
+    form.submit();
   }
 
 };
