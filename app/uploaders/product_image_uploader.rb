2a564e734d3cc936a278455c27ebe459d8094da9
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-16 15:17:05 +0100

    some change on fitting the images on upload (carrierwave)

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -6,7 +6,7 @@ class ProductImageUploader < CarrierWave::Uploader::Base
 
   self.qiniu_can_overwrite = true
   self.qiniu_protocal = 'https'
-  
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -19,23 +19,21 @@ class ProductImageUploader < CarrierWave::Uploader::Base
     !self.file.content_type.include? 'image'
   end
 
-  if Rails.env.local?
-    version :thumb, :if => :image? do
-      process :resize_and_pad => [400, 400]
-    end
-
-    version :detail, :if => :image? do
-      process :resize_and_pad => [400, 400]
-    end
-
-    version :fullsize, :if => :image? do
-      process :resize_and_pad => [600, 600]
-    end
-
-    version :zoomin, :if => :image? do
-      process :resize_and_pad => [1000, 1000]
-    end
+  version :thumb, :if => :image? do
+    process resize_to_limit: [500, 500]
   end
+
+  # version :detail, :if => :image? do
+  #   process resize_to_fill: [400, 400]
+  # end
+  #
+  # version :fullsize, :if => :image? do
+  #   process resize_to_fill: [600, 600]
+  # end
+  #
+  # version :zoomin, :if => :image? do
+  #   process resize_to_fill: [1000, 1000]
+  # end
 
   def default_url
     [version_name].compact.join('_')
