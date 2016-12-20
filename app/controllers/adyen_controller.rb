34d9afa2c4406a886057a85c7b32aee0f506ec05
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-20 19:33:29 +0100

    adyen todo in place

--- app/controllers/adyen_controller.rb
+++ app/controllers/adyen_controller.rb
@@ -8,10 +8,14 @@ class AdyenController < ActionController::Base
   end
 
   def callback
+    
     payment_method = params[:paymentMethod]
     psp_reference = params[:pspReference]
     merchant_signature = params[:merchantSig]
     auth_result = params[:authResult] # status of transaction
+
+    # TODO : merchantSig must be compared to the transaction to be sure it matches with an existing valid transaction which wasn't forged
+
     # {"merchantReference"=>"SKINTEST-1482235768306",
     # "skinCode"=>"Bsot7GRa",
     # "shopperLocale"=>"zh_CN",
@@ -21,13 +25,13 @@ class AdyenController < ActionController::Base
     # "merchantSig"=>"WU7JEnS4DMlE/p7WK5XklDzGw2Q=",
     # "controller"=>"adyen",
     # "action"=>"callback"}
-    #
-#     AUTHORISED: the payment authorisation was successfully completed
-# REFUSED: the payment was refused. Payment authorisation was unsuccessful.
-# CANCELLED: the payment was cancelled by the shopper before completion, or the shopper returned to the merchant's site before completing the transaction.
-# PENDING: it is not possible to obtain the final status of the payment.
-# This can happen if the systems providing final status information for the payment are unavailable, or if the shopper needs to take further action to complete the payment.
-# ERROR: an error occurred during the payment processing.
+
+    # AUTHORISED: the payment authorisation was successfully completed
+    # REFUSED: the payment was refused. Payment authorisation was unsuccessful.
+    # CANCELLED: the payment was cancelled by the shopper before completion, or the shopper returned to the merchant's site before completing the transaction.
+    # PENDING: it is not possible to obtain the final status of the payment.
+    # This can happen if the systems providing final status information for the payment are unavailable, or if the shopper needs to take further action to complete the payment.
+    # ERROR: an error occurred during the payment processing.
 
   end
 
