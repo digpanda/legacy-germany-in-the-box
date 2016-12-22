ba257af69688d383f0f5168f2c949a826614e415
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-22 15:26:58 +0100

    minor address model bugfix

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -109,7 +109,7 @@ class Shop
   validates :function,      length: {maximum: Rails.configuration.max_tiny_text_length}
 
   scope :is_active,       ->    { where( :status => true ).where( :approved.ne => nil ) }
-  scope :has_address,     ->    { self.in({:id => Address.is_shipping.all.map(&:shop_id)}) }
+  scope :has_address,     ->    { self.in({:id => addresses.is_shipping.all.map(&:shop_id)}) }
   scope :is_bg_merchant,  ->    { where(:bg_merchant_id.ne => nil) }
   scope :can_buy,         ->    { is_active.is_bg_merchant.has_address }
 
