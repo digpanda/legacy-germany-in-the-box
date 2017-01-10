9c97d03decd88522cbc6ac62dc440bfd8eb2abe5
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-01-10 18:51:11 +0100

    factories and tests on package set under progress (good base)

---
+++ spec/factories/package_sku.rb
@@ -0,0 +1,30 @@
+FactoryGirl.define do
+  factory :package_sku do
+
+    quantity 2
+    price { BigDecimal.new(rand(1..10)) }
+
+    before(:create) do |package_sku|
+      shop = package_sku.package_set.shop
+      product = shop.products.first
+      sku = product.skus.first
+
+      package_sku.product = product
+      package_sku.sku_id = sku.id
+
+    end
+    #product { FactoryGirl.create(:product) }
+    # sku_id { product.skus.first.id }
+
+
+    # shipping_address        { FactoryGirl.build(:customer_address) }
+    # billing_address         { FactoryGirl.build(:customer_address) }
+    # shop                    { FactoryGirl.create(:shop) }
+    # user                    { FactoryGirl.create(:customer) }
+    #
+    # trait :with_custom_checkable do
+    #   status :custom_checkable
+    #   minimum_sending_date { 48.hours.from_now }
+    # end
+  end
+end
--- spec/features/package_set_process_spec.rb
+++ spec/features/package_set_process_spec.rb
@@ -6,7 +6,13 @@ feature "checkout process", :js => true  do
     login!(customer)
   end
 
+  subject!(:package_set) { FactoryGirl.create(:package_set) }
+
   scenario "get a package set and go to checkout" do
+    visit guest_package_sets_path
+    # TODO : this task was not finished because we can't test it without the new design
+    # those are acceptance test, there's no complex logic behind the package set
+    # therefore we must wait until the new front-end is done.
   end
 
   scenario "get a package set, apply a coupon and go to checkout" do
