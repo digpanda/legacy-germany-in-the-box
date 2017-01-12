647378c375c50b6b73597f3db1cf24323d506db7
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-01-12 14:00:45 +0100

    slight routing change + refactoring layout

--- app/controllers/guest/home_controller.rb
+++ app/controllers/guest/home_controller.rb
@@ -1,6 +1,16 @@
 class Guest::HomeController < ApplicationController
 
+  before_action :admin_redirection
+
   def show
+  end
+
+  private
+
+  def admin_redirection
+    if identity_solver.potential_admin?
+      redirect_to edit_admin_account_path
+    end
   end
 
 end
