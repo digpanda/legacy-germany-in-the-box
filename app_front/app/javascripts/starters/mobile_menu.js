6be6675e0d7b21a646da0c9608cfd1ef580184a3
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-01-16 10:24:46 +0100

    header logic done

--- app_front/app/javascripts/starters/mobile_menu.js
+++ app_front/app/javascripts/starters/mobile_menu.js
@@ -17,22 +17,27 @@ var MobileMenu = {
      */
     startMobileMenu: function() {
 
+      this.manageFade();
+
+    },
+
+    manageFade: function() {
+
       $('.navmenu')
           .on('show.bs.offcanvas', function () {
           console.log('show');
           $('.canvas').addClass('sliding');
       })
           .on('shown.bs.offcanvas', function () {
-          console.log('shown');
       })
           .on('hide.bs.offcanvas', function () {
           $('.canvas').removeClass('sliding');
-              console.log('hide');
       })
           .on('hidden.bs.offcanvas', function () {
           console.log('hidden');
       });
-    },
+      
+    }
 
 }
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2427,14 +2427,16 @@ var MobileMenu = {
      */
     startMobileMenu: function startMobileMenu() {
 
+        this.manageFade();
+    },
+
+    manageFade: function manageFade() {
+
         $('.navmenu').on('show.bs.offcanvas', function () {
             console.log('show');
             $('.canvas').addClass('sliding');
-        }).on('shown.bs.offcanvas', function () {
-            console.log('shown');
-        }).on('hide.bs.offcanvas', function () {
+        }).on('shown.bs.offcanvas', function () {}).on('hide.bs.offcanvas', function () {
             $('.canvas').removeClass('sliding');
-            console.log('hide');
         }).on('hidden.bs.offcanvas', function () {
             console.log('hidden');
         });
