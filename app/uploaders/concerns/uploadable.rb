d949ca0dce06e6aec4af61e8057e51f3cf6c7dd5
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-01-20 17:22:07 +0100

    shrinking uploader codebase

--- app/uploaders/concerns/uploadable.rb
+++ app/uploaders/concerns/uploadable.rb
@@ -10,19 +10,12 @@ module Uploadable
     self.qiniu_can_overwrite = true
     self.qiniu_protocal = 'https'
 
-    # Where should files be stored?
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
-    # Returns true if the file is an image
     def image?(new_file)
       self.file.content_type.include? 'image'
-    end
-
-    # Returns true if the file is not an image
-    def not_image?(new_file)
-      !self.file.content_type.include? 'image'
     end
 
   end
