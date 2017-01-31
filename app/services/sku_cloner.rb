8d9c50553af2115ba32885772ce42fa84fa00872
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-01-31 10:53:19 +0100

    image system bugfix (important)

--- app/services/sku_cloner.rb
+++ app/services/sku_cloner.rb
@@ -64,6 +64,7 @@ class SkuCloner < BaseService
   def copy_file(field)
     if sku.send(field).present?
       CopyCarrierwaveFile::CopyFileService.new(sku, clone, field).set_file
+      clone.save!
     end
   end
 
