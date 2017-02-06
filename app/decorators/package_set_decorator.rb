817adda6148849200d67e9dfb4aa345eb7336bea
Andres Alvidrez <andres.ag4@hotmail.com>
2017-02-06 09:34:05 -0700

    Fixed issue under package set description when it was too big

--- app/decorators/package_set_decorator.rb
+++ app/decorators/package_set_decorator.rb
@@ -1,8 +1,12 @@
 class PackageSetDecorator < Draper::Decorator
 
   include Concerns::Imageable
+  include ActionView::Helpers::TextHelper
 
   delegate_all
   decorates :package_set
 
+  def short_desc(characters=100)
+    truncate(self.desc, length: characters)
+  end
 end
