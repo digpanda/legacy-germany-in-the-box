512c681e61d9a0eaf7efcebe09e7e130201eae49
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-02-10 16:21:22 +0100

    borderguru ftp changes with backups

--- lib/border_guru_ftp/transfer_orders/makers/store.rb
+++ lib/border_guru_ftp/transfer_orders/makers/store.rb
@@ -22,6 +22,7 @@ module BorderGuruFtp
           raise BorderGuruFtp::Error, "No order to place." if orders.empty?
           prepare_directories
           write_orders_csv
+          write_backup
         end
 
         private
@@ -29,6 +30,16 @@ module BorderGuruFtp
         def prepare_directories
           FileUtils.mkdir_p(BorderGuruFtp.local_directory)
           FileUtils.mkdir_p(shop_directory)
+          FileUtils.mkdir_p(backup_directory)
+        end
+
+        def write_backup
+          file = File.open(csv_backup_with_path, "w")
+          file.write(orders_csv)
+        rescue IOError
+          raise BorderGuruFtp::Error => "Impossible to write to local file"
+        ensure
+          file.close unless file.nil?
         end
 
         def write_orders_csv
@@ -44,12 +55,20 @@ module BorderGuruFtp
           "#{BorderGuruFtp.local_directory}#{shop.id}"
         end
 
+        def backup_directory
+          "#{Rails.root}/public/uploads/backups/border_guru_ftp/"
+        end
+
         def csv_file_with_path
           if Rails.env.production?
             "#{shop_directory}/#{formatted_date}_#{border_guru_merchant_id}.csv"
           else
             "#{shop_directory}/#{formatted_date}_#{border_guru_merchant_id}_TEST.csv"
           end
+        end
+
+        def csv_backup_with_path
+          "#{backup_directory}/#{formatted_date}_#{border_guru_merchant_id}_BACKUP.csv"
         end
 
         def formatted_date
