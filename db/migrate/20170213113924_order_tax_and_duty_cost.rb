2ccaa61db779ea08287fcffcd4292150d27b4c9e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-02-14 10:37:46 +0100

    tax cost cancellation

--- db/migrate/20170213113924_order_tax_and_duty_cost.rb
+++ db/migrate/20170213113924_order_tax_and_duty_cost.rb
@@ -1,8 +1,8 @@
 class OrderTaxAndDutyCost < Mongoid::Migration
   def self.up
     Order.all.each do |order|
-      order.taxes_cost = order.tax_and_duty_cost
-      order.save
+      # order.taxes_cost = order.tax_and_duty_cost
+      # order.save
     end
   end
 
