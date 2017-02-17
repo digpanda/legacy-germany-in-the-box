33a7deba565d2f6efa472d92c35d83a06fd5b669
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-02-17 10:55:31 +0100

    shopkeeper page cancelled

--- app/controllers/guest/home_controller.rb
+++ app/controllers/guest/home_controller.rb
@@ -1,6 +1,6 @@
 class Guest::HomeController < ApplicationController
 
-  before_action :admin_redirection
+  before_action :admin_redirection, :shopkeeper_redirection
 
   def show
     @shops = Shop.can_buy.order_by(:position => :asc).all
@@ -11,6 +11,12 @@ class Guest::HomeController < ApplicationController
   def admin_redirection
     if identity_solver.potential_admin?
       redirect_to edit_admin_account_path
+    end
+  end
+
+  def shopkeeper_redirection
+    if identity_solver.potential_shopkeeper?
+      redirect_to new_guest_shop_application_path
     end
   end
 
