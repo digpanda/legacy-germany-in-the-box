dca77936e8318ade5e1b2f59f37a7ae64c0d1f2d
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-02-17 16:51:44 +0100

    geocoder issue tests

--- app/services/identity_solver.rb
+++ app/services/identity_solver.rb
@@ -49,6 +49,7 @@ class IdentitySolver < BaseService
 
   def german_ip?
     @german_ip ||= begin
+      SlackDispatcher.new.mesage("REQUEST GERMAN IP #{request.remote_ip}")
       # SlackDispatcher.new.message("GEOCODER STARTED FOR IP RECOGNITION")
       # country_code = Geocoder.search(request.remote_ip).first&.country_code
       # SlackDispatcher.new.message("IP IS #{request.remote_ip} AND WAS SOLVED")
