ac586e2639e19f1cfa5139d7404e0987602dd08c
Andres Alvidrez <andres.ag4@hotmail.com>
2017-03-01 15:45:01 +0100

    Fixed Alipay initializer production credentials

--- config/initializers/alipay.rb
+++ config/initializers/alipay.rb
@@ -1,18 +1,7 @@
-Alipay.pid = '2088101122136241'
-Alipay.key = '760bdzec6y9goq7ctyx96ezkz78287de'
-# Alipay.pid = '2088621169102875'
-# Alipay.key = 'rl6bmklv0qieuqee02psosxxgrdb0v9f'
-
-
-
-AlipayGlobal.api_partner_id = '2088101122136241'
-AlipayGlobal.api_secret_key = '760bdzec6y9goq7ctyx96ezkz78287de'
-
-
-AlipayGlobal::Service::Trade.create(
-    out_trade_no: '20150401000-0001',
-    subject: 'Subject',
-    currency: 'EUR',
-    total_fee: '10.00',
-    notify_url: 'https://example.com/orders/20150401000-0001/notify'
-)
+if Rails.env.production?
+  Alipay.pid = '2088621169102875'
+  Alipay.key = 'rl6bmklv0qieuqee02psosxxgrdb0v9f'
+else
+  Alipay.pid = '2088101122136241'
+  Alipay.key = '760bdzec6y9goq7ctyx96ezkz78287de'
+end
