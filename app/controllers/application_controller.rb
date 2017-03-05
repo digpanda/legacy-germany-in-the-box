0c7d43a2ab659ee6c0b573d0115b967fd33a0273
Andres Alvidrez <andres.ag4@hotmail.com>
2017-03-05 11:51:05 +0100

    Slackdispatcher silent login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,11 @@ class ApplicationController < ActionController::Base
   before_action :silent_login
 
   def silent_login
+    SlackDispatcher.new.silent_login_attempt("1. params code: #{params[:code]}")
     if params[:code]
+      SlackDispatcher.new.silent_login_attempt("Attempting to authorize...")
       if wechat_auth.success?
+        SlackDispatcher.new.silent_login_attempt("Success!!")
         if wechat_auth.data[:tourist_guide]
           redirect_to customer_referrer_path
           return
