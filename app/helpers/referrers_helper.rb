80fcd5eec9070ab402441b3a6bf74df7f1e99d66
Andres Alvidrez <andres.ag4@hotmail.com>
2017-03-07 15:06:08 +0100

    Added provision column in referrers index

---
+++ app/helpers/referrers_helper.rb
@@ -0,0 +1,13 @@
+module ReferrersHelper
+  def get_referrer_provision(referrer)
+    referrer.referrer_coupons.inject(0) do |provision, coupon|
+      provision += coupon_orders_provision(coupon)
+    end
+  end
+
+  def coupon_orders_provision(coupon)
+    coupon.orders.bought.inject(0) do |sum, order|
+      sum += order.end_price * order.referrer_rate / 100
+    end
+  end
+end
