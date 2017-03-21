ccd9e1a3a94e3365c06f89313f70d11d5158acb4
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-03-21 17:11:56 +0100

    pid + mobile validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -38,8 +38,8 @@ class Address
   scope :is_only_shipping,    ->  { any_of({type: :shipping}) }
   scope :is_only_both,        ->  { any_of({type: :both}) }
 
-  # NOTE : The PID system was temporarily made not mandatory
-  # validates :pid, presence: false, :format => { :with => CHINESE_ID }, :if => -> { user&.customer? }
+  validates :mobile, presence: true, :if => -> { user&.customer? }
+  validates :pid, presence: true, :format => { :with => CHINESE_ID }, :if => -> { user&.customer? }
   validates :fname, presence: true, :format => { :with => CHINESE_CHARACTERS }, :if => -> { user&.customer? }
   validates :lname, presence: true, :format => { :with => CHINESE_CHARACTERS }, :if => -> { user&.customer? }
 
