a231650154ca5465b9ae033ef202fc4aa3ddb041
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-03-30 12:03:54 +0200

    body changed for transmitted data

--- app/controllers/api/webhook/wechatpay/customers_controller.rb
+++ app/controllers/api/webhook/wechatpay/customers_controller.rb
@@ -5,32 +5,33 @@ class Api::Webhook::Wechatpay::CustomersController < Api::ApplicationController
 
   skip_before_filter :verify_authenticity_token
 
-  attr_reader :data
+  attr_reader :transmit_data
 
   def create
 
     devlog.info "Wechatpay started to communicate with us ..."
-    SlackDispatcher.new.message("PROCESSED BODY : #{Hash.from_xml(request.body.read)}")
-    @data = Hash.from_xml(request.body.read)&.[]("xml")
+    body = Hash.from_xml(request.body.read)
+    SlackDispatcher.new.message("PROCESSED BODY : #{body}")
+    @transmit_data = body&.[]("xml")
 
     unless valid_xml?
       throw_api_error(:bad_format, {error: "Wrong format transmitted"}, :bad_request)
       return
     end
 
-    devlog.info("Raw params : #{data}")
+    devlog.info("Raw params : #{transmit_data}")
 
-    if wrong_data?
-      throw_api_error(:bad_format, {error: "Wrong datas transmitted"}, :bad_request)
+    if wrong_transmit_data?
+      throw_api_error(:bad_format, {error: "Wrong transmit_datas transmitted"}, :bad_request)
       return
     end
 
     if checkout_callback.success?
       devlog.info "Transaction successfully processed."
-      SlackDispatcher.new.message("[Webhook] Wechatpay transaction SUCCESS processed : #{data}")
+      SlackDispatcher.new.message("[Webhook] Wechatpay transaction SUCCESS processed : #{transmit_data}")
     else
       devlog.info "Processing of the transaction failed."
-      SlackDispatcher.new.message("[Webhook] Wechatpay transaction FAIL processed : #{data}")
+      SlackDispatcher.new.message("[Webhook] Wechatpay transaction FAIL processed : #{transmit_data}")
     end
 
     devlog.info "End of process."
@@ -46,11 +47,11 @@ class Api::Webhook::Wechatpay::CustomersController < Api::ApplicationController
   private
 
   def checkout_callback
-    @checkout_callback ||= CheckoutCallback.new(nil, cart_manager, data).wechatpay!
+    @checkout_callback ||= CheckoutCallback.new(nil, cart_manager, transmit_data).wechatpay!
   end
 
-  def wrong_data?
-    data["out_trade_no"].nil? || data["transaction_id"].nil? || data["return_code"].nil?
+  def wrong_transmit_data?
+    transmit_data["out_trade_no"].nil? || transmit_data["transaction_id"].nil? || transmit_data["return_code"].nil?
   end
 
   def valid_xml?
