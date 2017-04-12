8853d5a740fed0f70d74bc849b73fd3b1f5d3bc5
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-04-12 11:38:22 +0200

    initialization of react

--- app_react/app/components/App.jsx
+++ app_react/app/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import CounterComponent from 'components/Counter';
 =======
 >>>>>>> initialization of react
+=======
+import CounterComponent from 'components/Counter';
+>>>>>>> react
 
 export default class App extends React.Component {
   render() {
     return (
+<<<<<<< HEAD
 <<<<<<< HEAD
       <div>
         <div className="+spacer"></div>
@@ -15,6 +20,11 @@ export default class App extends React.Component {
       <div id="content">
         <h5>jTime to <a href="https://facebook.github.io/react/">React</a>.</h5>
 >>>>>>> initialization of react
+=======
+      <div>
+        <div className="+spacer"></div>
+        <CounterComponent count={0} />
+>>>>>>> react
       </div>
     );
   }
