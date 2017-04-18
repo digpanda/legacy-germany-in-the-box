56fdd331e0f28ba184dca36abbbd5d073651c535
Andres Alvidrez <andres.ag4@hotmail.com>
2017-04-18 09:50:46 +0200

    Hide inactive package set from navitagion in show

--- app/models/concerns/entry_position.rb
+++ app/models/concerns/entry_position.rb
@@ -6,11 +6,11 @@ module EntryPosition
 
   # previous entry
   def previous
-    self.class.where(:_id.lt => self._id).order_by([[:_id, :desc]]).limit(1).first
+    self.class.where(:_id.lt => self._id, :active => true).order_by([[:_id, :desc]]).limit(1).first
   end
 
   # next entry
   def next
-    self.class.where(:_id.gt => self._id).order_by([[:_id, :asc]]).limit(1).first
+    self.class.where(:_id.gt => self._id, :active => true).order_by([[:_id, :asc]]).limit(1).first
   end
 end
