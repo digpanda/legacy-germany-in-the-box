0ed9fc26a3b88314b410d88601d9793e1274ad27
Andres Alvidrez <andres.ag4@hotmail.com>
2017-04-21 12:50:32 +0200

    Added referrer token crud for admin

--- app/models/referrer_token.rb
+++ app/models/referrer_token.rb
@@ -2,7 +2,20 @@ class ReferrerToken
   include MongoidBase
 
   field :token, type: String
+  field :name, type: String
+  field :desc, type: String
+  field :type, type: Symbol, default: :publisher
+  field :group, type: String
+  field :coupon_discount, type: Boolean, default: true
+  field :coupon_discount_extra, type: Float, default: 0
+  field :sales_provision, type: Boolean, default: true
+  field :sales_provision_extra, type: Float, default: 0
+  field :redeem_discount, type: Float, default: 0
+  field :note, type: String
   field :used_at, type: Time
+
+  validates_presence_of :name, :group
+  validates_uniqueness_of :name, :group
 
   before_save :generate_token
 
@@ -20,5 +33,13 @@ class ReferrerToken
     else
       return false
     end
+  end
+
+  def referrer_url
+    "https://open.weixin.qq.com/connect/oauth2/authorize?" +
+        "appid=#{Rails.application.config.wechat[:username_mobile]}&" +
+        "redirect_uri=http%3A%2F%2Fgermanyinbox.com/connect/auth/referrer?" +
+        "token%3D#{self.token}" +
+        "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
   end
 end
