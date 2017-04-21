65b762949ffc52b60b47490b6a96fffd86518344
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-04-21 10:37:42 +0200

    fixing merge conflict

--- app_react/app/components/App.jsx
+++ app_react/app/components/App.jsx
@@ -1,40 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import CounterComponent from 'components/Counter';
-=======
->>>>>>> initialization of react
-=======
-import CounterComponent from 'components/Counter';
->>>>>>> react
-=======
-import CounterComponent from 'components/Counter';
->>>>>>> counter system test on react
 
 export default class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       <div>
         <div className="+spacer"></div>
         <CounterComponent count={0} />
-=======
-      <div id="content">
-        <h5>jTime to <a href="https://facebook.github.io/react/">React</a>.</h5>
->>>>>>> initialization of react
-=======
-      <div>
-        <div className="+spacer"></div>
-        <CounterComponent count={0} />
->>>>>>> react
-=======
-      <div>
-        <div className="+spacer"></div>
-        <CounterComponent count={0} />
->>>>>>> counter system test on react
       </div>
     );
   }
