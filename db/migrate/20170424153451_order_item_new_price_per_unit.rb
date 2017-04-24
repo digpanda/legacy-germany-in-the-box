3a3e2153ff9a336d49a311de866ede01815ca7c2
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-04-24 18:06:14 +0200

    minor bugfix

--- db/migrate/20170424153451_order_item_new_price_per_unit.rb
+++ db/migrate/20170424153451_order_item_new_price_per_unit.rb
@@ -2,11 +2,13 @@ class OrderItemNewPricePerUnit < Mongoid::Migration
   def self.up
 
     OrderItem.all.each do |order_item|
-      order_item.order.bypass_locked! # make sure it will systematically work
-      order_item.price_per_unit = order_item.sku.price_per_unit || 0.0
-      puts "Order Item Price Per Unit `#{order_item.price_per_unit}`"
-      order_item.save
-      puts "Saved : #{order_item.save}"
+      if order_item.order
+        order_item.order.bypass_locked! # make sure it will systematically work
+        order_item.price_per_unit = order_item.sku.price_per_unit || 0.0
+        puts "Order Item Price Per Unit `#{order_item.price_per_unit}`"
+        order_item.save
+        puts "Saved : #{order_item.save}"
+      end
     end
 
   end
