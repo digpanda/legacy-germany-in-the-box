ab769fed85c53b9d92d1d4286b639a94ac5846ed
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-04-25 19:36:26 +0200

    echos for migration

--- db/migrate/20170425150009_fixing_new_image_system.rb
+++ db/migrate/20170425150009_fixing_new_image_system.rb
@@ -2,13 +2,18 @@ class FixingNewImageSystem < Mongoid::Migration
   def self.up
     # Copy old image system to new image system for skus in products
     Product.all.each do |product|
+      puts "Select Product `#{product.id}` ..."
       product.skus.each do |sku|
+        puts "Select Sku `#{sku.id}` ..."
         sku.images.delete_all
         [:img0, :img1, :img2, :img3].each do |field|
+          puts "Converting `#{field}` if it's not empty ..."
           if sku.send(field) && !sku.send(field).url.empty?
+            puts "It's not empty. Processing ..."
             image = sku.images.new
             image.remote_file_url = sku.send(field).url
             image.save
+            puts "Conversion was done."
           end
         end
       end
@@ -16,13 +21,18 @@ class FixingNewImageSystem < Mongoid::Migration
 
     # Copy old image system to new image system for skus in order items
     OrderItem.all.each do |order_item|
+      puts "Select OrderItem `#{order_item.id}` ..."
       sku = order_item.sku
       sku.images.delete_all
+        puts "Select Sku `#{sku.id}` ..."
       [:img0, :img1, :img2, :img3].each do |field|
+        puts "Converting `#{field}` if it's not empty ..."
         if sku.send(field) && !sku.send(field).url.empty?
+          puts "It's not empty. Processing ..."
           image = sku.images.new
           image.remote_file_url = sku.send(field).url
           image.save
+          puts "Conversion was done."
         end
       end
     end
