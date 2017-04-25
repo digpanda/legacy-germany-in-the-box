82be7fedd6849c4b8346ff638653ab47109ac8c1
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-04-25 20:25:09 +0200

    no timeout + removed validation to speed up the migrations

--- db/migrate/20170425150009_fixing_new_image_system.rb
+++ db/migrate/20170425150009_fixing_new_image_system.rb
@@ -1,9 +1,11 @@
 class FixingNewImageSystem < Mongoid::Migration
+  # NOTE : no_timeout is here because there are so many entries
+  # a cursor error would occur after a while.
   def self.up
     # Copy old image system to new image system for skus in products
-    Product.all.each do |product|
+    Product.no_timeout.all.each do |product|
       puts "Select Product `#{product.id}` ..."
-      product.skus.each do |sku|
+      product.skus.no_timeout.each do |sku|
         puts "Select Sku `#{sku.id}` ..."
         sku.images.delete_all
         [:img0, :img1, :img2, :img3].each do |field|
@@ -12,7 +14,7 @@ class FixingNewImageSystem < Mongoid::Migration
             puts "It's not empty. Processing ..."
             image = sku.images.new
             image.remote_file_url = sku.send(field).url
-            image.save
+            image.save(validation: false)
             puts "Conversion was done."
           end
         end
@@ -20,18 +22,18 @@ class FixingNewImageSystem < Mongoid::Migration
     end
 
     # Copy old image system to new image system for skus in order items
-    OrderItem.all.each do |order_item|
+    OrderItem.no_timeout.all.each do |order_item|
       puts "Select OrderItem `#{order_item.id}` ..."
       sku = order_item.sku
       sku.images.delete_all
-        puts "Select Sku `#{sku.id}` ..."
+      puts "Select Sku `#{sku.id}` ..."
       [:img0, :img1, :img2, :img3].each do |field|
         puts "Converting `#{field}` if it's not empty ..."
         if sku.send(field) && !sku.send(field).url.empty?
           puts "It's not empty. Processing ..."
           image = sku.images.new
           image.remote_file_url = sku.send(field).url
-          image.save
+          image.save(validation: false)
           puts "Conversion was done."
         end
       end
