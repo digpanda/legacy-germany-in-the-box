65c3ecf49b37c39c605909b7978a2acb416fa296
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-04-25 11:37:57 +0200

    test more

--- db/migrate/20170424153451_order_item_new_price_per_unit.rb
+++ db/migrate/20170424153451_order_item_new_price_per_unit.rb
@@ -2,7 +2,7 @@ class OrderItemNewPricePerUnit < Mongoid::Migration
   def self.up
 
     OrderItem.all.each do |order_item|
-      order_item.ensure_price_per_unit
+      order_item.price_per_unit = order_item.sku.price_per_unit
       puts "Order Item Price Per Unit `#{order_item.price_per_unit}` (#{order_item.id})"
       order_item.save(validation: false)
       # if order_item.order
