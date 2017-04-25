68b295990ce1e326266de574cdd093294617f14d
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-04-25 10:45:25 +0200

    some more change with lockign system

--- db/migrate/20170424153451_order_item_new_price_per_unit.rb
+++ db/migrate/20170424153451_order_item_new_price_per_unit.rb
@@ -2,12 +2,12 @@ class OrderItemNewPricePerUnit < Mongoid::Migration
   def self.up
 
     OrderItem.all.each do |order_item|
-      if order_item.order
-        order_item.order.bypass_locked! # make sure it will systematically work
-      end
       order_item.price_per_unit = order_item.sku.price_per_unit || 0.0
       puts "Order Item Price Per Unit `#{order_item.price_per_unit}`"
       order_item.save(validation: false)
+      # if order_item.order
+      #   order_item.order.bypass_locked! # make sure it will systematically work
+      # end
       puts "Saved : #{order_item.save}"
     end
 
