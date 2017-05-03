cd7bac8a4ca97e369f706aa8c0e96087541013b7
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-05-03 18:37:26 +0200

    all tests green

--- spec/support/features/login.rb
+++ spec/support/features/login.rb
@@ -8,16 +8,10 @@ module Helpers
         visit new_user_session_path
         fill_in 'user[email]', :with => account.email
         fill_in 'user[password]', :with => '12345678'
-        sleep(0.5)
+        sleep(0.2)
         page.first("#sign_in").trigger('click')
+        sleep(0.2)
         expect(page).not_to have_current_path(new_user_session_path)
-      end
-
-      def logout!
-        # visit new_user_session_path
-        # fill_in 'user[email]', :with => account.email
-        # fill_in 'user[password]', :with => '12345678'
-        # page.first("#sign_in").trigger('click')
       end
 
     end
