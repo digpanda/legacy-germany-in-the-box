d19e7a07e14d2d6725dc2757d1b72bc35997d9ff
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-05-16 13:43:19 +0200

    reverse

--- app/services/sku_cloner.rb
+++ app/services/sku_cloner.rb
@@ -5,6 +5,9 @@
 # the `relationship` is the way we should integrate the sku to the `target` (many or one sku relationship)
 class SkuCloner < BaseService
 
+  IMAGES_MAP = [:img0, :img1, :img2, :img3]
+  ATTACH_MAP = [:attach0]
+
   attr_reader :target, :sku, :relationship
 
   def initialize(target, sku, relationship=:plural)
@@ -14,6 +17,8 @@ class SkuCloner < BaseService
   end
 
   def process
+    entry!
+    # we save after all alterations
     if target.save
       FileWorker.perform_async(sku.product.id, target.id, sku.id, clone.id, relationship)
       return_with(:success, :clone => clone)
@@ -32,6 +37,37 @@ class SkuCloner < BaseService
           target.sku = clone
         end
       end
+    end
+  end
+
+  private
+
+  # we simply call the memoized method
+  # this will process it once
+  def entry!
+    clone
+  end
+
+  def images!
+    IMAGES_MAP.each do |image_field|
+      copy_file(image_field)
+    end
+  end
+
+  def attach!
+    ATTACH_MAP.each do |attach_field|
+      copy_file(attach_field)
+    end
+  end
+
+
+  def copy_file(field)
+    if sku.send(field).present?
+      # clone.send("#{field}=", sku.send(field))
+      # CopyCarrierwaveFile::CopyFileService.new(sku, clone, field).set_file
+      clone.save!
+      #Delayed::Worker.logger.debug "Task in queue"
+      #Delayed::Job.enqueue CopyFileJob.new(sku, clone, field), :queue => 'clone_images'
     end
   end
 
