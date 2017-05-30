a07744bb2fb18f112578a32900cc895a23e246b0
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-05-30 12:40:46 +0200

    progress in test but failing because of memoization

--- spec/controllers/api/guest/package_sets_controller_spec.rb
+++ spec/controllers/api/guest/package_sets_controller_spec.rb
@@ -6,15 +6,49 @@ describe Api::Guest::PackageSetsController, :type => :controller do
 
     context "from guest customer" do
 
-      # chain creation depending on the test
-      let(:package_set) { FactoryGirl.create(:package_set) }
-      
-      it "adds a package set to an order" do
-        post :create, {'package_set_id': package_set.id}
-        expect_json(success: true)
-        # small check to know if we actually made
-        # a real order successfully
-        expect(Order.count).to eq(1)
+      context "with one package set" do
+
+        let(:package_set) { FactoryGirl.create(:package_set) }
+
+        it "adds a package set to an order" do
+          post :create, {'package_set_id': package_set.id}
+          expect_json(success: true)
+          # small check to know if we actually made
+          # a real order successfully
+          expect(Order.count).to eq(1)
+        end
+
+      end
+
+      context "with multiple package sets from different shops" do
+
+        let(:package_sets) { FactoryGirl.create_list(:package_set, 3) }
+
+        it "adds package sets to multiple orders" do
+          package_sets.each do |package_set|
+            post :create, {'package_set_id': package_set.id}
+            expect_json(success: true)
+          end
+          expect(Order.count).to eq(3)
+        end
+
+      end
+
+      context "with multiple package sets from the same shop" do
+
+        let(:shop) { FactoryGirl.create(:shop) }
+        let(:package_sets) { FactoryGirl.create_list(:package_set, 3, shop: shop) }
+
+        it "adds package sests to an order" do
+          package_sets.each do |package_set|
+            post :create, {'package_set_id': package_set.id}
+            expect_json(success: true)
+          end
+          expect(Order.count).to eq(1)
+          # were the paclage sets correctly put in it ?
+          expect(Order.first.package_sets.count).to eq(3)
+        end
+
       end
 
     end
