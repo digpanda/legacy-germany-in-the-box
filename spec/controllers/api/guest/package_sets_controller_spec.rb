e89b65844589cdb6441d2a91c85c66cd3c9091df
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-05-30 12:49:40 +0200

    tests all green, ready to refactor

--- spec/controllers/api/guest/package_sets_controller_spec.rb
+++ spec/controllers/api/guest/package_sets_controller_spec.rb
@@ -28,6 +28,10 @@ describe Api::Guest::PackageSetsController, :type => :controller do
           package_sets.each do |package_set|
             post :create, {'package_set_id': package_set.id}
             expect_json(success: true)
+            # we need to hook this to make it look like it's different request
+            # avoid instance variable memoization
+            # NOTE : https://stackoverflow.com/questions/44259942/rspec-keeps-instance-variable-between-post-requests/44260046#44260046
+            controller.instance_variable_set(:@order, nil)
           end
           expect(Order.count).to eq(3)
         end
@@ -53,59 +57,5 @@ describe Api::Guest::PackageSetsController, :type => :controller do
 
     end
   end
-
-  # describe "#update" do
-  #
-  #   context "from guest customer" do
-  #
-  #     let(:shop) { FactoryGirl.create(:shop, :with_orders) }
-  #
-  #     let(:order_item) { shop.orders.first.order_items.first }
-  #     let(:product) { order_item.product }
-  #
-  #     # we get the correct sku matching the order_item
-  #     # we are careful to take the original sku and not the one in the order item
-  #     # so we can basically manipulate quantities
-  #     let(:sku) { order_item.sku_origin }
-  #
-  #     before :each do
-  #       sku.update!({
-  #         :quantity => 5,
-  #         :unlimited => false,
-  #         })
-  #     end
-  #
-  #     it "cannot set quantity to 0" do
-  #
-  #       params = {"quantity" => "0"}
-  #       patch :update, {"id" => order_item.id, "product_id" => product.id}.merge(params)
-  #
-  #       expect(response).to have_http_status(:bad_request)
-  #       expect_json(success: false, code: 8)
-  #
-  #     end
-  #
-  #     it "set quantity to 5" do
-  #
-  #       params = {"quantity" => "5"}
-  #       patch :update, {"id" => order_item.id, "product_id" => product.id}.merge(params)
-  #
-  #       expect(response).to have_http_status(:success)
-  #       expect_json(success: true)
-  #
-  #     end
-  #
-  #     it "cannot set quantity to 500" do
-  #
-  #       params = {"quantity" => "500"}
-  #       patch :update, {"id" => order_item.id, "product_id" => product.id}.merge(params)
-  #
-  #       expect_json(success: false, code: 13)
-  #
-  #     end
-  #
-  #   end
-  #
-  # end
 
 end
