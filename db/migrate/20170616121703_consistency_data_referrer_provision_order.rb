b2db8f38bef3e4f1bd8d0527a256a0d5c34abf87
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-06-16 14:19:32 +0200

    provision migration + update force mobile on customers

---
+++ db/migrate/20170616121703_consistency_data_referrer_provision_order.rb
@@ -0,0 +1,20 @@
+class ConsistencyDataReferrerProvisionOrder < Mongoid::Migration
+  def self.up
+    puts "Checking through all the referrer for data consistency ..."
+    Referrer.all.each do |referrer|
+      puts "Check Referrer `#{referrer.id}`"
+      referrer.provisions.each do |provision|
+        puts "Check Provision `#{provision.id}`"
+        unless provision.order
+          puts "Provision is orphan / has no order"
+          provision.delete
+          puts "Provision deleted."
+        end
+      end
+    end
+    puts "End of process."
+  end
+
+  def self.down
+  end
+end
