4eb41c875bbc82b1257fa75d58653f5b720c29e9
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-06-16 14:51:44 +0200

    slack dispatcher

--- app/services/signin_handler.rb
+++ app/services/signin_handler.rb
@@ -14,6 +14,7 @@ class SigninHandler
 
   def solve!
     if user.customer?
+      SlackDispatcher.new.message("REQUEST #{request.url}")
       force_chinese!
       handle_past_orders!
       recover_last_order!
