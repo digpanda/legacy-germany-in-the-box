f2f90a3a5ccc7b5de4db73ec94f2ff7680d84dbe
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-06-22 13:20:02 +0200

    overall improvement display white table

--- public/javascripts/vendor.js
+++ public/javascripts/vendor.js
@@ -41607,7 +41607,7 @@ return $;
       });
     }
   };
-  var port = ar.port || 9486;
+  var port = ar.port || 9485;
   var host = br.server || window.location.hostname || 'localhost';
 
   var connect = function connect() {
