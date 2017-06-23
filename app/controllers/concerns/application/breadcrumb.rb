4dfdf173dda24bcb2dcf93aa63bb2b3c7a367bef
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-06-23 12:02:21 +0200

    breadcrumb total redesign

--- app/controllers/concerns/application/breadcrumb.rb
+++ app/controllers/concerns/application/breadcrumb.rb
@@ -46,6 +46,10 @@ module Application
       add_breadcrumb I18n.t(:home, scope: :breadcrumb), :root_path
     end
 
+    # =====
+    # SHOPKEEPER
+    # =====
+
     def breadcrumb_shopkeeper_products
       add_breadcrumb current_user.shop.shopname, shopkeeper_products_path
     end
@@ -66,8 +70,12 @@ module Application
       add_breadcrumb I18n.t(:variants, scope: :breadcrumb), shopkeeper_product_variants_path(@product) if @product
     end
 
+    # =====
+    # ADMIN
+    # =====
+
     def breadcrumb_admin_shops
-      add_breadcrumb I18n.t(:shops, scope: :breadcrumb), admin_shops_path
+      add_breadcrumb "Shops", admin_shops_path
     end
 
     def breadcrumb_admin_shop
@@ -85,17 +93,17 @@ module Application
     end
 
     def breadcrumb_admin_product_skus
-      add_breadcrumb I18n.t(:skus, scope: :breadcrumb), admin_shop_product_skus_path(@product.shop, @product) if @product
+      add_breadcrumb "Skus", admin_shop_product_skus_path(@product.shop, @product) if @product
     end
 
     def breadcrumb_admin_product_edit_sku
       if @sku
-        add_breadcrumb I18n.t(:edit, scope: :breadcrumb), edit_admin_shop_product_sku_path(@sku.product.shop, @sku.product, @sku)
+        add_breadcrumb "Edit", edit_admin_shop_product_sku_path(@sku.product.shop, @sku.product, @sku)
       end
     end
 
     def breadcrumb_admin_product_variants
-      add_breadcrumb I18n.t(:variants, scope: :breadcrumb), admin_shop_product_variants_path(@product.shop, @product) if @product
+      add_breadcrumb "Variants", admin_shop_product_variants_path(@product.shop, @product) if @product
     end
   end
 end
---
+++ app/decorators/category_decorator.rb
@@ -0,0 +1,8 @@
+class CategoryDecorator < Draper::Decorator
+
+  include Concerns::Imageable
+
+  delegate_all
+  decorates :category
+
+end
