dc69739db2971ac0355c63987107185686662f22
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-06-29 12:58:09 +0200

    order maker fully tested

--- spec/services/order_maker_spec.rb
+++ spec/services/order_maker_spec.rb
@@ -1,10 +1,10 @@
 describe OrderMaker do
 
-  context "#sku" do
+  let(:current_user) { FactoryGirl.create(:customer) }
+  let(:request) { double('request', url: nil, session: {}, params: {}) }
+  let(:identity_solver) { IdentitySolver.new(request, current_user) }
 
-    let(:current_user) { FactoryGirl.create(:customer) }
-    let(:request) { double('request', url: nil, session: {}, params: {}) }
-    let(:identity_solver) { IdentitySolver.new(request, current_user) }
+  context "#sku" do
 
     let(:order) { FactoryGirl.create(:order, status: :new) }
 
@@ -27,4 +27,25 @@ describe OrderMaker do
     end
 
   end
+
+  context "#package_set" do
+
+    let(:order) { FactoryGirl.create(:order, status: :new) }
+    let(:package_set) { FactoryGirl.create(:package_set) }
+
+    it "should add a package set to the order" do
+      expect {
+        OrderMaker.new(identity_solver, order).package_set(package_set).refresh!(1)
+      }.to change{order.package_set_quantity(package_set)}.by(1)
+    end
+
+    it "should add an item multiple times to the order and change its quantity" do
+      expect {
+        OrderMaker.new(identity_solver, order).package_set(package_set).refresh!(2)
+        OrderMaker.new(identity_solver, order).package_set(package_set).refresh!(3, increment: true)
+      }.to change{order.package_set_quantity(package_set)}.by(5)
+    end
+
+  end
+
 end
