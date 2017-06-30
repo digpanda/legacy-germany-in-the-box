6b8617f5eb181b643ab302b710a08bc580deaeee
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-06-30 15:43:57 +0200

    bypass locked

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -19,10 +19,12 @@ class Admin::OrderItemsController < ApplicationController
   # we refresh the referrer rate and the provision linked to it
   def refresh_referrer_rate
     order_item.refresh_referrer_rate!
+    order_item.bypass_locked!
     order_item.save(validation: false) # allow locked entry to be saved
     SlackDispatcher.new.message("ORDER ITEM ERROR ON REFRESH : #{order_item.errors.full_messages.join(', ')}")
     order.refresh_referrer_provision!
-    order.save(validation: false)
+    order.bypass_locked!
+    order.save
     flash[:success] = "The referrer rate for this order item was refreshed."
     redirect_to navigation.back(1)
   end
