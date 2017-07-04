e11fe68f2fe27a1da539f9cdb7781bd15f2e3d58
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-04 13:14:44 +0200

    param sdispatcher

--- app/controllers/customer/account_controller.rb
+++ app/controllers/customer/account_controller.rb
@@ -13,6 +13,7 @@ class Customer::AccountController < ApplicationController
   # NOTE : this update is used from many different points
   # within the system (e.g checkout process) be careful with this.
   def update
+    SlackDispatcher.new.message("PARAMS : #{params}")
     if valid_password? && ensure_password! && user.update(user_params)
       flash[:success] = I18n.t("notice.account_updated")
       sign_in(user, :bypass => true)
