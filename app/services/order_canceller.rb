f85e05a4ca59a916cb2c27c8b6d7df963c51df10
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-10 15:19:52 +0300

    order canceller refactoring started (but more complicated than expected)

--- app/services/order_canceller.rb
+++ app/services/order_canceller.rb
@@ -9,7 +9,7 @@ class OrderCanceller < BaseService
 
   def all!
     # managing possible problems
-    unless order.decorate.cancellable?
+    unless order.cancellable?
       return return_with(:error, "Impossible to cancel order")
     end
     # we cancel the order
@@ -20,6 +20,17 @@ class OrderCanceller < BaseService
     return_with(:success)
   end
 
+  private
 
+  # # will physically remove the order
+  # # it should be used only if the order was not bought
+  # def remove_order!
+  #   order.order_payments.where(status: :scheduled).delete_all
+  #   if order.order_payments.count > 0
+  #     raise Error, "Order cannot be physically removed, some payments were confirmed."
+  #   end
+  #   order.order_items.delete_all
+  #   order.delete
+  # end
 
 end
