03f9a73fc1f38880b4e23bc4686f95c814b8c504
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-10 17:51:12 +0300

    refactoring order canceller in progress (stopped for now)

--- app/services/order_canceller.rb
+++ app/services/order_canceller.rb
@@ -8,29 +8,45 @@ class OrderCanceller < BaseService
   end
 
   def all!
-    # managing possible problems
+    # NOTE : destroy! isn't tested nor functional so far.
+    # if order.bought?
+      cancel!
+    # else
+    #   destroy!
+    # end
+  end
+
+  private
+
+  def destroy!
+    order.order_payments.where(status: :scheduled).delete_all
+    if order.order_payments.count > 0
+      return return_with(:error, "Order cannot be physically removed, some payments were confirmed.")
+    end
+    order.order_items.delete_all
+    order.remove_coupon(identity_solver) if order.coupon
+    order.delete
+  end
+
+  def cancel!
+    # should never happen but it's
+    # a supplementary protection
     unless order.cancellable?
       return return_with(:error, "Impossible to cancel order")
     end
     # we cancel the order
     order.status = :cancelled
-    StockManager.new(order).out_order!
-    return return_with(:error, order.errors.full_messages.join(', ')) unless order.save
-    # we go back now
-    return_with(:success)
+    stock_manager.out_order!
+
+    if order.save
+      return_with(:success)
+    else
+      return_with(:error, order.errors.full_messages.join(', '))
+    end
   end
 
-  private
-
-  # # will physically remove the order
-  # # it should be used only if the order was not bought
-  # def remove_order!
-  #   order.order_payments.where(status: :scheduled).delete_all
-  #   if order.order_payments.count > 0
-  #     raise Error, "Order cannot be physically removed, some payments were confirmed."
-  #   end
-  #   order.order_items.delete_all
-  #   order.delete
-  # end
+  def stock_manager
+    @stock_manager ||= StockManager.new(order)
+  end
 
 end
