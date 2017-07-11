8c756aafd82d7b3c5cf52e773f76b637ae63930b
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-11 16:02:38 +0300

    format xml system done for webhook wechat

--- app/controllers/api/webhook/wechat_controller.rb
+++ app/controllers/api/webhook/wechat_controller.rb
@@ -17,34 +17,6 @@ class Api::Webhook::WechatController < Api::ApplicationController
 
   def create
     handle
-
-    # devlog.info "Wechatpay started to communicate with us ..."
-    # body = Hash.from_xml(request.body.read)
-    #
-    # binding.pry
-    #
-    # @transmit_data = body&.[]("xml")
-    #
-    # unless valid_xml?
-    #   throw_api_error(:bad_format, {error: "Wrong format transmitted"}, :bad_request)
-    #   return
-    # end
-    #
-    # devlog.info("Raw params : #{transmit_data}")
-    #
-    # if wrong_transmit_data?
-    #   throw_api_error(:bad_format, {error: "Wrong transmit_datas transmitted"}, :bad_request)
-    #   return
-    # end
-    #
-    # if checkout_callback.success?
-    #   devlog.info "Transaction successfully processed."
-    #   SlackDispatcher.new.message("[Webhook] Wechatpay transaction SUCCESS processed : #{transmit_data}")
-    # else
-    #   devlog.info "Processing of the transaction failed."
-    #   SlackDispatcher.new.message("[Webhook] Wechatpay transaction FAIL processed : #{transmit_data}")
-    # end
-
   end
 
   # WARNING : Must stay public for throw_error to work well for now.
@@ -55,33 +27,39 @@ class Api::Webhook::WechatController < Api::ApplicationController
   private
 
   def handle
+    return if hook_activation?
 
-    if params[:echostr]
-      SlackDispatcher.new.message("[Wechat] Our Webhook is now verified (echostr `#{params[:echostr]}`).")
-      devlog.info "End of process."
-      render text: params[:echostr]
+    devlog.info "Wechat started to communicate with us ..."
+    body = Hash.from_xml(request.body.read)
+    @transmit_data = body&.[]("xml")
+
+    unless valid_xml?
+      throw_api_error(:bad_format, {error: "Wrong format transmitted"}, :bad_request)
       return
     end
 
-    # {"signature"=>"cae5295df1dcbda69d56fe45a3559386172db163", "echostr"=>"17559559235148053755", "timestamp"=>"1499775155", "nonce"=>"988885973", "format"=>:json, "controller"=>"api/webhook/wechat/qrcodes", "action"=>"show"}
-    SlackDispatcher.new.message("PARAMS: #{params}")
-    SlackDispatcher.new.message("QRCODE : #{request.body.read}")
+    devlog.info("Raw params : #{transmit_data}")
+
+    SlackDispatcher.new.message("PARAMS TRANSMIT : #{transmit_data}")
+
   end
 
-  #
-  # def checkout_callback
-  #   @checkout_callback ||= CheckoutCallback.new(nil, cart_manager, transmit_data).wechatpay!
-  # end
-  #
-  # def wrong_transmit_data?
-  #   transmit_data["out_trade_no"].nil? || transmit_data["transaction_id"].nil? || transmit_data["return_code"].nil?
-  # end
-  #
-  # def valid_xml?
-  #   Hash.from_xml(request.body.read)
-  #   true
-  # rescue REXML::ParseException
-  #   false
-  # end
+  def valid_xml?
+    Hash.from_xml(request.body.read)
+    true
+  rescue REXML::ParseException
+    false
+  end
+
+  def hook_activation?
+    if params[:echostr]
+      SlackDispatcher.new.message("[Webhook] Our Wechat Webhook is now verified / activated (echostr `#{params[:echostr]}`).")
+      devlog.info "End of process."
+      render text: params[:echostr]
+      true
+    else
+      false
+    end
+  end
 
 end
