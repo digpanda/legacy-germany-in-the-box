478e525c995ff43f16c3597bde111f5e8668872d
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-13 17:26:27 +0300

    menu commented in

--- app/services/wechat_api_user_info.rb
+++ app/services/wechat_api_user_info.rb
@@ -19,17 +19,14 @@ class WechatApiUserInfo < BaseService
 
   private
 
-  # NOTE : this is used for the wechat webhook to precreate user accounts
-  # maybe we should move the logic in another library which we connect
-  # on the webhook itself
-  def ensure_menu!
-    SlackDispatcher.new.message("MENU : #{menu_gateway}")
-    menu_gateway
-  end
+  # def ensure_menu!
+  #   SlackDispatcher.new.message("MENU : #{menu_gateway}")
+  #   menu_gateway
+  # end
 
-  def menu_gateway
-    @menu_gateway ||= get_url menu_url
-  end
+  # def menu_gateway
+  #   @menu_gateway ||= get_url menu_url
+  # end
 
   def access_token_gateway
     @access_token_gateway ||= get_url access_token_url
@@ -47,9 +44,9 @@ class WechatApiUserInfo < BaseService
     "https://api.weixin.qq.com/cgi-bin/user/info?access_token=#{access_token}&openid=#{openid}"
   end
 
-  def menu_url
-    "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token=#{access_token}"
-  end
+  # def menu_url
+  #   "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token=#{access_token}"
+  # end
 
   def get_url(url)
     response = Net::HTTP.get(URI.parse(url))
