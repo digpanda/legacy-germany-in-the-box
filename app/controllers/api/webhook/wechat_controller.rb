0f0161df91fd3b357e7c23b9849a4be5231ffbfe
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-14 15:19:00 +0300

    dispatcher added for webhook wechat

--- app/controllers/api/webhook/wechat_controller.rb
+++ app/controllers/api/webhook/wechat_controller.rb
@@ -53,13 +53,18 @@ class Api::Webhook::WechatController < Api::ApplicationController
     SlackDispatcher.new.message("Raw params : #{transmit_data}")
 
     if event == "SCAN"
+      SlackDispatcher.new.message("Event scan detected ...")
       handle_qrcode_callback!
     elsif event == "CLICK"
+      SlackDispatcher.new.message("Event click detected ...")
       if raw_extra_data == "coupon"
+        SlackDispatcher.new.message("Coupon event key.")
         render text: "2017a"
       elsif raw_extra_data == "support"
+        SlackDispatcher.new.message("Support event key.")
         render text: "欢迎您通过微信和我们交流。\n请点击左下角小键盘直接留言，工作时间会在一小时内回复， 非工作时间会定期检查留言并回复。"
       elsif raw_extra_data == "buyerguide"
+        SlackDispatcher.new.message("Buyer guide event key.")
         render text: "---购买下单注意事项---\n1. 将产品添加到购物车后点击手机屏幕右上方进入购物车下单\n2.\t请填写收件人的收件地址，手机号，身份证号码(中国海关通关要求)\n3.\t确定支付前，如果您有活动的打折码请输入打折码，并点击使用\n4.\t点击下单，在我的支付方式里选择付款方式并支付\n\n---付款方式---\n来因盒支持在线支付宝和微信支付，购物费用均以人民币在线支付结算。\n通过微信访问来因盒，支付体验首选微信支付。 支付宝需要拷贝粘贴支付宝链接到手机浏览器里完成支付。\n\n---来因盒产品价格---\n目前来因盒德国礼包的价格均为产品包邮，包税寄到您中国家里的价格\n\n---海关关税---\n来因盒里的所有商品都从德国直接发货至国内，经阳光清关完税， 安全可靠。当前推广期内来因盒来替您缴付所有产品的关税。"
       end
       return
