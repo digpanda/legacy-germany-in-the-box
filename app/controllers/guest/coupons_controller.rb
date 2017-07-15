9cfd0e7870d51434c80d16b216b9216a9721bf93
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-15 12:45:31 +0300

    qrcode added to normal flyer

--- app/controllers/guest/coupons_controller.rb
+++ app/controllers/guest/coupons_controller.rb
@@ -6,10 +6,22 @@ class Guest::CouponsController < ApplicationController
   before_action :set_coupon
 
   def flyer
-    send_data Flyer.new.process_steps(coupon).image.to_blob, :stream => "false", :filename => "test.jpg", :type => "image/jpeg", :disposition => "inline"
+    send_data Flyer.new.process_steps(coupon, qrcode_path).image.to_blob, :stream => "false", :filename => "test.jpg", :type => "image/jpeg", :disposition => "inline"
   end
 
   private
+
+  def qrcode_path
+    if coupon.referrer
+      if wechat_referrer_qrcode.success?
+        wechat_referrer_qrcode.data[:local_file]
+      end
+    end
+  end
+
+  def wechat_referrer_qrcode
+    @wechat_referrer_qrcode ||= WechatReferrerQrcode.new(coupon.referrer).resolve!
+  end
 
   def set_coupon
     @coupon = Coupon.find(params[:id] || params[:coupon_id])
