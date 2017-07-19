d566936070717c402c1bccb1a5a76c6bade929c3
Hao Jiang <hao.jiang@gmail.com>
2017-07-19 15:01:54 +0200

    use larger QR img template

--- app/services/flyer.rb
+++ app/services/flyer.rb
@@ -33,9 +33,9 @@ class Flyer < BaseService
   def process_qrcode(qrcode_path)
     @image = Magick::Image.read("#{Rails.root}/public/images/flyers/qrcode.jpg").first
     append_image =  Magick::Image.read(qrcode_path).first
-    append_image = append_image.resize_to_fit(140, 140)
+    append_image = append_image.resize_to_fit(208, 208)
 
-    image.composite!(append_image, 105, 540, Magick::OverCompositeOp)
+    image.composite!(append_image, 67, 540, Magick::OverCompositeOp)
 
     image.format = "jpeg"
     self
