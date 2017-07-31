0e7e0800cab86e27eca54c019dd68211033be39e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-31 13:04:05 +0200

    referrer provision null removal

---
+++ db/migrate/20170731110225_remove_all_null_referrer_provision.rb
@@ -0,0 +1,14 @@
+class RemoveAllNullReferrerProvision < Mongoid::Migration
+  def self.up
+    ReferrerProvision.all.each do |referrer_provision|
+      puts "Checking ReferrerProvision `#{referrer_provision.id}`"
+      if referrer_provision.provision <= 0.0
+        referrer_provision.delete
+        puts "ReferrerProvision was removed."
+      end
+    end
+  end
+
+  def self.down
+  end
+end
