f22c7854e3e0174f6a96371d3fabd80fa36d3772
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-31 18:40:13 +0200

    provision handler spec added

---
+++ spec/services/provision_handler_spec.rb
@@ -0,0 +1,20 @@
+describe ProvisionHandler do
+
+  context "#refresh!" do
+
+    # order with referrer and which was bought
+    # provision > 0
+    let(:order) { FactoryGirl.create(:order, :with_referrer, status: :paid) }
+
+    it "should add a new provision and then remove it" do
+      # ProvisionHandler.new(order).refresh! <- it's automatically generated on saving
+      expect(order.referrer.provisions.count).to eq(1)
+      expect(order.referrer.provisions.first.provision).to be > 0.0
+      order.status = :cancelled
+      order.save
+      # ProvisionHandler.new(order).refresh! <- it's automatically generated on saving
+      expect(order.referrer.provisions.count).to eq(0)
+    end
+
+  end
+end
