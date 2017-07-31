05afb41c5baa92df2d86754db8ba68be589c9a9f
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-07-31 13:05:36 +0200

    minor migration bugfix

--- db/migrate/20170731110225_remove_all_null_referrer_provision.rb
+++ db/migrate/20170731110225_remove_all_null_referrer_provision.rb
@@ -2,7 +2,7 @@ class RemoveAllNullReferrerProvision < Mongoid::Migration
   def self.up
     ReferrerProvision.all.each do |referrer_provision|
       puts "Checking ReferrerProvision `#{referrer_provision.id}`"
-      if referrer_provision.provision <= 0.0
+      if referrer_provision.provision == 0.0
         referrer_provision.delete
         puts "ReferrerProvision was removed."
       end
