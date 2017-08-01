e6bbae8e610405ee5c8436c558917e39d3bdedcc
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-08-01 11:54:09 +0200

    referrer system change migration slight change

--- db/migrate/20170801093918_order_referrer_field.rb
+++ db/migrate/20170801093918_order_referrer_field.rb
@@ -9,12 +9,12 @@ class OrderReferrerField < Mongoid::Migration
           order.referrer = order.user.parent_referrer
           order.referrer_origin = :user
           order.save(validation: false)
-          puts "Order Referrer was changed to a user one (Referrer `#{order.user.parent_referrer}`)."
+          puts "Order Referrer was changed to a user one (Referrer `#{order.user.parent_referrer.id}`)."
         elsif order.coupon&.referrer
           order.referrer = order.coupon.referrer
           order.referrer_origin = :coupon
           order.save(validation: false)
-          puts "Order Referrer was changed to a coupon one (Referrer `#{order.coupon.referrer}`)."
+          puts "Order Referrer was changed to a coupon one (Referrer `#{order.coupon.referrer.id}`)."
         end
       end
       puts "End of process for this order."
