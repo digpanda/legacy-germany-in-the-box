f878c2b6ef8605cc1376899d6ef806b42c338791
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-08-04 11:31:33 +0200

    search system normalization for inside shops

--- app/controllers/admin/shops/products_controller.rb
+++ app/controllers/admin/shops/products_controller.rb
@@ -17,7 +17,7 @@ class Admin::Shops::ProductsController < ApplicationController
   def index
     respond_to do |format|
       format.html do
-        @products = shop.products.order_by(c_at: :desc).full_text_search(query, match: :any, allow_empty_search: true).paginate(page: current_page, per_page: 10)
+        @products = shop.products.order_by(c_at: :desc).full_text_search(query, match: :all, allow_empty_search: true).paginate(page: current_page, per_page: 10)
       end
       format.csv do
         @products = shop.products.order_by(c_at: :desc)
@@ -26,10 +26,6 @@ class Admin::Shops::ProductsController < ApplicationController
                disposition: 'attachment'
       end
     end
-  end
-
-  def query
-    params.require(:query) if params[:query].present?
   end
 
   def new
@@ -92,6 +88,10 @@ class Admin::Shops::ProductsController < ApplicationController
   end
 
   private
+
+  def query
+    params.require(:query) if params[:query].present?
+  end
 
   def product_params
     params.require(:product).permit!
