e7aab0a91a79690ca2d02e9831140d67183a075b
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-08-11 12:29:45 +0200

    valid url added

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,6 +5,7 @@ class Link
   field :title, type: String
   field :url, type: String
   field :position, type: Integer, default: 0
+  field :valid_url, type: Boolean, default: true
 
   # research system
   search_in :title, :url
