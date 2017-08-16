d236031a255e44a4008180e080fda03d97640238
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-08-16 13:53:43 +0200

    fixing test and avoid external API calls

--- spec/services/weixin_api_ticket_spec.rb
+++ spec/services/weixin_api_ticket_spec.rb
@@ -21,6 +21,10 @@ describe WeixinApiTicket  do
     end
 
     it 'fails resolving the ticket' do
+      
+      allow_any_instance_of(described_class).to receive(:access_token_gateway).and_return(
+        BaseService.new.return_with(:success, access_token: 'random token')
+      )
 
       allow_any_instance_of(described_class).to receive(:ticket_gateway).and_return(
         'errcode' => '1',
