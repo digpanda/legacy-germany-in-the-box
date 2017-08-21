474cc9b52f7ad2bd27dd366cd6559e79f2bc1130
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-08-21 13:21:33 +0200

    brand details added with html activation

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -4,8 +4,9 @@ class Brand
   strip_attributes
 
   field :name, type: String, localize: true
+  field :html_desc, type: String, localize: true
   field :position, type: Integer, default: 0
-  
+
   has_many :products, inverse_of: :brand
 
 end
