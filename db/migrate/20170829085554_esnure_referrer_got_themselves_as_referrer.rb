e6e642b544a5dd8d879f10aa883becf5f8c2708e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-08-29 11:22:49 +0200

    change migration logic and force referrer as himself each time

--- db/migrate/20170829085554_esnure_referrer_got_themselves_as_referrer.rb
+++ db/migrate/20170829085554_esnure_referrer_got_themselves_as_referrer.rb
@@ -3,10 +3,8 @@ class EsnureReferrerGotThemselvesAsReferrer < Mongoid::Migration
     puts "Looking through all the referrers ..."
     Referrer.all.each do |referrer|
       tour_guide_user = referrer.user
-      unless tour_guide_user.parent_referrer
-        tour_guide_user.update(parent_referrer: referrer)
-        puts "Updating Customer #{tour_guide_user.id} which is Referrer #{referrer.id}"
-      end
+      tour_guide_user.update(parent_referrer: referrer)
+      puts "Updating Customer #{tour_guide_user.id} which is Referrer #{referrer.id}"
     end
   end
 
