06ffbba59f524eaee933f235d28ec941a3461b35
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-08-29 11:34:04 +0200

    trying to find the issue now

--- db/migrate/20170829085554_esnure_referrer_got_themselves_as_referrer.rb
+++ db/migrate/20170829085554_esnure_referrer_got_themselves_as_referrer.rb
@@ -2,7 +2,7 @@ class EsnureReferrerGotThemselvesAsReferrer < Mongoid::Migration
   def self.up
     puts "Looking through all the referrers ..."
     Referrer.all.each do |referrer|
-      save = referrer.user.update(parent_referrer: referrer)
+      save = referrer.user.update!(parent_referrer: referrer)
       puts "Updating Customer #{referrer.user.id} (#{referrer.user.decorate.full_name}) which is Referrer #{referrer.id} (operation : #{save})"
     end
   end
