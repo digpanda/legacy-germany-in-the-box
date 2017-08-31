aa95c98eb2e728b190c93eecebc3c83cfc96e56a
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-08-31 16:11:13 +0200

    change migration published at date (more consistent on c_at)

--- db/migrate/20170831140456_ensure_link_published_at.rb
+++ db/migrate/20170831140456_ensure_link_published_at.rb
@@ -2,8 +2,8 @@ class EnsureLinkPublishedAt < Mongoid::Migration
   def self.up
     Link.all.each do |link|
       unless link.published_at
-        puts "For link #{link.id}, `published_at` will automatically be set to #{link.u_at}"
-        link.published_at = link.u_at
+        puts "For link #{link.id}, `published_at` will automatically be set to #{link.c_at}"
+        link.published_at = link.c_at
         link.save
       end
     end
