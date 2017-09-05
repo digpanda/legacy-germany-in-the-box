fa6ae4cf4e86b15aa62bf12f2d0b11f5cb9c0260
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-05 11:00:43 +0200

    tracking handler added to the system

--- app/controllers/admin/orders/order_trackings_controller.rb
+++ app/controllers/admin/orders/order_trackings_controller.rb
@@ -30,26 +30,12 @@ class Admin::Orders::OrderTrackingsController < ApplicationController
 
   # NOTE : it is different from update
   # it calls the API and refresh the model accordingly.
-  # TODO : could be put into a service TrackingHandler to be more clean
   def refresh
-    # BIG NOTE : LOGISTIC PARTNER IS HARDCODED AS MKPOST, WE NEED TO CHANGE THAT AFTER (it has to depend on the order)
-    tracking = KuaidiApi.new(tracking_id: order.order_tracking&.unique_id, logistic_partner: :mkpost).perform!
-
-    if tracking.success?
-
-      order_tracking.update(
-        state: tracking.data[:current_state],
-        histories: tracking.data[:current_history],
-        refreshed_at: Time.now
-      )
-
-      if order_tracking.errors.any?
-        flash[:error] = "The tracking was not updated (#{order_tracking.errors.full_messages.join(', ')})"
-      else
-        flash[:success] = "The tracking was updated successfully."
-      end
+    tracking_handler = TrackingHandler.new(order).refresh!
+    if tracking_handler.success?
+      flash[:success] = "Tracking was successfully refreshed"
     else
-      flash[:error] = tracking.error
+      flash[:error] = tracking_handler.error
     end
     redirect_to navigation.back(1)
   end
--- app/services/tracking_handler.rb
+++ app/services/tracking_handler.rb
@@ -1,3 +1,37 @@
 class TrackingHandler
+  attr_reader :order
+
+  def initialize(order)
+    @order = order
+  end
+
+  def refresh!
+    if kuaidi_api.success?
+      if update_order_tracking!
+        return return_with(:success)
+      else
+        return return_with(:error, order_tracking.errors.full_messages.join(', '))
+      end
+    else
+      return_with(:error, kuaidi_api.error)
+    end
+  end
+
+  def update_order_tracking!
+    order_tracking.update(
+      state: kuaidi_api.data[:current_state],
+      histories: kuaidi_api.data[:current_history],
+      refreshed_at: Time.now
+    )
+  end
+
+  # BIG NOTE : LOGISTIC PARTNER IS HARDCODED AS MKPOST, WE NEED TO CHANGE THAT AFTER (it has to depend on the order)
+  def kuaidi_api
+    @kuaidi_api ||= KuaidiApi.new(tracking_id: order_tracking.unique_id, logistic_partner: :mkpost).perform!
+  end
+
+  def order_tracking
+    @order_tracking ||= order.order_tracking || OrderTracking.create(order: order)
+  end
 
 end
