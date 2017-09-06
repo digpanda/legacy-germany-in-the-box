0f0b0f835ae27717ab52c51713ad07bed9e14f23
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-06 17:24:37 +0200

    another test of migration in staging ...

--- db/migrate/20170906144242_keenio_user_was_registered_dispatch_bis.rb
+++ db/migrate/20170906144242_keenio_user_was_registered_dispatch_bis.rb
@@ -2,8 +2,11 @@ class KeenioUserWasRegisteredDispatchBis < Mongoid::Migration
   def self.up
     EventDispatcher.new.activate!
     User.where(role: :customer).all.each do |user|
-      EventDispatcher.new.customer_was_registered(user).dispatch!
-      puts "Dispatched User #{user.id}"
+      callback = EventDispatcher.new.customer_was_registered(user).dispatch!
+      puts "Dispatched User #{user.id} (callback: `#{callback}`)"
+      unless callback['created']
+        raise Exception, "Could not dispatch the user properly."
+      end
     end
   end
 
