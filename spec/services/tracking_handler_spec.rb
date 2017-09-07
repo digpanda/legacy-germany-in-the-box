4d6d9efd15ac9e617e79d10b09286f7a855741cb
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-07 16:24:36 +0200

    tests tracking handler fixed

--- spec/services/tracking_handler_spec.rb
+++ spec/services/tracking_handler_spec.rb
@@ -7,7 +7,7 @@ describe TrackingHandler do
 
       before(:each) do
         # automatic true from the API call
-        allow_any_instance_of(described_class).to receive(:kuaidi_api).and_return(kuaidi_valid_response)
+        allow_any_instance_of(described_class).to receive(:api_performed).and_return(kuaidi_valid_response)
       end
 
       it 'update the tracking successfully' do
@@ -21,7 +21,7 @@ describe TrackingHandler do
 
       it 'returns a wrong API result' do
 
-        allow_any_instance_of(described_class).to receive(:kuaidi_api).and_return(kuaidi_wrong_response)
+        allow_any_instance_of(described_class).to receive(:api_performed).and_return(kuaidi_wrong_response)
         refreshed = subject.refresh!
         expect(refreshed.success?).to eq(false)
 
@@ -32,7 +32,7 @@ describe TrackingHandler do
         let(:order_tracking) { FactoryGirl.create(:order_tracking, :with_recent_refresh) }
 
         it 'use the cached version' do
-          
+
           refreshed = subject.refresh!
           expect(refreshed.success?).to eq(true)
           expect(order_tracking.refreshed_at).to be < 1.minute.ago # it did not really refresh
