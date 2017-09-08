dd6740cf3666ebfcf16ad29a68302a6407d88054
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-08 16:44:37 +0200

    refactoring home controller test

--- spec/controllers/guest/categories_controller_spec.rb
+++ spec/controllers/guest/categories_controller_spec.rb
@@ -2,23 +2,23 @@ describe Guest::CategoriesController, type: :controller do
   render_views
 
   let(:category) { FactoryGirl.create(:category) }
+
   describe '#show' do
+
+    subject { get :show, id: category }
+
     context "as guest" do
-      it do
-        get :show, id: category
-        expect(response).to render_template('show')
-      end
+      it { is_expected.to render_template('show') }
     end
 
     # we test the restrict_to as well
     context "as admin" do
       let(:current_user) { FactoryGirl.create(:admin) }
       before(:each) { login_admin current_user }
-      it do
-        get :show, id: category
-        expect(response).not_to have_http_status(200)
-      end
+
+      it { is_expected.not_to have_http_status(200) }
     end
+
   end
 
 end
