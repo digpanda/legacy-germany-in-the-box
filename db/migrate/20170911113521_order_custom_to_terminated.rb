c0840ed0d893b3bbba2ba3a22a93d4b1ec436763
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-11 13:48:15 +0200

    more migration details

--- db/migrate/20170911113521_order_custom_to_terminated.rb
+++ db/migrate/20170911113521_order_custom_to_terminated.rb
@@ -2,6 +2,7 @@ class OrderCustomToTerminated < Mongoid::Migration
   def self.up
     Order.all.each do |order|
       if order.status == :custom_checking || order.status == :custom_checkable || order.status == :shipped
+        puts "Changing #{order.id} status from `#{order.status}` to `#{terminated}`"
         order.status = :terminated
         order.save!(validate: false)
       end
