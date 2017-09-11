dbd434b234bbb5efe58d1ad4295d48d1cb977e55
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-11 14:23:21 +0200

    condition strategy change

--- db/migrate/20170911113521_order_custom_to_terminated.rb
+++ db/migrate/20170911113521_order_custom_to_terminated.rb
@@ -2,7 +2,7 @@ class OrderCustomToTerminated < Mongoid::Migration
   def self.up
     Order.all.each do |order|
       puts "Processing #{order.id} ..."
-      if order.status == :custom_checking || order.status == :custom_checkable || order.status == :shipped
+      if [:custom_checking, :custom_checkable, :shipped].include? order.status
         puts "Changing #{order.id} status from `#{order.status}` to `#{terminated}`"
         order.status = :terminated
         order.save!(validate: false)
