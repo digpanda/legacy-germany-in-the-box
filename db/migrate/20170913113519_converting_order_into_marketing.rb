0e675b7116163146566400ca7502b44ad393bf23
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-13 13:41:23 +0200

    trying troubleshoot non working migration

--- db/migrate/20170913113519_converting_order_into_marketing.rb
+++ db/migrate/20170913113519_converting_order_into_marketing.rb
@@ -1,7 +1,7 @@
 class ConvertingOrderIntoMarketing < Mongoid::Migration
   def self.up
     Order.all do |order|
-      if order.c_at > 3.months.ago && order.status == :paid
+      if (order.c_at > 3.months.ago) && (order.status == :paid)
         puts "Order `#{order.id}` was created on #{order.c_at} and has the status `#{order.status}` will be converted."
         order.marketing = true
         order.save(validate: false)
