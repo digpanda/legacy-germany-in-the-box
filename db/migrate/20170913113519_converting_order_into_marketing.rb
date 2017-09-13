81c5c40b517f5523d4080526dff537483e27d9be
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-13 14:18:41 +0200

    another migration fix for tracking

--- db/migrate/20170913113519_converting_order_into_marketing.rb
+++ db/migrate/20170913113519_converting_order_into_marketing.rb
@@ -1,8 +1,8 @@
 class ConvertingOrderIntoMarketing < Mongoid::Migration
   def self.up
     Order.all.each do |order|
-      puts "Order convertible ? TIME : #{(order.c_at > 3.months.ago)}, STATUS : #{order.status}, TRACKING : #{!order.order_tracking}"
-      if (order.c_at > 3.months.ago) && (order.status == :paid) && (!order.order_tracking)
+      puts "Order convertible ? TIME : #{(order.c_at > 3.months.ago)}, STATUS : #{order.status}, TRACKING : #{!order.order_tracking&.delivery_id}"
+      if (order.c_at > 3.months.ago) && (order.status == :paid) && (!order.order_tracking&.delivery_id)
         puts "Order `#{order.id}` was created on #{order.c_at} and has the status `#{order.status}` will be converted."
         order.marketing = true
         order.save(validate: false)
