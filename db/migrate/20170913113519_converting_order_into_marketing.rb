15e38d7221716fab004702d20c7b42b1e83119f3
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-13 13:54:08 +0200

    again

--- db/migrate/20170913113519_converting_order_into_marketing.rb
+++ db/migrate/20170913113519_converting_order_into_marketing.rb
@@ -1,7 +1,7 @@
 class ConvertingOrderIntoMarketing < Mongoid::Migration
   def self.up
     Order.all do |order|
-      puts "Order convertible ? TIME : #{(order.c_at > 3.months.ago)}, STATUS : #{order.status == :paid}"
+      puts "Order convertible ? TIME : #{(order.c_at > 3.months.ago)}, STATUS : #{order.status}"
       if (order.c_at > 3.months.ago) && (order.status == :paid)
         puts "Order `#{order.id}` was created on #{order.c_at} and has the status `#{order.status}` will be converted."
         order.marketing = true
