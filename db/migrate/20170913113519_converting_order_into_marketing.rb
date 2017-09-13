1af8fb7a279dcae817de37e9d16c4c2c1aabe475
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-13 14:13:26 +0200

    change typo on migration

--- db/migrate/20170913113519_converting_order_into_marketing.rb
+++ db/migrate/20170913113519_converting_order_into_marketing.rb
@@ -1,7 +1,7 @@
 class ConvertingOrderIntoMarketing < Mongoid::Migration
   def self.up
     Order.all.each do |order|
-      puts "Order convertible ? TIME : #{(order.c_at > 3.months.ago)}, STATUS : #{order.status}, TRACKING : #{order.tracking.present?}"
+      puts "Order convertible ? TIME : #{(order.c_at > 3.months.ago)}, STATUS : #{order.status}, TRACKING : #{!order.tracking}"
       if (order.c_at > 3.months.ago) && (order.status == :paid) && (!order.order_tracking)
         puts "Order `#{order.id}` was created on #{order.c_at} and has the status `#{order.status}` will be converted."
         order.marketing = true
