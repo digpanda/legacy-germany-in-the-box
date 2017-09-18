ac36d852fc9869b24d94daa65574c804396b504f
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-18 12:18:05 +0200

    vcr removed from specific test

--- spec/controllers/guest/referrers_controller_spec.rb
+++ spec/controllers/guest/referrers_controller_spec.rb
@@ -1,9 +1,6 @@
 describe Guest::ReferrersController, type: :controller do
   render_views
-
-  before(:each) { VCR.turn_on! }
-  after(:each) { VCR.turn_off! }
-
+  
   let(:referrer) { FactoryGirl.create(:customer, :with_referrer).referrer }
 
   describe '#qrcode' do
