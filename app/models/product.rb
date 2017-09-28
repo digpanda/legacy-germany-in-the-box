8ae0356c7896e691d7f7c61fc3d820f06525915c
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-09-28 11:14:04 +0200

    cover removal crashes the sample data (investigate later)

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product
   field :name, type: String, localize: true
   slug :name
 
-  # field :cover, type: String # deprecated ?
+  field :cover, type: String # deprecated ?
   field :desc, type: String, localize: true
   field :desc_below, type: String, localize: true
   field :status, type: Boolean, default: true
