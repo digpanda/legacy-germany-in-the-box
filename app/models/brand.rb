283abccd57497bf5d2abc844213bfa205d38d0fd
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-04 11:57:24 +0200

    query ready for services brands

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -14,11 +14,16 @@ class Brand
   has_many :products, inverse_of: :brand
 
   scope :with_package_sets, -> { where(:id.in => package_set_brand_ids) }
+  scope :with_services, -> { where(:id.in => services_brand_ids) }
   scope :used_as_filters, -> { where(:used_as_filter.ne => false) }
 
   private
 
     def self.package_set_brand_ids
       PackageSet.active.map(&:brands).flatten.map(&:id)
+    end
+
+    def self.services_brand_ids
+      Service.active.map(&:brand).map(&:id)
     end
 end
