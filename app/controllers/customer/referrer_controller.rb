fb51ed24dd484016404b9025ea9c6732f22f97e2
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-05 17:28:41 +0200

    minor referrer protection bugfix

--- app/controllers/customer/referrer_controller.rb
+++ app/controllers/customer/referrer_controller.rb
@@ -47,7 +47,7 @@ class Customer::ReferrerController < ApplicationController
     def valid_referrer?
       unless current_user&.referrer?
         flash[:error] = I18n.t('general.not_allowed_section')
-        redirect_to navigation.back(1)
+        redirect_to root_path
         false
       end
     end
