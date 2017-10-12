a50581f1ba2c4b83ecdabb53f3684ab96537110f
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-12 12:34:09 +0200

    lots of work and refactoring on the graph system

---
+++ app/services/chart/draw.rb
@@ -0,0 +1,66 @@
+class Chart < BaseService
+  class Draw
+    attr_reader :chart, :color, :label, :fill, :positions, :values, :type
+
+    def initialize(chart, color:, label:, fill: false, type:nil)
+      @chart = chart
+      @color = color
+      @label = label
+      @fill = fill
+      @values = []
+      @positions = {}
+      # we can specify types for each line / drawing in a mixed chart
+      @type = chart.type unless type
+    end
+
+    def store
+      chart.entries << { positions: positions, dataset: dataset }
+      chart
+    end
+
+    def dataset
+      {
+        label: label,
+        type: type,
+        borderColor: border_color,
+        backgroundColor: background_color,
+        fill: fill
+      }
+    end
+
+    def border_color
+      case color
+      when :blue
+        '#3e95cd'
+      when :purple
+        '#8e5ea2'
+      when :green
+        '#3cba9f'
+      when :red
+        '#c45850'
+      else
+        '#000000'
+      end
+    end
+
+    def background_color
+      case color
+      when :blue
+        'rgba(52,152,219, 0.50)'
+      when :purple
+        'rgba(142,94,162, 0.50)'
+      when :green
+        'rgba(60,186,159, 0.50)'
+      when :red
+        'rgba(255, 99, 132, 0.50)'
+      else
+        'rgb(0,0,0, 0.50)'
+      end
+    end
+
+    def data(position:, value:)
+      @positions[position] = value
+      self
+    end
+  end
+end
