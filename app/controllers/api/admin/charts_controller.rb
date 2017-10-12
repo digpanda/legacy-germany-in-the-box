c5a223377766847daeaadf6a617976f57493138a
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-12 13:16:52 +0200

    sample seems stable

--- app/controllers/api/admin/charts_controller.rb
+++ app/controllers/api/admin/charts_controller.rb
@@ -6,22 +6,20 @@ class Api::Admin::ChartsController < Api::ApplicationController
           json: { success: true, data: total_users_hash }.to_json
   end
 
-  def sample
-    chart = Chart.new(title: 'Title', type: :bar)
-    draw = chart.draw(color: :green, label: 'Label 1')
-    draw.data(position: 'Week 1', value: 20).data(position: 'Week 2', value: 5).data(position: 'Week 3', value: 35)
-    draw.store
-    draw = chart.draw(color: :red, label: 'Label 2', type: :line, fill: true)
-    draw.data(position: 'Week 0', value: 15).data(position: 'Week 2', value: 30)
-    draw.store
-    chart.render
-  end
-
   private
 
-
-    # NOTE : need to make a service for this.
     def total_users_hash
       sample
+    end
+
+    def sample
+      chart = Chart.new(title: 'Title', type: :bar)
+      draw = chart.draw(color: :green, label: 'Label 1')
+      draw.data(position: 'Week 1', value: 20).data(position: 'Week 2', value: 5).data(position: 'Week 3', value: 35)
+      draw.store
+      draw = chart.draw(color: :red, label: 'Label 2', type: :line, fill: true)
+      draw.data(position: 'Week 0', value: 15).data(position: 'Week 2', value: 30)
+      draw.store
+      chart.render
     end
 end
