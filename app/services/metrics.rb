ce79ae4580c8bde7149843b1c051b311fd4ac885
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-13 12:50:37 +0200

    order graph totaly done and ready

--- app/services/metrics.rb
+++ app/services/metrics.rb
@@ -15,23 +15,27 @@ class Metrics < BaseService
 
   protected
 
+    class << self
+      # NOTE : this allows us to draw different lines datasets within a graph
+      # right from the #render from any method in a subclass.
+      # e.g. draw(:new_users_per_month, label: 'New users', color: :light)
+      def draw(metric, *args)
+        draw = chart.draw(*args)
+        self.send(metric).each do |metric|
+          draw.data(position: metric.first, value: metric.last)
+        end
+        draw.store
+      end
+
+      # NOTE : settings is set in each subclass in the initializer
+      def chart
+        @chart ||= Chart.new(settings)
+      end
+    end
+
+  private
+
     def to_call
       "Metrics::#{metric.to_s.camelize}".constantize
-    end
-
-    # NOTE : this allows us to draw different lines datasets within a graph
-    # right from the #render from any method in a subclass.
-    # e.g. draw(:new_users_per_month, label: 'New users', color: :light)
-    def draw(metric, *args)
-      draw = chart.draw(*args)
-      self.send(metric).each do |metric|
-        draw.data(position: metric.first, value: metric.last)
-      end
-      draw.store
-    end
-
-    # NOTE : settings is set in each subclass in the initializer
-    def chart
-      @chart ||= Chart.new(settings)
     end
 end
