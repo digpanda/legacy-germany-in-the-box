3fb547131260520f3973773e06b6484488990ce0
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-13 11:20:24 +0200

    optimization of the metric library and abstraction + cache (done)

--- app/services/metrics.rb
+++ app/services/metrics.rb
@@ -8,12 +8,30 @@ class Metrics < BaseService
   end
 
   def render
-    #Rails.cache.fetch("cache-metric-#{metric}" :expires_in => CACHE_EXPIRATION) do
+    Rails.cache.fetch("cache-metric-#{metric}" :expires_in => CACHE_EXPIRATION) do
       to_call.new.render if defined?(to_call)
-    #end
+    end
   end
 
-  def to_call
-    "Metrics::#{metric.to_s.camelize}".constantize
-  end
+  protected
+
+    def to_call
+      "Metrics::#{metric.to_s.camelize}".constantize
+    end
+
+    # NOTE : this allows us to draw different lines datasets within a graph
+    # right from the #render from any method in a subclass.
+    # e.g. draw(:new_users_per_month, label: 'New users', color: :light)
+    def draw(metric, *args)
+      draw = chart.draw(*args)
+      self.send(metric).each do |metric|
+        draw.data(position: metric.first, value: metric.last)
+      end
+      draw.store
+    end
+
+    # NOTE : those variable are set in each subclass in the initializer
+    def chart
+      @chart ||= Chart.new(title: title, type: type, vertical_label: vertical_label)
+    end
 end
--- app/services/metrics/total_users.rb
+++ app/services/metrics/total_users.rb
@@ -1,9 +1,11 @@
 class Metrics < BaseService
-  class TotalUsers
-    TITLE = '# of Users'.freeze
-    VERTICAL_LABEL = 'Demography'.freeze
+  class TotalUsers < Metrics
+    attr_reader :title, :type, :vertical_label
 
     def initialize
+      @title = '# of Users'
+      @type = :bar
+      @vertical_label = 'Demography'
     end
 
     def render
@@ -15,21 +17,9 @@ class Metrics < BaseService
 
     private
 
-      def draw(metric, *args)
-        draw = chart.draw(*args)
-        self.send(metric).each do |metric|
-          draw.data(position: metric.first, value: metric.last)
-        end
-        draw.store
-      end
-
-      def chart
-        @chart ||= Chart.new(title: TITLE, type: :bar, vertical_label: VERTICAL_LABEL)
-      end
-
       # NOTE : metrics must have a format such as {'Entry 1' => 1, 'Entry 2' => 2}
       # this norm will be automatically understood and drawn by the system.
-  
+
       def new_users_per_month
         @new_users_per_month ||= begin
           User.all.order(c_at: :asc).group_by do |user|
@@ -51,7 +41,5 @@ class Metrics < BaseService
           end
         end
       end
-
   end
-
 end
