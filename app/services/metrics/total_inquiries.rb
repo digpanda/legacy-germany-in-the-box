f644a6a66bfaac65e32bc36078121c652d6bf42b
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-16 14:43:32 +0100

    fix inquiries metrics

--- app/services/metrics/total_inquiries.rb
+++ app/services/metrics/total_inquiries.rb
@@ -12,7 +12,6 @@ class Metrics < BaseService
     def render
       draw(:new_inquiries_per_month, label: 'New inquiries', color: :light)
       draw(:total_inquiries_per_month, label: 'Total inquiries', color: :blue, type: :line)
-      draw(:total_paid_inquiries_per_month, label: 'Paid inquiries', color: :red, type: :line, fill: false)
 
       chart.render
     end
@@ -37,18 +36,6 @@ class Metrics < BaseService
         @total_inquiries_per_month ||= begin
           counter = 0
           Inquiry.all.order(c_at: :asc).group_by do |user|
-            user.c_at.strftime('%Y-%m')
-          end.reduce({}) do |acc, group|
-            counter += group.last.count
-            acc.merge({"#{group.first}": counter})
-          end
-        end
-      end
-
-      def total_paid_inquiries_per_month
-        @total_paid_inquiries_per_month ||= begin
-          counter = 0
-          Inquiry.bought.order(c_at: :asc).group_by do |user|
             user.c_at.strftime('%Y-%m')
           end.reduce({}) do |acc, group|
             counter += group.last.count
