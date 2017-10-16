9dd62c9223643f95467789211af3b0165dd47365
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-16 15:26:54 +0100

    bugfix date user

--- app/services/metrics/referrer_children_users.rb
+++ app/services/metrics/referrer_children_users.rb
@@ -25,7 +25,7 @@ class Metrics < BaseService
       def children_per_week
         @children_per_week ||= begin
           referrer.children_users.order(parent_referred_at: :asc).group_by do |user|
-            user.c_at.strftime('%Y : Week %W')
+            user.parent_referred_at.strftime('%Y : Week %W')
           end.reduce({}) do |acc, group|
             acc.merge({"#{group.first}": group.last.count})
           end
@@ -36,7 +36,7 @@ class Metrics < BaseService
         @total_children_per_week ||= begin
           counter = 0
           referrer.children_users.order(parent_referred_at: :asc).group_by do |user|
-            user.c_at.strftime('%Y : Week %W')
+            user.parent_referred_at.strftime('%Y : Week %W')
           end.reduce({}) do |acc, group|
             counter += group.last.count
             acc.merge({"#{group.first}": counter})
