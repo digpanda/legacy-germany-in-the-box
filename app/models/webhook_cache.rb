79563f04b784ba163f81035290027d06da0d4a99
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-18 15:13:46 +0100

    minor fix on model cache

--- app/models/webhook_cache.rb
+++ app/models/webhook_cache.rb
@@ -5,6 +5,6 @@ class WebhookCache
   field :key, type: String, presence: true, uniqueness: true
   field :section, type: Symbol
 
-  scope :cached?, -> (key) { where(cache: key) }
+  scope :cached?, -> (key) { where(key: key).count > 0 }
 
 end
