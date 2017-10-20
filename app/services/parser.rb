81bd476c001386ed8fc12ccc00a5c8d0698cd74c
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-20 14:19:14 +0100

    file rollback

--- app/services/parser.rb
+++ app/services/parser.rb
@@ -23,7 +23,7 @@ class Parser
       # to_hash RestClient.post(url, upload: { file: File.new(file, 'rb'), multipart: true })
       # http://local.dev:3000/images/logo.png
       # "#{Rails.root}/public/images/no_image_available.jpg"
-      rest_result = RestClient.post(url, upload: { file: open(file).read, multipart: true })
+      rest_result = RestClient.post(url, upload: { file: open(file), multipart: true })
       SlackDispatcher.new.message("REST RESULT #{rest_result}")
       to_hash rest_result
     rescue Exception => exception
