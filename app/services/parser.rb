1aedf70e1edcfcc6dc604657a58462381023649f
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-20 14:08:17 +0100

    alternative technicé

--- app/services/parser.rb
+++ app/services/parser.rb
@@ -23,9 +23,7 @@ class Parser
       # to_hash RestClient.post(url, upload: { file: File.new(file, 'rb'), multipart: true })
       # http://local.dev:3000/images/logo.png
       # "#{Rails.root}/public/images/no_image_available.jpg"
-      SlackDispatcher.new.message("FILE TAKEN IS : #{file}")
-      SlackDispatcher.new.message("OPEN IS : #{open(file)}")
-      rest_result = RestClient.post(url, upload: { file: open(file), multipart: true })
+      rest_result = RestClient.post(url, upload: { file: open(file).read, multipart: true })
       SlackDispatcher.new.message("REST RESULT #{rest_result}")
       to_hash rest_result
     rescue Exception => exception
