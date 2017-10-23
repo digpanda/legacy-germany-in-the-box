77628c875f25da406973d39923fe02739c433a59
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-23 15:51:37 +0200

    package set auto add system on cart

--- app/controllers/customer/cart_controller.rb
+++ app/controllers/customer/cart_controller.rb
@@ -8,26 +8,29 @@ class Customer::CartController < ApplicationController
   authorize_resource class: false
 
   def show
-    # if params[:product_id]
-    #   product = Product.where(id: params[:product_id]).first
-    #
-    # end
-    #
-    # # handling auto adding of package set
-    # package_set = PackageSet.find(params[:package_set_id]) if params[:package_set_id]
-    # if package_set
-    #
-    #   order = cart_manager.order(shop: package_set.shop)
-    #   order_maker = OrderMaker.new(identity_solver, order)
-    #   add = order_maker.package_set(package_set).refresh!(1, increment: true)
-    #   if add.success?
-    #     cart_manager.store(order)
-    #   end
-    # end
-
+    handle_package_set_add
   end
 
   private
+
+    # NOTE : maybe we should make another layer to handle
+    # adding package set into orders / carts automatically
+    def handle_package_set_add
+      if package_set
+        order = cart_manager.order(shop: package_set.shop)
+        order_maker = OrderMaker.new(identity_solver, order)
+        add = order_maker.package_set(package_set).refresh!(1, increment: true)
+        if add.success?
+          cart_manager.store(order)
+        end
+      end
+    end
+
+    def package_set
+      PackageSet.find(params[:package_set_id]) if params[:package_set_id]
+    rescue Mongoid::Errors::DocumentNotFound
+      # we need to rescue to avoid crashing the application
+    end
 
     def set_cart
       @cart = cart_manager.current_cart
