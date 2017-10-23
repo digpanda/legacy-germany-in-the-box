4ba1155cf3db1b478f4d6319d78bf72c8add4db0
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-10-23 11:37:31 +0200

    change to test exact same file

--- app/services/wechat_api_media.rb
+++ app/services/wechat_api_media.rb
@@ -25,7 +25,7 @@ class WechatApiMedia < BaseService
 
     def media_gateway
       SlackDispatcher.new.message("SENDING LOGO TO SEE WHAT HAPPENS")
-      @media_gateway ||= Parser.post_media(url, "https://germanyinthebox.com/images/logo.png") #Parser.post_media url, target
+      @media_gateway ||= Parser.post_media(url, "https://www.germanyinthebox.com/images/no_image_available.jpg") #Parser.post_media url, target
     end
 
     def url
