94c14aa29a8cc893029c6a1122ebcfcb312f2265
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-10 14:23:25 +0100

    refactoring under progress, very good optimizations so far

--- app/services/wechat_bot/exchange/process.rb
+++ app/services/wechat_bot/exchange/process.rb
@@ -5,23 +5,26 @@ module WechatBot
       # load the requests / responses
       # it can be changed to any namespace / class
       BASE_NAMESPACE = Scheme
+      MATCH_WILDCARD = ''
 
       def perform
+        return true unless matches_base? == false
+        return true unless matches_breakpoints? == false
+        false
+      end
 
-        subclasses(BASE_NAMESPACE).each do |subclass|
-          return true unless process_match(subclass) == false
-        end
+      private
 
-        # we will do the same from memory breakpoint now
+      def matches_breakpoints?
         breakpoints.fetch.each do |breakpoint|
           response = process_match(breakpoint.class_trace.constantize)
+          # the logic here is even if the response is false (boolean)
+          # we lock the system as if it succeeded
+          # to avoid firing other events, while giving the chance
+          # to the same sequence to repeat itself on request
           if response == false
-            # the logic here is even if the response is false (boolean)
-            # we lock the system as if it succeeded
-            # to avoid firing other events, while giving the chance
-            # to the same sequence to repeat itself on request
             return true
-          else
+          elsif response != :not_processed
             breakpoint.delete
             return response
           end
@@ -29,7 +32,17 @@ module WechatBot
         false
       end
 
-      private
+      def matches_base?
+        subclasses(BASE_NAMESPACE).each do |subclass|
+          response = process_match(subclass)
+          if response == false
+            return true
+          elsif response != :not_processed
+            return response
+          end
+        end
+        false
+      end
 
       # take a class and try to solve the request with all its subclasses
       # if one subclass matches we run it completely and return its #response
@@ -37,14 +50,13 @@ module WechatBot
       def process_match(class_match)
         class_instance = instance(class_match)
         class_request = class_instance.request
-
         # we check if it matches
-        if class_request == request
+        if class_request == request || class_request == MATCH_WILDCARD
           # we insert all its subclasses
           insert_subclasses(class_match)
           return class_instance.response
         end
-        false
+        :not_processed
       end
 
       # we will insert in database all the subclasses of `mainclass`
