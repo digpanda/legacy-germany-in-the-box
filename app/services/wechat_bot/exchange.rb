3f3b6b8fca0f33abc14be0302025230c42b2b326
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-13 17:16:38 +0100

    another attempt to debug

--- app/services/wechat_bot/exchange.rb
+++ app/services/wechat_bot/exchange.rb
@@ -1,8 +1,7 @@
 # we make sure all is loaded to get the constants and subclasses
 # we start from wechat_bot to avoid load mismatch conflicts
-require 'wechat_bot'
-
 Dir["#{File.dirname(__FILE__)}/exchange/**/*.rb"].each do |file|
+  puts "requiring #{file}"
   require file
 end
 
