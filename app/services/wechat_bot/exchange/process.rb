754d14c9d95a62a362458a37bc1d33ab487d3ad5
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-13 16:09:32 +0100

    minor change

--- app/services/wechat_bot/exchange/process.rb
+++ app/services/wechat_bot/exchange/process.rb
@@ -1,4 +1,4 @@
-# require 'wechat_bot'
+require 'wechat_bot'
 
 # we manage the memory of the bot and trigger different events depending  on what the customer transmit
 # so far it's used only with Wechat Bot but could be astracted elsewhere later on by changing it a little bit.
