3735546a9b0a5f1278590131ca208d8f4e8f68dd
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-13 16:31:46 +0100

    require change

--- app/services/wechat_bot/exchange.rb
+++ app/services/wechat_bot/exchange.rb
@@ -1,6 +1,7 @@
 # we make sure all is loaded to get the constants and subclasses
 # we start from wechat_bot to avoid load mismatch conflicts
 require 'wechat_bot'
+
 Dir["#{File.dirname(__FILE__)}/**/*.rb"].each do |file|
   require file
 end
