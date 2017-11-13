773753b5c3bbcc21ab67a0c12a0ff4f9804ca6bb
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-13 17:39:21 +0100

    typo

--- app/services/wechat_bot.rb
+++ app/services/wechat_bot.rb
@@ -1,6 +1,6 @@
 # we make sure all is loaded to get the constants and subclasses
 # we start from wechat_bot to avoid load mismatch conflicts
-unless Rails.staging? || Rails.production?
+unless Rails.env.staging? || Rails.env.production?
   # no auto load if dev or test
   Dir["#{File.dirname(__FILE__)}/wechat_bot/**/*.rb"].each do |file|
     require file
