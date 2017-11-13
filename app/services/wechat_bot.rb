15d21c5d5a561bb2ae6aa2913b690820f21c451e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-13 17:25:13 +0100

    requiring moved on up level wechatbot

--- app/services/wechat_bot.rb
+++ app/services/wechat_bot.rb
@@ -1,2 +1,9 @@
+# we make sure all is loaded to get the constants and subclasses
+# we start from wechat_bot to avoid load mismatch conflicts
+Dir["#{File.dirname(__FILE__)}/**/*.rb"].each do |file|
+  puts "requiring #{file}"
+  require file
+end
+
 module WechatBot
 end
--- app/services/wechat_bot/exchange.rb
+++ app/services/wechat_bot/exchange.rb
@@ -1,10 +1,3 @@
-# we make sure all is loaded to get the constants and subclasses
-# we start from wechat_bot to avoid load mismatch conflicts
-Dir["#{File.dirname(__FILE__)}/exchange/**/*.rb"].each do |file|
-  puts "requiring #{file}"
-  require file
-end
-
 module WechatBot
   module Exchange
   end
