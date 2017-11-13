3ca2014a1fca9a41879f18b1926d0a9678d0dfba
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-13 14:40:14 +0100

    another attempt

--- app/services/wechat_bot/exchange/process.rb
+++ app/services/wechat_bot/exchange/process.rb
@@ -1,7 +1,9 @@
 # we make sure all is loaded to get the constants and subclasses
-Dir["#{File.dirname(__FILE__)}/**/*.rb"].reverse.each do |file|
-  load file
-end
+# Dir["#{File.dirname(__FILE__)}/**/*.rb"].reverse.each do |file|
+#   load file
+# end
+
+require 'wechat_bot'
 # we manage the memory of the bot and trigger different events depending  on what the customer transmit
 # so far it's used only with Wechat Bot but could be astracted elsewhere later on by changing it a little bit.
 module WechatBot
