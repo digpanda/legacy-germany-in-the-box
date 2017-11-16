9fc4df085187f9e82f965d14766da9bff8d09d4e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-16 12:57:56 +0100

    improved sign-in global speed

--- spec/support/features/login.rb
+++ spec/support/features/login.rb
@@ -7,9 +7,7 @@ module Helpers
         visit new_user_session_path
         fill_in 'user[email]', with: account.email
         fill_in 'user[password]', with: '12345678'
-        sleep(0.6)
         page.first('#sign_in').trigger('click')
-        sleep(0.6)
         expect(page).not_to have_current_path(new_user_session_path)
       end
     end
