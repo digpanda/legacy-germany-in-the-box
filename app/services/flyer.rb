ec2064d85c85cbab4d2a5a5a277a6f7f7cd7f5cc
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-27 11:41:08 +0100

    refactoring and progress on the qrcode system for package sets

--- app/services/flyer.rb
+++ app/services/flyer.rb
@@ -8,6 +8,18 @@ class Flyer < BaseService
   def initialize
   end
 
+  def process_cover_qrcode(cover, qrcode_path)
+    # here we want to integrate the image
+    # of the package set and the qrcode
+    @image = Magick::ImageList.new("#{Rails.root}/public/images/flyers/qrcode-with-image.jpg")
+    qrcode = Magick::Image.read("#{Rails.root}/public#{qrcode_path}").first
+    qrcode = qrcode.resize_to_fit(140, 140)
+    image.composite!(qrcode, 105, 540, Magick::OverCompositeOp)
+    image.format = 'jpeg'
+
+    self
+  end
+
   def process_steps(coupon, qrcode_path)
     @image = Magick::ImageList.new("#{Rails.root}/public/images/flyers/steps.jpg")
     text = Magick::Draw.new
@@ -25,7 +37,6 @@ class Flyer < BaseService
 
     image.composite!(append_image, 105, 540, Magick::OverCompositeOp)
 
-    image.format = 'jpeg'
     self
   end
 
