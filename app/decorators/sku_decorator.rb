30acf655550e8993a9c6eacf2dd95d40c8afee81
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-11-30 17:34:19 +0000

    reseller prices shown

--- app/decorators/sku_decorator.rb
+++ app/decorators/sku_decorator.rb
@@ -34,15 +34,15 @@ class SkuDecorator < Draper::Decorator
     !self.data.nil? && (self.data.is_a?(String) && !self.data.empty?)
   end
 
+  # def total_price
+  #   price_with_taxes * quantity
+  # end
+
   # if the casual total price (the normal one) is different from the current price with taxes
   # it means the price has been altered and is different
   def custom_price?
     casual_total_price != price_with_taxes
   end
-
-  # def total_price
-  #   price_with_taxes * quantity
-  # end
 
   def casual_total_price
     custom_price = Thread.current[:custom_price]
