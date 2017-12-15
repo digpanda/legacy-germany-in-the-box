484d9ef3f16e9ae513082cf026b05adae06800cf
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-12-15 13:06:49 +0100

    short fix

--- app_front/app/javascripts/controllers/guest/package_sets/show.js
+++ app_front/app/javascripts/controllers/guest/package_sets/show.js
@@ -1,27 +1,27 @@
-// import VueAwesomeSwiper from 'vue-awesome-swiper'
-
-/**
-* PackageSetsShow Class
-* We use vue for a carousel system within the page
-*/
-var PackageSetsShow = {
-  //
-  // vue: null,
-  // init: function() {
-  //
-  //   // everything is auto managed ... incredible vuejs.
-  //   Vue.use(VueAwesomeSwiper);
-  //
-  //   this.vue = new Vue({
-  //     el: '#slider-vue',
-  //     data: {
-  //     },
-  //     watch: {
-  //     },
-  //     methods: {
-  //     },
-  //   });
-  },
-}
-
-module.exports = PackageSetsShow;
+// // import VueAwesomeSwiper from 'vue-awesome-swiper'
+//
+// /**
+// * PackageSetsShow Class
+// * We use vue for a carousel system within the page
+// */
+// var PackageSetsShow = {
+//   //
+//   // vue: null,
+//   // init: function() {
+//   //
+//   //   // everything is auto managed ... incredible vuejs.
+//   //   Vue.use(VueAwesomeSwiper);
+//   //
+//   //   this.vue = new Vue({
+//   //     el: '#slider-vue',
+//   //     data: {
+//   //     },
+//   //     watch: {
+//   //     },
+//   //     methods: {
+//   //     },
+//   //   });
+//   },
+// }
+//
+// module.exports = PackageSetsShow;
