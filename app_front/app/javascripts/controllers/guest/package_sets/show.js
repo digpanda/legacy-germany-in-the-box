502b950593400a456e42988a6409d7dfb926c48e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-12-15 12:47:29 +0100

    cancel slider for push to prod

--- app_front/app/javascripts/controllers/guest/package_sets/show.js
+++ app_front/app/javascripts/controllers/guest/package_sets/show.js
@@ -1,26 +1,26 @@
-import VueAwesomeSwiper from 'vue-awesome-swiper'
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 /**
 * PackageSetsShow Class
 * We use vue for a carousel system within the page
 */
 var PackageSetsShow = {
-
-  vue: null,
-  init: function() {
-
-    // everything is auto managed ... incredible vuejs.
-    Vue.use(VueAwesomeSwiper);
-
-    this.vue = new Vue({
-      el: '#slider-vue',
-      data: {
-      },
-      watch: {
-      },
-      methods: {
-      },
-    });
+  //
+  // vue: null,
+  // init: function() {
+  //
+  //   // everything is auto managed ... incredible vuejs.
+  //   Vue.use(VueAwesomeSwiper);
+  //
+  //   this.vue = new Vue({
+  //     el: '#slider-vue',
+  //     data: {
+  //     },
+  //     watch: {
+  //     },
+  //     methods: {
+  //     },
+  //   });
   },
 }
 
