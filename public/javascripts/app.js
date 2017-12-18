cf7147e3764264cc3db20c52849d00b0aacb90ab
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-12-18 13:20:13 +0100

    validation fix on the customer country pid

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3413,15 +3413,17 @@ var InputValidation = {
     return $('form').find(field).prop('required', false).removeData(['bs.validator.errors', 'bs.validator.previous']).end().validator('validate');
   },
 
+  requireValidation: function requireValidation(field) {
+    return $('form').find(field).prop('required', true).removeData(['bs.validator.errors', 'bs.validator.previous']).end().validator('validate');
+  },
+
   // Depending the country we remove or not the mandatory attribute
   // and then hide the field (Europe / China)
   // NOTE : all those are sensitive since we use the raw selectors generated from Rails
   // If there's any problem about it, check them from the HTML directly and fix it here
   addressPidValidation: function addressPidValidation() {
-    console.log('refresh validation');
     InputValidation.refreshPidValidation();
     $("input[name='address[country]'").on('click', function (e) {
-      console.log('something is happening');
       InputValidation.refreshPidValidation();
     });
   },
@@ -3429,10 +3431,10 @@ var InputValidation = {
   refreshPidValidation: function refreshPidValidation() {
     console.log($('#address_country_europe:checked').val());
     if ($('#address_country_europe:checked').val()) {
-      //this.cancelValidation('#address_pid')
+      this.cancelValidation('#address_pid');
       $('#address_pid').hide();
     } else {
-      //this.requireValidation('#address_pid')
+      this.requireValidation('#address_pid');
       $('#address_pid').show();
     }
   },
