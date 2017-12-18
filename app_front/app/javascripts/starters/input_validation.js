51796e91b40ae0a35cbf607de0346c20ab6d92a2
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-12-18 07:55:31 +0100

    pid mandatory on chinese addresses only (front + back done)

--- app_front/app/javascripts/starters/input_validation.js
+++ app_front/app/javascripts/starters/input_validation.js
@@ -9,7 +9,39 @@ var InputValidation = {
   init: function() {
 
     this.restrictToChinese();
+    this.addressPidValidation();
 
+  },
+
+  cancelValidation: function(field) {
+
+    return $('form').find(field)
+             .prop('required', false)
+             .removeData(['bs.validator.errors', 'bs.validator.previous']).end()
+             .validator('validate');
+
+  },
+
+  // Depending the country we remove or not the mandatory attribute
+  // and then hide the field (Europe / China)
+  // NOTE : all those are sensitive since we use the raw selectors generated from Rails
+  // If there's any problem about it, check them from the HTML directly and fix it here
+  addressPidValidation: function() {
+    InputValidation.refreshPidValidation();
+    $("input[name='address[country]'").on('click', function(e) {
+      InputValidation.refreshPidValidation();
+    })
+  },
+
+  refreshPidValidation() {
+    console.log( $('#address_country_europe:checked').val() );
+    if ($('#address_country_europe:checked').val()) {
+      //this.cancelValidation('#address_pid')
+      $('#address_pid').hide();
+    } else {
+      //this.requireValidation('#address_pid')
+      $('#address_pid').show();
+    }
   },
 
   /**
