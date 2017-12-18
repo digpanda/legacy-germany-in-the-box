598f238f424f1c3007a9feb5ff43b0c2dfbd5b24
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-12-18 12:59:30 +0100

    workbench under progress

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3406,7 +3406,36 @@ var InputValidation = {
   init: function init() {
 
     this.restrictToChinese();
+    this.addressPidValidation();
   },
+
+  cancelValidation: function cancelValidation(field) {
+
+    return $('form').find(field).prop('required', false).removeData(['bs.validator.errors', 'bs.validator.previous']).end().validator('validate');
+  },
+
+  // Depending the country we remove or not the mandatory attribute
+  // and then hide the field (Europe / China)
+  // NOTE : all those are sensitive since we use the raw selectors generated from Rails
+  // If there's any problem about it, check them from the HTML directly and fix it here
+  addressPidValidation: function addressPidValidation() {
+    InputValidation.refreshPidValidation();
+    $("input[name='address[country]'").on('click', function (e) {
+      InputValidation.refreshPidValidation();
+    });
+  },
+
+  refreshPidValidation: function refreshPidValidation() {
+    console.log($('#address_country_europe:checked').val());
+    if ($('#address_country_europe:checked').val()) {
+      //this.cancelValidation('#address_pid')
+      $('#address_pid').hide();
+    } else {
+      //this.requireValidation('#address_pid')
+      $('#address_pid').show();
+    }
+  },
+
 
   /**
    * USE : just add data-error to an input to have the custom error show up
