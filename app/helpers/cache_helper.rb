360d41ec4edf5c8de1a9a3b04f39eefd4bef411d
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-12-20 11:34:25 +0100

    cache labelling under progress (better cache with multiples resources

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -1,12 +1,23 @@
 module CacheHelper
 
-  def solve_cache(&block)
-    return Rails.cache.fetch("anything") if Rails.cache.fetch("anything")
+  def solve_cache(request, resources, &block)
+
+    cache_key = resources.map do |resource|
+      if resource.respond_to?(:id)
+        resource.id.to_s
+      elsif resource.nil?
+        'nil'
+      else
+        resource.to_s
+      end
+    end.join('-')
+
+    return Rails.cache.fetch(cache_key) if Rails.cache.fetch(cache_key)
     data = capture do
       yield
     end
-    Rails.cache.write("anything", data)
-    Rails.cache.fetch("anything")
+    Rails.cache.write(cache_key, data)
+    Rails.cache.fetch(cache_key)
   end
 
 end
