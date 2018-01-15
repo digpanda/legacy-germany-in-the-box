b88a6db70e79bbf00a5b564b101fe5e4c299a572
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-01-15 16:36:16 +0100

    dispatcher limited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -87,8 +87,10 @@ class ApplicationController < ActionController::Base
   # if there is a URL containing a code
   # we try to store it before sign-in
   def assign_referrer
-    SlackDispatcher.new.message("PARAM REFERENCE ID #{params[:reference_id]}")
-    session[:reference_id] = params[:reference_id] if params[:reference_id]
+    if params[:reference_id]
+      session[:reference_id] = params[:reference_id]
+      SlackDispatcher.new.message("PARAM REFERENCE ID #{params[:reference_id]}")
+    end
   end
 
   def current_page
