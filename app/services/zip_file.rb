69c195368f60ed3bb9dc087dd31a23399df1b5ea
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-01-31 12:35:29 +0100

    change in zipper

--- app/services/zip_file.rb
+++ app/services/zip_file.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'zip'
 
 class ZipFile
+  attr_reader :input_dir, :output_file
 
   # Initialize with the directory to zip and the location of the output archive.
   def initialize(input_dir, output_file)
@@ -12,7 +13,7 @@ class ZipFile
   def perform
     # Dir["#{TEMPORARY_DIRECTORY}*"]
     Zip::File.open(output_file, Zip::File::CREATE) do |zip|
-      Dir["#{TEMPORARY_DIRECTORY}*"].each do |file_to_zip|
+      Dir["#{input_dir}*"].each do |file_to_zip|
         filename = File.basename(file_to_zip)
         zip.add(filename, file_to_zip)
       end
