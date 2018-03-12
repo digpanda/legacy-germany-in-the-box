a3c73a313e1714a52ee4a700cbac8776e9657707
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-03-12 12:43:39 +0100

    accept any method on cors restriction

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module DigPanda
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :put, :patch, :post, :options]
       end
     end
 
