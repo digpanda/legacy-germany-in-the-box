e571fcb0febf3f96f19c37ae801a11cdf0b56c68
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-03-28 18:08:45 +0200

    pdf fix on long table

--- app/controllers/guest/orders_controller.rb
+++ app/controllers/guest/orders_controller.rb
@@ -9,7 +9,7 @@ class Guest::OrdersController < ApplicationController
     respond_to do |format|
       format.pdf do
         render pdf: "#{bill_file_name}", disposition: disposition,
-               margin: { bottom: 20, top: 30, right: 5, left: 5 },
+               margin: { bottom: 30, top: 30, right: 5, left: 5 },
                footer: { html: { template: 'layouts/pdf/footer.pdf.erb' } },
                header: { html: { template: 'layouts/pdf/header.pdf.erb' } }
       end
